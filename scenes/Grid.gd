5db2dd3bf5d14f42795c56db95a047cee40aea37
Aaron Pieper <Aaron Pieper>
2020-02-22 10:11:56 -0500

    Refactoring, comments, variable names.
    Pieces are now universally called 'blocks', I'd like to adopt a
    universal glossary of terms for terminology like this which could
    otherwise be ambiguous.

--- scenes/Grid.gd
+++ scenes/Grid.gd
@@ -1,34 +1,57 @@
+"""
+Stores information about the game grid: writing blocks to the grid, calculating whether a line was cleared or whether
+a box was made, pausing and playing sound effects
+"""
 extends Control
 
+# constants used when drawing cubits which are connected to other cubits
 const CONNECTED_UP = 1
 const CONNECTED_DOWN = 2
 const CONNECTED_LEFT = 4
 const CONNECTED_RIGHT = 8
 
+# grid dimensions. the grid extends a few rows higher than what the player can see
+const ROW_COUNT = 18
+const COL_COUNT = 9
+
+# signal emitted when a line is cleared
 signal line_clear
 
-var row_count = 18
-var col_count = 9
-
+# lines which are currently being cleared
 var cleared_lines = []
+# total frames to wait when clearing a line/making a box
 var line_clear_delay = 0
+# remaining frames to wait for clearing the current lines
 var remaining_line_clear_frames = 0
+# remaining frames to wait for making the current box
 var remaining_box_build_frames = 0
+# 'true' if the 'line fall sound' should play after the current lines are cleared. The sound doesn't play if nothing
+# drops.
 var should_play_line_fall_sound = false
 
+# sounds which play as the player continues a combo
 onready var combo_sound_arr = [$Combo1Sound, $Combo1Sound, $Combo2Sound, $Combo3Sound, $Combo4Sound, $Combo5Sound,
 		$Combo6Sound, $Combo7Sound, $Combo8Sound, $Combo9Sound, $Combo10Sound, $Combo11Sound, $Combo12Sound,
 		$Combo13Sound, $Combo14Sound, $Combo15Sound, $Combo16Sound, $Combo17Sound, $Combo18Sound, $Combo19Sound,
 		$Combo20Sound]
+
+# bonus points which are awarded as the player continues a combo
 var combo_score_arr = [0, 5, 5, 10, 10, 15, 15, 20]
+
+# player's current combo
 var combo = 0
+# The number of blocks the player has dropped without clearing a line or making a box, plus one.
 var combo_break = 0
 
-var stats_lines = 0
+# total number of seconds for the current game
 var stats_seconds = 0
+# raw number of cleared lines for the current game, not counting any bonus points
+var stats_lines = 0
+# total number of bonus points awarded in the current game by clearing blocks
 var stats_block_score = 0
+# total number of bonus points awarded in the current game for combos
 var stats_combo_score = 0
-
+# 'true' if the player is currently playing, and the time spent should count towards their stats
 var clock_running = false
 
 onready var Score = get_node("../Score")
@@ -62,6 +85,9 @@ func _physics_process(delta):
 				if !cleared_lines.empty():
 					delete_rows()
 
+"""
+Clears the grid and resets everything for a new game.
+"""
 func start_game():
 	stats_lines = 0
 	stats_block_score = 0
@@ -69,26 +95,37 @@ func start_game():
 	stats_seconds = 0
 	$TileMap.clear()
 
+"""
+Writes a block to the grid, checking whether it makes any boxes or clears any lines.
+
+Returns true if the newly written block results in a pause of some sort.
+"""
 func write_block(pos, rotation, type, new_line_clear_frames, death_block = false):
 	for i in range(0, type.pos_arr[rotation].size()):
 		var cubit_pos = type.pos_arr[rotation][i]
 		var cubit_color = type.color_arr[rotation][i]
-		set_cell(pos.x + cubit_pos.x, pos.y + cubit_pos.y, cubit_color)
+		set_cubit(pos.x + cubit_pos.x, pos.y + cubit_pos.y, cubit_color)
 	
 	if death_block:
 		Score.add_score(Score.combo_score)
 		Score.set_combo_score(0)
-		return
+		return false
 	
 	if check_for_boxes():
 		line_clear_delay = new_line_clear_frames
 		remaining_box_build_frames = line_clear_delay
+		return true
 	elif check_for_line_clear():
 		line_clear_delay = new_line_clear_frames
 		remaining_line_clear_frames = line_clear_delay
+		return true
 	else:
 		line_clear_delay = 0
+		return false
 
+"""
+Deletes all cleared lines from the grid, shifting everything above them down to fill the gap.
+"""
 func delete_rows():
 	should_play_line_fall_sound = false
 	for cleared_line in cleared_lines:
@@ -97,87 +134,115 @@ func delete_rows():
 	if should_play_line_fall_sound:
 		$LineFallSound.play()
 
+"""
+Creates a new integer matrix of the same dimensions as the grid.
+"""
 func int_matrix():
 	var matrix = []
-	for y in range(0, row_count):
+	for y in range(0, ROW_COUNT):
 		matrix.append([])
-		for x in range(0, col_count):
-			matrix[y].resize(col_count)
+		for x in range(0, COL_COUNT):
+			matrix[y].resize(COL_COUNT)
 	return matrix
 
-func discussion_trucks(vv, box_height):
-	var wv = int_matrix()
-	for y in range(0, row_count):
-		for x in range(0, col_count):
-			if vv[y][x] >= box_height:
-				if x == 0:
-					wv[y][x] = 1
-				else:
-					wv[y][x] = wv[y][x - 1] + 1
-			else:
-				wv[y][x] = 0
-	return wv
+"""
+Calculates the possible locations for a (width x height) rectangle in the grid, given an integer matrix with the
+possible locations for a (1 x height) rectangle in the grid. These rectangles must consist of dropped blocks which
+haven't been split apart by lines. They can't consist of any empty cells or any previously built boxes.
 
-func check_for_boxes():
-	# counts the number of consecutive filled cubits, vertically. used for detecting boxes
-	var vv = int_matrix()
-	
-	for y in range(0, row_count):
-		for x in range(0, col_count):
+The method name is nonsensical and should be changed.
+"""
+func discussion_trucks(db, box_height):
+	var dt = int_matrix()
+	for y in range(0, ROW_COUNT):
+		for x in range(0, COL_COUNT):
+			if db[y][x] >= box_height:
+				if x == 0:
+					dt[y][x] = 1
+				else:
+					dt[y][x] = dt[y][x - 1] + 1
+			else:
+				dt[y][x] = 0
+	return dt
+
+"""
+Calculates the possible locations for a (1 x height) rectangle in the grid, capable of being a part of a 3x3, 3x4, or
+3x5 'box'. These rectangles must consist of dropped blocks which haven't been split apart by lines. They can't consist
+of any empty cells or any previously built boxes.
+
+The method name is nonsensical and should be changed.
+"""
+func discussion_bicycles():
+	var db = int_matrix()
+	for y in range(0, ROW_COUNT):
+		for x in range(0, COL_COUNT):
 			var block_color = $TileMap.get_cell(x, y)
 			var autotile_coord = $TileMap.get_cell_autotile_coord(x, y)
 			if block_color != -1 and autotile_coord.y != 0 and autotile_coord.y != 18:
 				if y == 0:
-					vv[y][x] = 1
+					db[y][x] = 1
 				else:
-					vv[y][x] = vv[y - 1][x] + 1
+					db[y][x] = db[y - 1][x] + 1
 			else:
-				vv[y][x] = 0
+				db[y][x] = 0
+	return db
+
+"""
+Builds any possible 3x3, 3x4 or 3x5 'boxes' in the grid, returning 'true' if a box was built.
+"""
+func check_for_boxes():
+	# Calculate the possible locations for a (w x h) rectangle in the grid
+	var db = discussion_bicycles()
+	var dt3 = discussion_trucks(db, 3)
+	var dt4 = discussion_trucks(db, 4)
+	var dt5 = discussion_trucks(db, 5)
 	
-	var wv3 = discussion_trucks(vv, 3)
-	var wv4 = discussion_trucks(vv, 4)
-	var wv5 = discussion_trucks(vv, 5)
-	
-	for y in range(0, row_count):
-		for x in range(0, col_count):
+	for y in range(0, ROW_COUNT):
+		for x in range(0, COL_COUNT):
 			# check for 5x3s (vertical)
-			if wv5[y][x] >= 3 and check_for_box(wv3, x - 2, y - 4, 3, 5, true):
+			if dt5[y][x] >= 3 and check_for_box(dt3, x - 2, y - 4, 3, 5, true):
 				$GoldBoxSound.play()
-				# exit box check; a block can only make one box, and making a box invalidates the vv cache
+				# exit box check; a dropped block can only make one box, and making a box invalidates the db cache
 				remaining_box_build_frames = line_clear_delay
 				return true
 			
 			# check for 4x3s (vertical)
-			if wv4[y][x] >= 3 and check_for_box(wv3, x - 2, y - 3, 3, 4, true):
+			if dt4[y][x] >= 3 and check_for_box(dt3, x - 2, y - 3, 3, 4, true):
 				$GoldBoxSound.play()
-				# exit box check; a block can only make one box, and making a box invalidates the vv cache
+				# exit box check; a dropped block can only make one box, and making a box invalidates the db cache
 				remaining_box_build_frames = line_clear_delay
 				return true
 			
 			# check for 5x3s (horizontal)
-			if wv3[y][x] >= 5 and check_for_box(wv3, x - 4, y - 2, 5, 3, true):
+			if dt3[y][x] >= 5 and check_for_box(dt3, x - 4, y - 2, 5, 3, true):
 				$GoldBoxSound.play()
-				# exit box check; a block can only make one box, and making a box invalidates the vv cache
+				# exit box check; a dropped block can only make one box, and making a box invalidates the db cache
 				remaining_box_build_frames = line_clear_delay
 				return true
 			
 			# check for 4x3s (horizontal)
-			if wv3[y][x] >= 4 and check_for_box(wv3, x - 3, y - 2, 4, 3, true):
+			if dt3[y][x] >= 4 and check_for_box(dt3, x - 3, y - 2, 4, 3, true):
 				$GoldBoxSound.play()
-				# exit box check; a block can only make one box, and making a box invalidates the vv cache
+				# exit box check; a dropped block can only make one box, and making a box invalidates the db cache
 				remaining_box_build_frames = line_clear_delay
 				return true
 			
 			# check for 3x3s
-			if wv3[y][x] >= 3 and check_for_box(wv3, x - 2, y - 2, 3, 3):
+			if dt3[y][x] >= 3 and check_for_box(dt3, x - 2, y - 2, 3, 3):
 				$SilverBoxSound.play()
-				# exit box check; a block can only make one box, and making a box invalidates the vv cache
+				# exit box check; a dropped block can only make one box, and making a box invalidates the db cache
 				remaining_box_build_frames = line_clear_delay
 				return true
-	
 	return false
 
-func check_for_box(wv3, x, y, width, height, gold = false):
+"""
+Checks whether the specified rectangle represents an enclosed box. An enclosed box must not connect to any pieces
+outside the box.
+
+It's assumed the rectangle's coordinates contain only dropped blocks which haven't been split apart by lines, and
+no empty cells.
+"""
+func check_for_box(dt3, x, y, width, height, gold = false):
 	for curr_x in range(x, x + width):
 		if int($TileMap.get_cell_autotile_coord(curr_x, y).x) & CONNECTED_UP > 0:
 			return
@@ -199,39 +264,43 @@ func check_for_box(wv3, x, y, width, height, gold = false):
 		offset = 16
 	
 	# corners
-	$TileMap.set_cell(x + 0, y + 0, 0, false, false, false, Vector2(offset + 0, 18))
-	$TileMap.set_cell(x + width - 1, y + 0, 0, false, false, false, Vector2(offset + 2, 18))
-	$TileMap.set_cell(x + 0, y + height - 1, 0, false, false, false, Vector2(offset + 6, 18))
-	$TileMap.set_cell(x + width - 1, y + height - 1, 0, false, false, false, Vector2(offset + 8, 18))
+	set_cubit(x + 0, y + 0, Vector2(offset + 0, 18))
+	set_cubit(x + width - 1, y + 0, Vector2(offset + 2, 18))
+	set_cubit(x + 0, y + height - 1, Vector2(offset + 6, 18))
+	set_cubit(x + width - 1, y + height - 1, Vector2(offset + 8, 18))
 	
 	# top/bottom edge
 	for curr_x in range(x + 1, x + width - 1):
-		$TileMap.set_cell(curr_x, y + 0, 0, false, false, false, Vector2(offset + 1, 18))
-		$TileMap.set_cell(curr_x, y + height - 1, 0, false, false, false, Vector2(offset + 7, 18))
+		set_cubit(curr_x, y + 0, Vector2(offset + 1, 18))
+		set_cubit(curr_x, y + height - 1, Vector2(offset + 7, 18))
 	
 	# center
 	for curr_x in range(x + 1, x + width - 1):
 		for curr_y in range(y + 1, y + height - 1):
-			$TileMap.set_cell(curr_x, curr_y, 0, false, false, false, Vector2(offset + 4, 18))
+			set_cubit(curr_x, curr_y, Vector2(offset + 4, 18))
 	
 	# left/right edge
 	for curr_y in range(y + 1, y + height - 1):
-		$TileMap.set_cell(x + 0, curr_y, 0, false, false, false, Vector2(offset + 3, 18))
-		$TileMap.set_cell(x + width - 1, curr_y, 0, false, false, false, Vector2(offset + 5, 18))
+		set_cubit(x + 0, curr_y, Vector2(offset + 3, 18))
+		set_cubit(x + width - 1, curr_y, Vector2(offset + 5, 18))
 	
 	return true
 
+"""
+Checks whether any lines in the grid are full and should be cleared. Updates the combo, awards points, and plays
+sounds appropriately. Returns 'true' if any lines were cleared.
+"""
 func check_for_line_clear():
 	var total_points = 0
 	var block_points = 0
-	for y in range(0, row_count):
+	for y in range(0, ROW_COUNT):
 		if row_is_full(y):
 			var line_score = 1
 			line_score += combo_score_arr[clamp(combo, 0, combo_score_arr.size() - 1)]
 			stats_lines += 1
 			stats_combo_score += combo_score_arr[clamp(combo, 0, combo_score_arr.size() - 1)]
 			cleared_lines.append(y)
-			for x in range(0, col_count):
+			for x in range(0, COL_COUNT):
 				var autotile_coord = $TileMap.get_cell_autotile_coord(x, y)
 				if autotile_coord.y == 18:
 					if autotile_coord.x == 7 || autotile_coord.x == 10 || autotile_coord.x == 13:
@@ -273,15 +342,22 @@ func check_for_line_clear():
 	
 	return total_points > 0
 
+"""
+Play sound effects for clearing a line. A cleared line can result in several sound effects getting queued and played
+consecutively.
+"""
 func play_line_clear_sfx(cleared_lines, combo, block_points):
 	var scheduled_sfx = []
+	
+	# determine the main line-clear sound effect, which plays for clearing any line
 	if cleared_lines.size() == 1:
 		scheduled_sfx.append($Erase1Sound)
 	elif cleared_lines.size() == 2:
 		scheduled_sfx.append($Erase2Sound)
 	else:
 		scheduled_sfx.append($Erase3Sound)
-	var chain_sound_count = 0
+	
+	# determine any combo sound effects, which play for continuing a combo
 	for combo_sfx in range(combo - cleared_lines.size(), combo):
 		if combo_sfx > 0:
 			scheduled_sfx.append(combo_sound_arr[min(combo_sfx, combo_sound_arr.size() - 1)])
@@ -289,26 +365,36 @@ func play_line_clear_sfx(cleared_lines, combo, block_points):
 		scheduled_sfx.append($ClearSilverBlockSound)
 	elif block_points >= 2:
 		scheduled_sfx.append($ClearGoldBlockSound)
+	
+	# play the calculated sound effects
 	if scheduled_sfx.size() > 0:
+		# play the first sound effect immediately
 		scheduled_sfx[0].play()
+		# enqueue other sound effects and play them later
 		for sfx_index in range(1, scheduled_sfx.size()):
 			yield(get_tree().create_timer(0.025 * sfx_index), "timeout")
 			scheduled_sfx[sfx_index].play()
 
 func row_is_full(y):
-	for x in range(0, col_count):
+	for x in range(0, COL_COUNT):
 		if is_cell_empty(x, y):
 			return false
 	return true
 
+"""
+Clear all cells in the specified row. This leaves any blocks above them floating in mid-air.
+"""
 func clear_row(y):
-	for x in range(0, col_count):
+	for x in range(0, COL_COUNT):
 		disconnect_cubit(x, y)
-		$TileMap.set_cell(x, y, -1)
+		clear_cubit(x, y)
 
+"""
+Deletes the specified row in the grid, dropping all higher rows down to fill the gap.
+"""
 func delete_row(y):
 	for curr_y in range(y, 0, -1):
-		for x in range(0, col_count):
+		for x in range(0, COL_COUNT):
 			var block_color = $TileMap.get_cell(x, curr_y - 1)
 			var autotile_coord = $TileMap.get_cell_autotile_coord(x, curr_y - 1)
 			$TileMap.set_cell(x, curr_y, block_color, false, false, false, autotile_coord)
@@ -317,9 +403,13 @@ func delete_row(y):
 				should_play_line_fall_sound = true
 	
 	# remove row
-	for x in range(0, col_count):
-		$TileMap.set_cell(x, 0, -1)
+	for x in range(0, COL_COUNT):
+		clear_cubit(x, 0)
 
+"""
+Disconnects the specified cubit from all cubits it's connected to, directly or indirectly. All disconnected cubits are
+flagged to ensure they can't be included in 'boxes' in the future.
+"""
 func disconnect_cubit(x, y):
 	# store connections
 	var old_autotile_coord = $TileMap.get_cell_autotile_coord(x, y)
@@ -329,22 +419,25 @@ func disconnect_cubit(x, y):
 		return
 	
 	# disconnect
-	$TileMap.set_cell(x, y, 0, false, false, false, Vector2(0, 0))
+	set_cubit(x, y, Vector2(0, 0))
 	
 	if y > 0 && int(old_autotile_coord.x) & CONNECTED_UP > 0:
 		disconnect_cubit(x, y - 1)
 	
-	if y < row_count - 1 && int(old_autotile_coord.x) & CONNECTED_DOWN > 0:
+	if y < ROW_COUNT - 1 && int(old_autotile_coord.x) & CONNECTED_DOWN > 0:
 		disconnect_cubit(x, y + 1)
 	
 	if x > 0 && int(old_autotile_coord.x) & CONNECTED_LEFT > 0:
 		disconnect_cubit(x - 1, y)
 	
-	if x < col_count - 1 && int(old_autotile_coord.x) & CONNECTED_RIGHT > 0:
+	if x < COL_COUNT - 1 && int(old_autotile_coord.x) & CONNECTED_RIGHT > 0:
 		disconnect_cubit(x + 1, y)
 
-func set_cell(x, y, block_color):
-	$TileMap.set_cell(x, y, 0, false, false, false, block_color)
+func set_cubit(x, y, cubit_color):
+	$TileMap.set_cell(x, y, 0, false, false, false, cubit_color)
+
+func clear_cubit(x, y):
+	$TileMap.set_cell(x, y, -1)
 
 func is_cell_empty(x, y):
 	return $TileMap.get_cell(x, y) == -1
