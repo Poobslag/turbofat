72254a8e00c1d57398cd949661b9a50fe46370ad
Poobslag <aaron.pieper@gmail.com>
2020-02-24 19:01:19 -0500

    Blocks can be smushed through other blocks by pressing 'down'
    Tapping 'down' on a block will try to smush it vertically through other
    blocks. Holding 'down' will smush a block at the last moment. Tapping
    'down' can also prevent a piece from locking, although this cannot be
    used indefinitely.
    
    Refactored block movement logic to use state pattern
    
    There's now a brief delay after a piece locks before lines are cleared.
    This delay has been subtracted from the 'appearance delay'. The primary
    purpose of this delay was to facilitate 'piece smushing', although
    there's also an arguable aesthetic benefit to seeing a piece dropped
    before lines are cleared.

--- scenes/PieceSpeeds.gd
+++ scenes/PieceSpeeds.gd
@@ -11,42 +11,42 @@ const G = 256
 
 var all_speeds = [
 	# Level 1: Beginner
-	PieceSpeed.new(   4, 27, 27, 16, 30, 40),
-	PieceSpeed.new(   6, 27, 27, 16, 30, 40),
-	PieceSpeed.new(  10, 27, 27, 16, 30, 40),
-	PieceSpeed.new(  16, 27, 27, 16, 30, 40),
-	PieceSpeed.new(  24, 27, 27, 16, 30, 40),
-	PieceSpeed.new(  32, 27, 27, 16, 30, 40),
-	PieceSpeed.new(  48, 27, 27, 16, 30, 40),
-	PieceSpeed.new(  64, 27, 27, 16, 30, 40),
-	PieceSpeed.new(  96, 27, 27, 16, 30, 40),
+	PieceSpeed.new(   4, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new(   6, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new(  10, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new(  16, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new(  24, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new(  32, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new(  48, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new(  64, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new(  96, 20, 20,  7, 16, 30, 40),
 	# Level 10: 0.5 G
-	PieceSpeed.new( 128, 27, 27, 16, 30, 40),
-	PieceSpeed.new( 152, 27, 27, 16, 30, 40),
-	PieceSpeed.new( 176, 27, 27, 16, 30, 40),
-	PieceSpeed.new( 200, 27, 27, 16, 30, 40),
-	PieceSpeed.new( 224, 27, 27, 16, 30, 40),
+	PieceSpeed.new( 128, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new( 152, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new( 176, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new( 200, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new( 224, 20, 20,  7, 16, 30, 40),
 	# Level 15: 1 G
-	PieceSpeed.new( 1*G, 27, 27, 16, 30, 40),
-	PieceSpeed.new( 2*G, 27, 27, 16, 30, 40),
-	PieceSpeed.new( 3*G, 27, 27, 16, 30, 40),
-	PieceSpeed.new( 4*G, 27, 27, 16, 30, 40),
-	PieceSpeed.new( 5*G, 27, 27, 16, 30, 40),
+	PieceSpeed.new( 1*G, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new( 2*G, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new( 3*G, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new( 4*G, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new( 5*G, 20, 20,  7, 16, 30, 40),
 	# Speed 20: 20G
-	PieceSpeed.new(20*G, 27, 27, 16, 30, 40),
-	PieceSpeed.new(20*G, 27, 27, 10, 30, 25),
-	PieceSpeed.new(20*G, 27, 18, 10, 30, 16),
-	PieceSpeed.new(20*G, 18, 14, 10, 30, 10),
-	PieceSpeed.new(20*G, 14,  8, 10, 30,  8),
+	PieceSpeed.new(20*G, 20, 20,  7, 16, 30, 40),
+	PieceSpeed.new(20*G, 20, 20,  7, 10, 30, 25),
+	PieceSpeed.new(20*G, 20, 12,  7, 10, 30, 16),
+	PieceSpeed.new(20*G, 12,  8,  7, 10, 30, 10),
+	PieceSpeed.new(20*G,  8,  4,  7, 10, 30,  8),
 	# Speed 25: Likely impossible
-	PieceSpeed.new(20*G, 12,  8,  8, 24,  6),
-	PieceSpeed.new(20*G, 12,  8,  8, 20,  6),
-	PieceSpeed.new(20*G, 12,  7,  8, 18,  5),
-	PieceSpeed.new(20*G, 12,  7,  8, 16,  5),
-	PieceSpeed.new(20*G, 12,  6,  8, 14,  4),
-	PieceSpeed.new(20*G,  6,  6,  6, 12,  4),
-	PieceSpeed.new(20*G,  6,  5,  6, 10,  3),
-	PieceSpeed.new(20*G,  6,  5,  6,  8,  3),
+	PieceSpeed.new(20*G,  8,  4,  5,  8, 24,  6),
+	PieceSpeed.new(20*G,  8,  4,  5,  8, 20,  6),
+	PieceSpeed.new(20*G,  8,  3,  5,  8, 18,  5),
+	PieceSpeed.new(20*G,  8,  3,  5,  8, 16,  5),
+	PieceSpeed.new(20*G,  2,  2,  5,  8, 14,  4),
+	PieceSpeed.new(20*G,  2,  2,  5,  6, 12,  4),
+	PieceSpeed.new(20*G,  1,  1,  5,  6, 10,  3),
+	PieceSpeed.new(20*G,  1,  1,  5,  6,  8,  3),
 ]
 
 """
@@ -69,14 +69,18 @@ class PieceSpeed:
 	# number of frames to pause before locking a piece into the playfield
 	var lock_delay
 	
+	# number of frames to pause after locking a piece into the playfield
+	var post_lock_delay
+	
 	# number of frames to pause when clearing a line
 	var line_clear_delay
 	
-	func _init(init_gravity, init_appearance_delay, init_line_appearance_delay, init_delayed_auto_shift_delay, \
-			init_lock_delay, init_line_clear_delay):
+	func _init(init_gravity, init_appearance_delay, init_line_appearance_delay, init_post_lock_delay, \
+			init_delayed_auto_shift_delay, init_lock_delay, init_line_clear_delay):
 		gravity = init_gravity
 		appearance_delay = init_appearance_delay
 		line_appearance_delay = init_line_appearance_delay
+		post_lock_delay = init_post_lock_delay
 		delayed_auto_shift_delay = init_delayed_auto_shift_delay
 		lock_delay = init_lock_delay
 		line_clear_delay = init_line_clear_delay
