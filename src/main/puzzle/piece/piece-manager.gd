Poobslag/turbofat

commit 56e0cb2b1c937572c292bfeba18bb02f9049c6f1

Node-based state pattern for pieces. Minor refactoring.
PieceManager already used a function-based state pattern where it stored
the name of a function to call. It now uses a node-based state pattern.
The state machine code is generic, and has been extracted to
/src/main/state-machine.gd to encourage reuse.

Merged PieceManager.tscn into Puzzle.tscn. PieceManager could not
function without a source of random pieces and playfield constraints. It
had logic for providing its own if these dependencies were null, but
that kind of code seemed out-of-place in src/main.

ShadowMap no longer requires a reference to '../TileMap', it now provides
shadows for its parent.

Fixed infinite loop when dropping an empty piece.

src/main/puzzle/piece/states/prelock.gd
@@ -1,0 +1,23 @@
+extends State
+"""
+The piece has locked into position. The player can still press 'down' to unlock it or squeeze it past other blocks.
+"""
+
+func update(piece_manager: PieceManager) -> String:
+	var new_state_name := ""
+	piece_manager.apply_player_input()
+	
+	if frames >= piece_manager.piece_speed.post_lock_delay:
+		var active_piece = piece_manager.active_piece
+		if piece_manager.playfield:
+			if piece_manager.playfield.write_piece(active_piece.pos, active_piece.orientation, active_piece.type,
+					piece_manager.piece_speed.line_clear_delay):
+				# line was cleared; different appearance delay
+				active_piece.spawn_delay = piece_manager.piece_speed.line_appearance_delay
+			else:
+				active_piece.spawn_delay = piece_manager.piece_speed.appearance_delay
+		active_piece.setType(PieceTypes.piece_null)
+		piece_manager.tile_map_dirty = true
+		new_state_name = "WaitForPlayfield"
+	
+	return new_state_name

src/main/puzzle/piece/states/wait-for-playfield.gd
@@ -1,0 +1,11 @@
+extends State
+"""
+State: The playfield is clearing lines or making boxes. We're waiting for these animations to complete before spawning
+a new piece.
+"""
+
+func update(piece_manager: PieceManager) -> String:
+	var new_state := ""
+	if piece_manager.playfield == null or piece_manager.playfield.ready_for_new_piece():
+		new_state = "Prespawn"
+	return new_state

src/main/state-machine.gd
@@ -1,0 +1,48 @@
+class_name StateMachine
+extends Node
+"""
+An implementation of the state pattern.
+
+State nodes can be added to this node as children. This class provides logic for switching between its child states,
+invoking their methods, and emitting signals.
+"""
+
+# signal emitted once when a state is entered
+signal entered_state
+
+# the currently active state
+var _state: State setget set_state, get_state
+
+# the host object which is passed into each state
+onready var _host := get_parent()
+
+"""
+Calls 'update' on the currently active state, and changes the current state if necessary.
+"""
+func update() -> void:
+	var old_state = _state
+	var new_state_name: String = _state.update(_host)
+	if new_state_name:
+		# The update() method returned a non-empty string. This signals us to transition to a new state.
+		
+		if old_state == _state:
+			# We only transition if the state stayed the same during the update() method. It's possible for
+			# State1.update() to transition to State2 and return State3. In that case, State2 wins.
+			set_state(get_node(new_state_name))
+	_state.frames += 1
+
+
+"""
+Transitions to a new state.
+
+Resets the state's internal variables and notifies any listeners.
+"""
+func set_state(state: State) -> void:
+	_state = state
+	_state.frames = 0
+	_state.enter(_host)
+	emit_signal("entered_state", state)
+
+
+func get_state() -> State:
+	return _state

project.godot
@@ -19,6 +19,11 @@ _global_script_classes=[ {
 "language": "GDScript",
 "path": "res://src/main/world/multi-outline-sprite.gd"
 }, {
+"base": "Control",
+"class": "PieceManager",
+"language": "GDScript",
+"path": "res://src/main/puzzle/piece/piece-manager.gd"
+}, {
 "base": "Reference",
 "class": "PieceSpeed",
 "language": "GDScript",
@@ -28,6 +33,11 @@ _global_script_classes=[ {
 "class": "PieceType",
 "language": "GDScript",
 "path": "res://src/main/puzzle/piece/piece-type.gd"
+}, {
+"base": "Control",
+"class": "Playfield",
+"language": "GDScript",
+"path": "res://src/main/puzzle/playfield.gd"
 }, {
 "base": "Reference",
 "class": "RankCalculator",
@@ -58,18 +68,32 @@ _global_script_classes=[ {
 "class": "SmoothPath",
 "language": "GDScript",
 "path": "res://src/main/world/restaurant/smooth-path.gd"
+}, {
+"base": "Node",
+"class": "State",
+"language": "GDScript",
+"path": "res://src/main/state.gd"
+}, {
+"base": "Node",
+"class": "StateMachine",
+"language": "GDScript",
+"path": "res://src/main/state-machine.gd"
 } ]
 _global_script_class_icons={
 "ActivePiece": "",
 "MultiOutlineSprite": "",
+"PieceManager": "",
 "PieceSpeed": "",
 "PieceType": "",
+"Playfield": "",
 "RankCalculator": "",
 "RankResult": "",
 "ScenarioButton": "",
 "ScenarioPerformance": "",
 "ScenarioSettings": "",
-"SmoothPath": ""
+"SmoothPath": "",
+"State": "",
+"StateMachine": ""
 }
 
 [application]

src/main/puzzle/Playfield.tscn
@@ -62,59 +62,6 @@
 [ext_resource path="res://assets/puzzle/make-snack-box2.wav" type="AudioStream" id=60]
 
 [sub_resource type="TileSet" id=1]
-0/name = "blocks-shadows.png 0"
-0/texture = ExtResource( 40 )
-0/tex_offset = Vector2( 0, -64 )
-0/modulate = Color( 1, 1, 1, 1 )
-0/region = Rect2( 2, 2, 2364, 656 )
-0/tile_mode = 2
-0/autotile/icon_coordinate = Vector2( 0, 0 )
-0/autotile/tile_size = Vector2( 144, 128 )
-0/autotile/spacing = 4
-0/autotile/occluder_map = [  ]
-0/autotile/navpoly_map = [  ]
-0/autotile/priority_map = [  ]
-0/autotile/z_index_map = [  ]
-0/occluder_offset = Vector2( 0, 0 )
-0/navigation_offset = Vector2( 0, 0 )
-0/shapes = [  ]
-0/z_index = 0
-1/name = "blocks-boxes-shadows.png 1"
-1/texture = ExtResource( 39 )
-1/tex_offset = Vector2( 0, -64 )
-1/modulate = Color( 1, 1, 1, 1 )
-1/region = Rect2( 2, 2, 2364, 656 )
-1/tile_mode = 2
-1/autotile/icon_coordinate = Vector2( 0, 0 )
-1/autotile/tile_size = Vector2( 144, 128 )
-1/autotile/spacing = 4
-1/autotile/occluder_map = [  ]
-1/autotile/navpoly_map = [  ]
-1/autotile/priority_map = [  ]
-1/autotile/z_index_map = [  ]
-1/occluder_offset = Vector2( 0, 0 )
-1/navigation_offset = Vector2( 0, 0 )
-1/shapes = [  ]
-1/z_index = 0
-2/name = "blocks-vegetable-shadows.png 2"
-2/texture = ExtResource( 38 )
-2/tex_offset = Vector2( 0, -64 )
-2/modulate = Color( 1, 1, 1, 1 )
-2/region = Rect2( 2, 2, 2660, 524 )
-2/tile_mode = 2
-2/autotile/icon_coordinate = Vector2( 0, 0 )
-2/autotile/tile_size = Vector2( 144, 128 )
-2/autotile/spacing = 4
-2/autotile/occluder_map = [  ]
-2/autotile/navpoly_map = [  ]
-2/autotile/priority_map = [  ]
-2/autotile/z_index_map = [  ]
-2/occluder_offset = Vector2( 0, 0 )
-2/navigation_offset = Vector2( 0, 0 )
-2/shapes = [  ]
-2/z_index = 0
-
-[sub_resource type="TileSet" id=2]
 0/name = "blocks.png"
 0/texture = ExtResource( 2 )
 0/tex_offset = Vector2( 0, 0 )
@@ -167,6 +114,59 @@
 2/shapes = [  ]
 2/z_index = 0
 
+[sub_resource type="TileSet" id=2]
+0/name = "blocks-shadows.png 0"
+0/texture = ExtResource( 40 )
+0/tex_offset = Vector2( 0, -64 )
+0/modulate = Color( 1, 1, 1, 1 )
+0/region = Rect2( 2, 2, 2364, 656 )
+0/tile_mode = 2
+0/autotile/icon_coordinate = Vector2( 0, 0 )
+0/autotile/tile_size = Vector2( 144, 128 )
+0/autotile/spacing = 4
+0/autotile/occluder_map = [  ]
+0/autotile/navpoly_map = [  ]
+0/autotile/priority_map = [  ]
+0/autotile/z_index_map = [  ]
+0/occluder_offset = Vector2( 0, 0 )
+0/navigation_offset = Vector2( 0, 0 )
+0/shapes = [  ]
+0/z_index = 0
+1/name = "blocks-boxes-shadows.png 1"
+1/texture = ExtResource( 39 )
+1/tex_offset = Vector2( 0, -64 )
+1/modulate = Color( 1, 1, 1, 1 )
+1/region = Rect2( 2, 2, 2364, 656 )
+1/tile_mode = 2
+1/autotile/icon_coordinate = Vector2( 0, 0 )
+1/autotile/tile_size = Vector2( 144, 128 )
+1/autotile/spacing = 4
+1/autotile/occluder_map = [  ]
+1/autotile/navpoly_map = [  ]
+1/autotile/priority_map = [  ]
+1/autotile/z_index_map = [  ]
+1/occluder_offset = Vector2( 0, 0 )
+1/navigation_offset = Vector2( 0, 0 )
+1/shapes = [  ]
+1/z_index = 0
+2/name = "blocks-vegetable-shadows.png 2"
+2/texture = ExtResource( 38 )
+2/tex_offset = Vector2( 0, -64 )
+2/modulate = Color( 1, 1, 1, 1 )
+2/region = Rect2( 2, 2, 2660, 524 )
+2/tile_mode = 2
+2/autotile/icon_coordinate = Vector2( 0, 0 )
+2/autotile/tile_size = Vector2( 144, 128 )
+2/autotile/spacing = 4
+2/autotile/occluder_map = [  ]
+2/autotile/navpoly_map = [  ]
+2/autotile/priority_map = [  ]
+2/autotile/z_index_map = [  ]
+2/occluder_offset = Vector2( 0, 0 )
+2/navigation_offset = Vector2( 0, 0 )
+2/shapes = [  ]
+2/z_index = 0
+
 [node name="Playfield" type="Control"]
 margin_right = 324.0
 margin_bottom = 544.0
@@ -201,26 +201,25 @@ __meta__ = {
 "_edit_use_anchors_": false
 }
 
-[node name="ShadowMap" type="TileMap" parent="TileMapClip"]
-modulate = Color( 0.1, 0.095, 0.095, 1 )
-position = Vector2( 0, -96 )
-scale = Vector2( 0.5, 0.5 )
-tile_set = SubResource( 1 )
-cell_size = Vector2( 72, 64 )
-format = 1
-tile_data = PoolIntArray( 1114113, 0, 2, 1179649, 0, 9, 1179650, 0, 3, 1179651, 0, 4 )
-script = ExtResource( 41 )
-
 [node name="TileMap" type="TileMap" parent="TileMapClip"]
 position = Vector2( 0, -96 )
 scale = Vector2( 0.5, 0.5 )
-tile_set = SubResource( 2 )
+tile_set = SubResource( 1 )
 cell_size = Vector2( 72, 64 )
 format = 1
 tile_data = PoolIntArray( 983046, 4, 0, 1048582, 4, 0, 1114113, 0, 2, 1114118, 4, 0, 1179648, 2, 0, 1179649, 0, 9, 1179650, 0, 12, 1179651, 0, 4, 1179656, 2, 4, 1245184, 2, 1, 1245185, 2, 2, 1245186, 2, 3, 1245190, 2, 5, 1245191, 2, 6, 1245192, 2, 7 )
 
 [node name="CornerMap" parent="TileMapClip/TileMap" instance=ExtResource( 5 )]
 tile_data = PoolIntArray( 2359299, 0, 1 )
+
+[node name="ShadowMap" type="TileMap" parent="TileMapClip/TileMap"]
+modulate = Color( 0.1, 0.095, 0.095, 1 )
+show_behind_parent = true
+tile_set = SubResource( 2 )
+cell_size = Vector2( 72, 64 )
+format = 1
+tile_data = PoolIntArray( 1114113, 0, 2, 1179649, 0, 9, 1179650, 0, 3, 1179651, 0, 4 )
+script = ExtResource( 41 )
 
 [node name="Erase1Sound" type="AudioStreamPlayer" parent="."]
 stream = ExtResource( 6 )

src/main/puzzle/Puzzle.tscn
@@ -1,8 +1,8 @@
-[gd_scene load_steps=22 format=2]
+[gd_scene load_steps=44 format=2]
 
 [ext_resource path="res://src/main/puzzle/puzzle.gd" type="Script" id=1]
 [ext_resource path="res://src/main/puzzle/Playfield.tscn" type="PackedScene" id=2]
-[ext_resource path="res://src/main/puzzle/piece/PieceManager.tscn" type="PackedScene" id=3]
+[ext_resource path="res://src/main/puzzle/piece/piece-manager.gd" type="Script" id=3]
 [ext_resource path="res://src/main/puzzle/piece/NextPieces.tscn" type="PackedScene" id=4]
 [ext_resource path="res://src/main/puzzle/Score.tscn" type="PackedScene" id=5]
 [ext_resource path="res://assets/ui/xolonium-48-outline.tres" type="DynamicFont" id=6]
@@ -18,14 +18,54 @@
 [ext_resource path="res://assets/ui/xolonium-16.tres" type="DynamicFont" id=16]
 [ext_resource path="res://assets/ui/xolonium-36.tres" type="DynamicFont" id=17]
 [ext_resource path="res://assets/ui/xolonium-24.tres" type="DynamicFont" id=18]
+[ext_resource path="res://src/main/puzzle/stretch-map.gd" type="Script" id=19]
+[ext_resource path="res://src/main/puzzle/piece/states/prelock.gd" type="Script" id=20]
+[ext_resource path="res://src/main/puzzle/piece/states/move-piece.gd" type="Script" id=21]
+[ext_resource path="res://src/main/puzzle/piece/states/wait-for-playfield.gd" type="Script" id=22]
+[ext_resource path="res://src/main/state-machine.gd" type="Script" id=23]
+[ext_resource path="res://src/main/puzzle/piece/CornerMap.tscn" type="PackedScene" id=24]
+[ext_resource path="res://assets/puzzle/blocks/blocks.png" type="Texture" id=25]
+[ext_resource path="res://assets/puzzle/squish.wav" type="AudioStream" id=26]
+[ext_resource path="res://assets/world/chef/gameover-voice1.wav" type="AudioStream" id=27]
+[ext_resource path="res://assets/puzzle/lock.wav" type="AudioStream" id=28]
+[ext_resource path="res://assets/puzzle/rotate0.wav" type="AudioStream" id=29]
+[ext_resource path="res://assets/puzzle/rotate1.wav" type="AudioStream" id=30]
+[ext_resource path="res://assets/puzzle/move.wav" type="AudioStream" id=31]
+[ext_resource path="res://assets/ui/gameover.wav" type="AudioStream" id=32]
+[ext_resource path="res://assets/world/chef/gameover-voice2.wav" type="AudioStream" id=33]
+[ext_resource path="res://assets/world/chef/gameover-voice3.wav" type="AudioStream" id=34]
+[ext_resource path="res://assets/world/chef/gameover-voice4.wav" type="AudioStream" id=35]
+[ext_resource path="res://assets/world/chef/gameover-voice0.wav" type="AudioStream" id=36]
+[ext_resource path="res://src/main/puzzle/piece/states/none.gd" type="Script" id=37]
+[ext_resource path="res://src/main/puzzle/piece/states/game-ended.gd" type="Script" id=38]
+[ext_resource path="res://src/main/puzzle/piece/states/prespawn.gd" type="Script" id=39]
 
-[sub_resource type="InputEventAction" id=3]
+[sub_resource type="TileSet" id=1]
+0/name = "blocks.png 0"
+0/texture = ExtResource( 25 )
+0/tex_offset = Vector2( 0, 0 )
+0/modulate = Color( 1, 1, 1, 1 )
+0/region = Rect2( 2, 2, 1212, 268 )
+0/tile_mode = 2
+0/autotile/icon_coordinate = Vector2( 0, 0 )
+0/autotile/tile_size = Vector2( 72, 64 )
+0/autotile/spacing = 4
+0/autotile/occluder_map = [  ]
+0/autotile/navpoly_map = [  ]
+0/autotile/priority_map = [  ]
+0/autotile/z_index_map = [  ]
+0/occluder_offset = Vector2( 0, 0 )
+0/navigation_offset = Vector2( 0, 0 )
+0/shapes = [  ]
+0/z_index = 0
+
+[sub_resource type="InputEventAction" id=2]
 action = "ui_cancel"
 
-[sub_resource type="ShortCut" id=4]
-shortcut = SubResource( 3 )
+[sub_resource type="ShortCut" id=3]
+shortcut = SubResource( 2 )
 
-[sub_resource type="StyleBoxFlat" id=5]
+[sub_resource type="StyleBoxFlat" id=4]
 bg_color = Color( 0.19873, 0.177771, 0.177771, 1 )
 border_width_left = 2
 border_width_top = 2
@@ -80,11 +120,102 @@ margin_top = 28.0
 margin_right = 688.0
 margin_bottom = 572.0
 
-[node name="Piece" parent="." instance=ExtResource( 3 )]
+[node name="PieceManager" type="Control" parent="."]
 margin_left = 364.0
 margin_top = 28.0
 margin_right = 688.0
 margin_bottom = 572.0
+rect_clip_content = true
+script = ExtResource( 3 )
+__meta__ = {
+"_edit_use_anchors_": false
+}
+
+[node name="TileMap" type="TileMap" parent="PieceManager"]
+position = Vector2( 0, -96 )
+scale = Vector2( 0.5, 0.5 )
+tile_set = SubResource( 1 )
+cell_size = Vector2( 72, 64 )
+format = 1
+tile_data = PoolIntArray( 196613, 0, 196608, 262147, 0, 196608, 262148, 0, 196608, 262149, 0, 196608 )
+
+[node name="CornerMap" parent="PieceManager/TileMap" instance=ExtResource( 24 )]
+
+[node name="StretchMap" type="TileMap" parent="PieceManager"]
+position = Vector2( 0, -96 )
+scale = Vector2( 0.5, 0.5 )
+tile_set = SubResource( 1 )
+cell_size = Vector2( 72, 64 )
+format = 1
+tile_data = PoolIntArray( 196613, 0, 196608, 262147, 0, 196608, 262148, 0, 196608, 262149, 0, 196608 )
+script = ExtResource( 19 )
+
+[node name="CornerMap" parent="PieceManager/StretchMap" instance=ExtResource( 24 )]
+
+[node name="States" type="Node" parent="PieceManager"]
+script = ExtResource( 23 )
+
+[node name="None" type="Node" parent="PieceManager/States"]
+script = ExtResource( 37 )
+
+[node name="Prespawn" type="Node" parent="PieceManager/States"]
+script = ExtResource( 39 )
+
+[node name="MovePiece" type="Node" parent="PieceManager/States"]
+script = ExtResource( 21 )
+
+[node name="Prelock" type="Node" parent="PieceManager/States"]
+script = ExtResource( 20 )
+
+[node name="WaitForPlayfield" type="Node" parent="PieceManager/States"]
+script = ExtResource( 22 )
+
+[node name="GameEnded" type="Node" parent="PieceManager/States"]
+script = ExtResource( 38 )
+
+[node name="Rotate0Sound" type="AudioStreamPlayer" parent="PieceManager"]
+stream = ExtResource( 29 )
+bus = "Reverb Bus"
+
+[node name="Rotate1Sound" type="AudioStreamPlayer" parent="PieceManager"]
+stream = ExtResource( 30 )
+bus = "Reverb Bus"
+
+[node name="MoveSound" type="AudioStreamPlayer" parent="PieceManager"]
+stream = ExtResource( 31 )
+bus = "Reverb Bus"
+
+[node name="LockSound" type="AudioStreamPlayer" parent="PieceManager"]
+stream = ExtResource( 28 )
+bus = "Reverb Bus"
+
+[node name="GameOverSound" type="AudioStreamPlayer" parent="PieceManager"]
+stream = ExtResource( 32 )
+bus = "Reverb Bus"
+
+[node name="GameOverVoice0" type="AudioStreamPlayer" parent="PieceManager"]
+stream = ExtResource( 36 )
+bus = "Voice Bus"
+
+[node name="GameOverVoice1" type="AudioStreamPlayer" parent="PieceManager"]
+stream = ExtResource( 27 )
+bus = "Voice Bus"
+
+[node name="GameOverVoice2" type="AudioStreamPlayer" parent="PieceManager"]
+stream = ExtResource( 33 )
+bus = "Voice Bus"
+
+[node name="GameOverVoice3" type="AudioStreamPlayer" parent="PieceManager"]
+stream = ExtResource( 34 )
+bus = "Voice Bus"
+
+[node name="GameOverVoice4" type="AudioStreamPlayer" parent="PieceManager"]
+stream = ExtResource( 35 )
+bus = "Voice Bus"
+
+[node name="SmushSound" type="AudioStreamPlayer" parent="PieceManager"]
+stream = ExtResource( 26 )
+bus = "Reverb Bus"
 
 [node name="NextPieces" parent="." instance=ExtResource( 4 )]
 margin_left = 688.0
@@ -144,7 +275,7 @@ margin_bottom = 79.0
 size_flags_horizontal = 0
 size_flags_vertical = 0
 custom_fonts/font = ExtResource( 18 )
-shortcut = SubResource( 4 )
+shortcut = SubResource( 3 )
 text = "Back"
 __meta__ = {
 "_edit_use_anchors_": false
@@ -159,7 +290,7 @@ margin_left = -120.0
 margin_top = -248.0
 margin_right = 118.0
 margin_bottom = -70.0
-custom_styles/normal = SubResource( 5 )
+custom_styles/normal = SubResource( 4 )
 custom_fonts/normal_font = ExtResource( 16 )
 text = "Controls
 
@@ -212,6 +343,7 @@ volume_db = -4.0
 bus = "Voice Bus"
 [connection signal="customer_left" from="Playfield" to="." method="_on_Playfield_customer_left"]
 [connection signal="line_cleared" from="Playfield" to="." method="_on_Playfield_line_cleared"]
-[connection signal="game_ended" from="Piece" to="." method="_on_Piece_game_ended"]
+[connection signal="game_ended" from="PieceManager" to="." method="_on_Piece_game_ended"]
+[connection signal="entered_state" from="PieceManager/States" to="PieceManager" method="_on_States_entered_state"]
 [connection signal="pressed" from="HUD/StartGameButton" to="." method="_on_StartGameButton_pressed"]
 [connection signal="pressed" from="HUD/BackButton" to="." method="_on_BackButton_pressed"]

src/main/puzzle/Scenario.tscn
@@ -21,8 +21,11 @@ border_width_right = 2
 border_width_bottom = 2
 border_color = Color( 1, 1, 1, 1 )
 
-[node name="PuzzleScenario" type="Node2D"]
+[node name="PuzzleScenario" type="Control"]
 script = ExtResource( 1 )
+__meta__ = {
+"_edit_use_anchors_": false
+}
 
 [node name="Puzzle" parent="." instance=ExtResource( 3 )]
 

commit 9c6efb59a21018d97218570ea954cfd26e296c70

Fixed piece physics glitches introduced in 56e0cb2.
56e0cb2 introduced a glitch where pieces would lock instantly.
Additionally, I've encountered sporadic problems where pieces might
freeze in place instantly or smush past open gaps. I believe I may have
fixed them, but they're unpredictable so I'm not certain.

gdscript's 'setget' keyword demonstrates unpredictable behavior sometimes. A statement like $States._state.frames = 3600 gets incorrectly interpreted as a 'set_state' call. I've removed the 'setget' keyword from statemachine.gd to avoid these issues.

Removed some unnecessary playfield null checks.

src/main/puzzle/piece/piece-manager.gd
@@ -38,7 +38,7 @@ func _ready() -> void:
 	piece_speed = PieceSpeeds.beginner_level_0
 	$States.set_state($States/Prespawn)
 	# Set the state frames so that the piece spawns immediately
-	$States._state.frames = 3600
+	$States/Prespawn.frames = 3600
 
 
 func get_state() -> State:
@@ -80,7 +80,8 @@ func _physics_process(_delta: float) -> void:
 
 
 func clear_piece() -> void:
-	$States.set_state($States/None)
+	if $States.get_state() != $States/None:
+		$States.set_state($States/None)
 	active_piece = ActivePiece.new(PieceTypes.piece_null)
 	tile_map_dirty = true
 
@@ -273,7 +274,8 @@ func _smush_to_target() -> void:
 	
 	_move_piece_to_target()
 	$SmushSound.play()
-	$States.set_state($States/MovePiece)
+	if $States.get_state() != $States/MovePiece:
+		$States.set_state($States/MovePiece)
 	active_piece.gravity = 0
 	_gravity_delay_frames = PieceSpeeds.SMUSH_FRAMES
 

src/main/puzzle/piece/states/prelock.gd
@@ -9,13 +9,12 @@ func update(piece_manager: PieceManager) -> String:
 	
 	if frames >= piece_manager.piece_speed.post_lock_delay:
 		var active_piece = piece_manager.active_piece
-		if piece_manager.playfield:
-			if piece_manager.playfield.write_piece(active_piece.pos, active_piece.orientation, active_piece.type,
-					piece_manager.piece_speed.line_clear_delay):
-				# line was cleared; different appearance delay
-				active_piece.spawn_delay = piece_manager.piece_speed.line_appearance_delay
-			else:
-				active_piece.spawn_delay = piece_manager.piece_speed.appearance_delay
+		if piece_manager.playfield.write_piece(active_piece.pos, active_piece.orientation, active_piece.type,
+				piece_manager.piece_speed.line_clear_delay):
+			# line was cleared; different appearance delay
+			active_piece.spawn_delay = piece_manager.piece_speed.line_appearance_delay
+		else:
+			active_piece.spawn_delay = piece_manager.piece_speed.appearance_delay
 		active_piece.setType(PieceTypes.piece_null)
 		piece_manager.tile_map_dirty = true
 		new_state_name = "WaitForPlayfield"

src/main/puzzle/piece/states/wait-for-playfield.gd
@@ -6,6 +6,6 @@ a new piece.
 
 func update(piece_manager: PieceManager) -> String:
 	var new_state := ""
-	if piece_manager.playfield == null or piece_manager.playfield.ready_for_new_piece():
+	if piece_manager.playfield.ready_for_new_piece():
 		new_state = "Prespawn"
 	return new_state

src/main/state-machine.gd
@@ -11,7 +11,7 @@ invoking their methods, and emitting signals.
 signal entered_state
 
 # the currently active state
-var _state: State setget set_state, get_state
+var _state: State
 
 # the host object which is passed into each state
 onready var _host := get_parent()

src/main/world/restaurant/CustomerView.tscn
@@ -6,14 +6,6 @@
 [ext_resource path="res://src/main/world/restaurant/customer-view.gd" type="Script" id=4]
 [ext_resource path="res://src/main/world/restaurant/fat-player.gd" type="Script" id=5]
 
-
-
-
-
-
-
-
-
 [sub_resource type="Animation" id=1]
 resource_name = "Fat"
 length = 10.0