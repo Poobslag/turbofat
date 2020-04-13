b6aae81f70b680e59f765d5f720672c4043d03ec
Poobslag <aaron.pieper@gmail.com>
2020-04-12 23:56:01 -0400

    Added version project setting. The old approach didn't work when exported.

--- src/main/ui/version-label.gd
+++ src/main/ui/version-label.gd
@@ -1,6 +1,4 @@
 extends Label
 
 func _ready() -> void:
-	var preset = ConfigFile.new()
-	preset.load("res://export_presets.cfg")
-	text = preset.get_value("preset.0.options", "application/file_version")
+	text = ProjectSettings.get_setting("application/config/version")
