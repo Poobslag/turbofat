Poobslag/turbofat

commit 379b73d439fe1300e46c6f715fb209b00f5fef6f

Pushed Scenario logic into child classes.
The Scenario class previously had logic for resetting the score, initializing the
playfield, loading up the little 'tutorial guy', saving the results, and
other stuff. This has all been pushed into different places. Instead,
scenario.gd manages loading/saving of scenario files (previously handled
by ScenarioLibrary)

PuzzleScore now handles the 'puzzle lifecycle' which was previously
managed by puzzle. It prepares a puzzle, pauses and starts the puzzle. It
calculates when the player wins or loses and emits appropriate signals.

Global.scenario_settings is now Scenario.settings. A new
Scenario.settings_changed signal gives classes a logical place to
initialize themselves if they have scenario-dependent logic, such as the
PieceQueue which needs to change its pieces if the scenario uses a
nonstandard piece set.

Moved puzzle-related scripts and assets from /puzzle/scenario into
/puzzle. /puzzle/scenario now only includes scripts and assets related to
saving, loading, and maintaining scenario settings and scenario history.

Moved tutorial-related scripts and assets from /puzzle into
/puzzle/tutorial.

Extracted start-end-sfx.gd from Puzzle class.

src/main/puzzle/puzzle.gd
@@ -5,32 +5,26 @@ Represents a minimal puzzle game with a piece, playfield of pieces, and next pie
 class to add goals, win conditions, challenges or time limits.
 """
 
-# emitted a few seconds after the game ends, for displaying messages
-signal after_game_ended
-
 signal topped_out
 
-onready var _go_voices := [$GoVoice0, $GoVoice1, $GoVoice2]
-
 func _ready() -> void:
-	PuzzleScore.connect("combo_ended", self, "_on_PuzzleScore_combo_ended")
+	PuzzleScore.reset() # erase any lines/score from previous games
+	PuzzleScore.connect("game_ended", self, "_on_PuzzleScore_game_ended")
 	$Playfield/TileMapClip/TileMap/Viewport/ShadowMap.piece_tile_map = $PieceManager/TileMap
 	
 	for i in range(3):
 		$CreatureView.summon_creature(i)
-
-
-func _input(event: InputEvent) -> void:
-	if PuzzleScore.game_active and event.is_action_pressed("ui_cancel"):
-		$TopOutTracker.make_player_lose()
+	
+	if Scenario.settings.other.tutorial:
+		Global.creature_queue.push_front({
+			"line_rgb": "6c4331", "body_rgb": "a854cb", "eye_rgb": "4fa94e dbe28e", "horn_rgb": "f1e398",
+			"ear": "2", "horn": "0", "mouth": "1", "eye": "1"
+		})
+	$CreatureView.summon_creature()
 
 
 func get_playfield() -> Playfield:
 	return $Playfield as Playfield
-
-
-func get_next_piece_displays() -> NextPieceDisplays:
-	return $NextPieceDisplays as NextPieceDisplays
 
 
 func get_piece_manager() -> PieceManager:
@@ -43,85 +37,6 @@ func hide_start_button() -> void:
 
 func show_start_button() -> void:
 	$HudHolder/Hud.show_start_button()
-
-
-"""
-Shows a detailed multi-line message, like how the game is controlled
-"""
-func show_detail_message(text: String) -> void:
-	$HudHolder/Hud.show_detail_message(text)
-
-
-"""
-Shows a succinct single-line message, like 'Game Over'
-"""
-func show_message(text: String) -> void:
-	$HudHolder/Hud.show_message(text)
-
-
-func start_game() -> void:
-	PuzzleScore.prepare_game()
-	show_message("3")
-	$ReadySound.play()
-	yield(get_tree().create_timer(0.8), "timeout")
-	show_message("2")
-	$ReadySound.play()
-	yield(get_tree().create_timer(0.8), "timeout")
-	show_message("1")
-	$ReadySound.play()
-	yield(get_tree().create_timer(0.8), "timeout")
-	$GoSound.play()
-	_go_voices[randi() % _go_voices.size()].play()
-	PuzzleScore.start_game()
-
-
-"""
-Ends the game. This occurs when the player loses, wins, or runs out of time.
-"""
-func end_game() -> void:
-	var delay: int = 2.2
-	var message: String
-	var sound: AudioStreamPlayer
-	if PuzzleScore.scenario_performance.lost:
-		sound = $GameOverSound
-		message = "Game over"
-	elif not PuzzleScore.milestone_met(Global.scenario_settings.success_condition):
-		sound = $MatchEndSound
-		message = "Finish!"
-	else:
-		sound = $ExcellentSound
-		message = "You win!"
-		delay = 4.2
-	
-	sound.play()
-	PuzzleScore.end_game()
-	show_message(message)
-	yield(get_tree().create_timer(delay), "timeout")
-	emit_signal("after_game_ended")
-
-
-func get_milestone_hud() -> MilestoneHud:
-	return $Chalkboard/MilestoneHud as MilestoneHud
-
-
-func set_chalkboard_visible(visible: bool) -> void:
-	$Chalkboard.visible = visible
-
-
-func clear_playfield() -> void:
-	$Playfield.clear()
-
-
-func set_block(pos: Vector2, tile: int, autotile_coord: Vector2 = Vector2.ZERO) -> void:
-	$Playfield.set_block(pos, tile, autotile_coord)
-
-
-func set_piece_types(types: Array) -> void:
-	$NextPieceDisplays.set_piece_types(types)
-
-
-func set_piece_start_types(types: Array) -> void:
-	$NextPieceDisplays.set_piece_start_types(types)
 
 
 func scroll_to_new_creature() -> void:
@@ -145,14 +60,14 @@ func _feed_creature(fatness_pct: float) -> void:
 	if PuzzleScore.game_active:
 		var old_fatness: float = $CreatureView.get_fatness()
 		var target_fatness := sqrt(1 + PuzzleScore.get_creature_score() / 50.0)
-		if Global.scenario_settings.other.tutorial:
+		if Scenario.settings.other.tutorial:
 			# make them a tiny amount fatter, so that they'll change when a new level is started
 			target_fatness = min(target_fatness, 1.001)
 		$CreatureView.set_fatness(lerp(old_fatness, target_fatness, fatness_pct))
 
 
 func _on_Hud_start_button_pressed() -> void:
-	start_game()
+	PuzzleScore.prepare_and_start_game()
 
 
 """
@@ -174,7 +89,20 @@ func _on_Playfield_line_cleared(y: int, total_lines: int, remaining_lines: int, 
 		$CreatureView/SceneClip/CreatureSwitcher/Scene.play_combo_voice()
 
 
-func _on_PuzzleScore_combo_ended() -> void:
-	if PuzzleScore.game_active and not Global.scenario_settings.other.tutorial:
-		$CreatureView.play_goodbye_voice()
-		$CreatureView.scroll_to_new_creature()
+"""
+Method invoked when the game ends. Stores the rank result for later.
+"""
+func _on_PuzzleScore_game_ended() -> void:
+	# ensure score is up to date before calculating rank
+	PuzzleScore.end_combo()
+	var rank_result := RankCalculator.new().calculate_rank()
+	PlayerData.scenario_history.add(Scenario.launched_scenario_name, rank_result)
+	PlayerData.scenario_history.prune(Scenario.launched_scenario_name)
+	PlayerData.money += rank_result.score
+	PlayerSave.save_player_data()
+	
+	match Scenario.settings.finish_condition.type:
+		Milestone.SCORE:
+			if not PuzzleScore.scenario_performance.lost and rank_result.seconds_rank < 24: $ApplauseSound.play()
+		_:
+			if not PuzzleScore.scenario_performance.lost and rank_result.score_rank < 24: $ApplauseSound.play()

commit 6713bf6dfc577e8a425abb30ddc4641898e09379

'First customer' fix. Move door chime. creature_def change.
Fixed regression introduced in 379b73d4 (pushing Scenario logic into child
classes) where scenarios no longer featured the overworld creature you
spoke with. That creature was ousted from the restaurant because we
summmoned a fourth creature.

Moved door chime from Creature into RestaurantScene. This makes sense
thematically but also keeps our code cleaner.

Resolved 'resumed after yield, but class instance is gone' errors when
playing door chimes. This was caused because the door chime logic was all
in one function with yield statements. It's now handled by timers.

Creature's 'creature_def' attribute is now a metadata property, instead of
a function. This is the same as how chat_theme_def is handled.

src/demo/world/restaurant/creature-view-demo.gd
@@ -23,7 +23,7 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	match Utils.key_scancode(event):
 		KEY_F: _creature().feed()
-		KEY_D: _creature().get_node("CreatureSfx").play_door_chime(0)
+		KEY_D: _creature().get_node("CreatureSfx").play_door_chime()
 		KEY_V:
 			Global.greetiness = 2
 			_creature().get_node("CreatureSfx").play_goodbye_voice()

src/main/puzzle/puzzle.gd
@@ -23,7 +23,9 @@ func _ready() -> void:
 			"line_rgb": "6c4331", "body_rgb": "a854cb", "eye_rgb": "4fa94e dbe28e", "horn_rgb": "f1e398",
 			"ear": "3", "horn": "0", "mouth": "2", "eye": "2"
 		})
-	$CreatureView.summon_creature()
+		$CreatureView.summon_creature()
+	
+	$CreatureView.get_creature().play_hello_voice(true)
 
 
 func get_playfield() -> Playfield:

src/main/puzzle/scenario/scenario.gd
@@ -63,5 +63,5 @@ func push_scenario_trail(scenario_settings: ScenarioSettings, creature_def: Dict
 	Scenario.settings = scenario_settings
 	PuzzleScore.reset()
 	Scenario.launched_scenario_name = scenario_settings.name
-	Global.creature_queue.push_back(creature_def)
+	Global.creature_queue.push_front(creature_def)
 	Breadcrumb.push_trail("res://src/main/puzzle/Puzzle.tscn")

src/main/ui/overworld-ui.gd
@@ -121,8 +121,8 @@ func _on_ChatUi_chat_event_played(chat_event: ChatEvent) -> void:
 				scenario = StringUtils.substring_after(meta_item, "scenario-")
 		if scenario:
 			_launched_scenario = Scenario.load_scenario_from_name(scenario)
-			if chatters[0].has_method("get_creature_def"):
-				_scenario_creature_def = chatters[0].get_creature_def()
+			if chatters[0].has_meta("creature_def"):
+				_scenario_creature_def = chatters[0].get_meta("creature_def")
 
 
 func _on_ChatUi_showed_choices() -> void:

src/main/world/boatricia.gd
@@ -8,10 +8,8 @@ func _ready() -> void:
 	set_meta("chat_name", "Boatricia")
 	set_meta("chat_theme_def", {"accent_scale":0.33, "accent_swapped": true,
 			"accent_texture": 1, "color": "0b45a6", "dark": true})
-
-"""
-Boatricia is dark blue with short stubby horns.
-"""
-func get_creature_def() -> Dictionary:
-	return {"line_rgb": "41281e", "body_rgb": "0b45a6", "eye_rgb": "fad541 ffffff", "horn_rgb": "282828",
-		"ear": "2", "horn": "0", "mouth": "1", "eye": "3"}
+	
+	# Boatricia is dark blue with short stubby horns.
+	set_creature_def({
+		"line_rgb": "41281e", "body_rgb": "0b45a6", "eye_rgb": "fad541 ffffff", "horn_rgb": "282828",
+		"ear": "2", "horn": "0", "mouth": "1", "eye": "3"})

src/main/world/bort.gd
@@ -6,14 +6,10 @@ Script for showing a creature 'Bort' in the 3D overworld.
 func _ready() -> void:
 	set_meta("chat_id", "bort")
 	set_meta("chat_name", "Bort")
-	set_meta("chat_theme_def", {"accent_scale":1.3,"accent_swapped":false,"accent_texture":0,"color":"6f83db"})
-
-
-"""
-Bort is light blue with a tentacle mouth.
-"""
-func get_creature_def() -> Dictionary:
-	return {
+	set_meta("chat_theme_def", {"accent_scale": 1.3, "accent_swapped": false,
+		"accent_texture": 0, "color": "6f83db"})
+	
+	# Bort is light blue with a tentacle mouth.
+	set_creature_def({
 		"line_rgb": "6c4331", "body_rgb": "6f83db", "eye_rgb": "374265 eaf2f4", "horn_rgb": "f1e398",
-		"ear": "3", "horn": "1", "mouth": "2", "eye": "1"
-	}
+		"ear": "3", "horn": "1", "mouth": "2", "eye": "1"})

src/main/world/creature/Creature.tscn
@@ -2733,17 +2733,18 @@ position = Vector2( 268.587, 210.527 )
 max_distance = 4000.0
 bus = "Voice Bus"
 
-[node name="DoorChime" type="AudioStreamPlayer2D" parent="CreatureSfx"]
-position = Vector2( 108.587, 210.527 )
-volume_db = -4.0
-max_distance = 4000.0
-bus = "Sound Bus"
-
 [node name="Voice" type="AudioStreamPlayer2D" parent="CreatureSfx"]
 position = Vector2( 268.587, 210.527 )
 volume_db = 6.0
 max_distance = 4000.0
 bus = "Voice Bus"
+
+[node name="HelloTimer" type="Timer" parent="CreatureSfx"]
+one_shot = true
+
+[node name="SuppressSfxTimer" type="Timer" parent="CreatureSfx"]
+one_shot = true
+autostart = true
 [connection signal="before_creature_arrived" from="." to="Mouth0Anims" method="_on_Creature_before_creature_arrived"]
 [connection signal="before_creature_arrived" from="." to="Mouth1Anims" method="_on_Creature_before_creature_arrived"]
 [connection signal="before_creature_arrived" from="." to="EmoteAnims" method="_on_Creature_before_creature_arrived"]
@@ -2752,28 +2753,29 @@ bus = "Voice Bus"
 [connection signal="movement_mode_changed" from="." to="Sprites/Body" method="_on_Creature_movement_mode_changed"]
 [connection signal="movement_mode_changed" from="." to="Sprites/FarLeg" method="_on_Creature_movement_mode_changed"]
 [connection signal="movement_mode_changed" from="." to="Sprites/FarArm" method="_on_Creature_movement_mode_changed"]
-[connection signal="movement_mode_changed" from="." to="Sprites/Neck0/HeadBobber/Head" method="_on_Creature_movement_mode_changed"]
-[connection signal="movement_mode_changed" from="." to="Sprites/NearArm" method="_on_Creature_movement_mode_changed"]
-[connection signal="movement_mode_changed" from="." to="Sprites/Neck0/HeadBobber/EarZ1" method="_on_Creature_movement_mode_changed"]
-[connection signal="movement_mode_changed" from="." to="Sprites/Neck0/HeadBobber/HornZ1" method="_on_Creature_movement_mode_changed"]
 [connection signal="movement_mode_changed" from="." to="Sprites/Body/NeckBlend" method="_on_Creature_movement_mode_changed"]
-[connection signal="movement_mode_changed" from="." to="Sprites/Neck0/HeadBobber/EarZ2" method="_on_Creature_movement_mode_changed"]
-[connection signal="movement_mode_changed" from="." to="Sprites/NearLeg" method="_on_Creature_movement_mode_changed"]
 [connection signal="movement_mode_changed" from="." to="Sprites/Neck0/HeadBobber/HornZ0" method="_on_Creature_movement_mode_changed"]
+[connection signal="movement_mode_changed" from="." to="Sprites/Neck0/HeadBobber/Head" method="_on_Creature_movement_mode_changed"]
+[connection signal="movement_mode_changed" from="." to="Sprites/Neck0/HeadBobber/EarZ1" method="_on_Creature_movement_mode_changed"]
+[connection signal="movement_mode_changed" from="." to="Sprites/NearArm" method="_on_Creature_movement_mode_changed"]
+[connection signal="movement_mode_changed" from="." to="Sprites/Neck0/HeadBobber/EarZ2" method="_on_Creature_movement_mode_changed"]
 [connection signal="movement_mode_changed" from="." to="Sprites/Neck0/HeadBobber/EarZ0" method="_on_Creature_movement_mode_changed"]
+[connection signal="movement_mode_changed" from="." to="Sprites/NearLeg" method="_on_Creature_movement_mode_changed"]
+[connection signal="movement_mode_changed" from="." to="Sprites/Neck0/HeadBobber/HornZ1" method="_on_Creature_movement_mode_changed"]
 [connection signal="orientation_changed" from="." to="Sprites/FarLeg" method="_on_Creature_orientation_changed"]
 [connection signal="orientation_changed" from="." to="Sprites/FarArm" method="_on_Creature_orientation_changed"]
-[connection signal="orientation_changed" from="." to="Sprites/Neck0/HeadBobber/Head" method="_on_Creature_orientation_changed"]
-[connection signal="orientation_changed" from="." to="Sprites/NearArm" method="_on_Creature_orientation_changed"]
-[connection signal="orientation_changed" from="." to="Sprites/Neck0/HeadBobber/EarZ1" method="_on_Creature_orientation_changed"]
-[connection signal="orientation_changed" from="." to="Sprites/Neck0/HeadBobber/HornZ1" method="_on_Creature_orientation_changed"]
 [connection signal="orientation_changed" from="." to="Sprites/Body/NeckBlend" method="_on_Creature_orientation_changed"]
-[connection signal="orientation_changed" from="." to="Sprites/Neck0/HeadBobber/EarZ2" method="_on_Creature_orientation_changed"]
-[connection signal="orientation_changed" from="." to="Sprites/NearLeg" method="_on_Creature_orientation_changed"]
 [connection signal="orientation_changed" from="." to="Sprites/Neck0/HeadBobber/HornZ0" method="_on_Creature_orientation_changed"]
+[connection signal="orientation_changed" from="." to="Sprites/Neck0/HeadBobber/Head" method="_on_Creature_orientation_changed"]
+[connection signal="orientation_changed" from="." to="Sprites/Neck0/HeadBobber/EarZ1" method="_on_Creature_orientation_changed"]
+[connection signal="orientation_changed" from="." to="Sprites/NearArm" method="_on_Creature_orientation_changed"]
+[connection signal="orientation_changed" from="." to="Sprites/Neck0/HeadBobber/EarZ2" method="_on_Creature_orientation_changed"]
 [connection signal="orientation_changed" from="." to="Sprites/Neck0/HeadBobber/EarZ0" method="_on_Creature_orientation_changed"]
+[connection signal="orientation_changed" from="." to="Sprites/NearLeg" method="_on_Creature_orientation_changed"]
+[connection signal="orientation_changed" from="." to="Sprites/Neck0/HeadBobber/HornZ1" method="_on_Creature_orientation_changed"]
 [connection signal="orientation_changed" from="." to="Mouth0Anims" method="_on_Creature_orientation_changed"]
 [connection signal="orientation_changed" from="." to="Mouth1Anims" method="_on_Creature_orientation_changed"]
 [connection signal="animation_finished" from="EmoteAnims" to="EmoteAnims" method="_on_animation_finished"]
 [connection signal="before_mood_switched" from="EmoteAnims" to="." method="_on_EmoteAnims_before_mood_switched"]
 [connection signal="animation_started" from="MovementAnims" to="." method="_on_MovementAnims_animation_started"]
+[connection signal="timeout" from="CreatureSfx/HelloTimer" to="CreatureSfx" method="_on_HelloTimer_timeout"]

src/main/world/creature/creature-3d.gd
@@ -11,16 +11,11 @@ func _ready() -> void:
 	$CollisionShape.disabled = true
 	$ShadowMesh.visible = false
 	$Viewport/Creature/Sprites/Shadow.visible = false
-	var creature_def := get_creature_def()
-	if creature_def:
-		$Viewport/Creature.summon(creature_def)
 
 
-"""
-Subclasses can override this method to define the creature's appearance.
-"""
-func get_creature_def() -> Dictionary:
-	return {}
+func set_creature_def(creature_def: Dictionary) -> void:
+	set_meta("creature_def", creature_def)
+	$Viewport/Creature.summon(creature_def)
 
 
 """

src/main/world/creature/creature-sfx.gd
@@ -3,24 +3,12 @@ extends Node
 Plays creature-related sound effects.
 """
 
-# delays between when creature arrives and when door chime is played (in seconds)
-const CHIME_DELAYS: Array = [0.2, 0.3, 0.5, 1.0, 1.5]
-
 # sounds the creatures make when they enter the restaurant
 onready var hello_voices := [
 	preload("res://assets/main/world/creature/hello-voice-0.wav"),
 	preload("res://assets/main/world/creature/hello-voice-1.wav"),
 	preload("res://assets/main/world/creature/hello-voice-2.wav"),
 	preload("res://assets/main/world/creature/hello-voice-3.wav"),
-]
-
-# sounds which get played when a creature shows up
-onready var _chime_sounds := [
-	preload("res://assets/main/world/door-chime0.wav"),
-	preload("res://assets/main/world/door-chime1.wav"),
-	preload("res://assets/main/world/door-chime2.wav"),
-	preload("res://assets/main/world/door-chime3.wav"),
-	preload("res://assets/main/world/door-chime4.wav"),
 ]
 
 # sounds which get played when the creature eats
@@ -64,22 +52,8 @@ onready var _goodbye_voices := [
 	preload("res://assets/main/world/creature/goodbye-voice-3.wav"),
 ]
 
-# we suppress the first door chime. we usually cheat and play the chime after the creature appears, but that doesn't
-# work when we can see them appear
-var _suppress_one_chime := true
-
 # index of the most recent combo sound that was played
 var _combo_voice_index := 0
-
-"""
-Sets the relative position of sound effects related to the restaurant door. Each seat has a different position
-relative to the restaurant's entrance; some are close to the door, some are far away.
-
-Parameter: 'position' is the position of the door relative to this seat, in world coordinates.
-"""
-func set_door_sound_position(door_sound_position: Vector2) -> void:
-	$DoorChime.position = door_sound_position
-
 
 """
 Plays a 'mmm!' voice sample, for when a player builds a big combo.
@@ -93,8 +67,8 @@ func play_combo_voice() -> void:
 """
 Plays a 'hello!' voice sample, for when a creature enters the restaurant
 """
-func play_hello_voice() -> void:
-	if Global.should_chat():
+func play_hello_voice(force: bool = false) -> void:
+	if Global.should_chat() or force:
 		$Voice.stream = hello_voices[randi() % hello_voices.size()]
 		$Voice.play()
 
@@ -102,34 +76,10 @@ func play_hello_voice() -> void:
 """
 Plays a 'check please!' voice sample, for when a creature is ready to leave
 """
-func play_goodbye_voice() -> void:
-	if Global.should_chat():
+func play_goodbye_voice(force: bool = false) -> void:
+	if Global.should_chat() or force:
 		$Voice.stream = _goodbye_voices[randi() % _goodbye_voices.size()]
 		$Voice.play()
-
-
-"""
-Plays a door chime sound effect, for when a creature enters the restaurant.
-
-Parameter: 'delay' is the delay in seconds before the chime sound plays. The default value of '-1' results in a random
-	delay.
-"""
-func play_door_chime(delay: float = -1) -> void:
-	if Scenario.settings.other.tutorial:
-		# suppress door chime for tutorials
-		return
-	
-	if delay < 0:
-		delay = CHIME_DELAYS[randi() % CHIME_DELAYS.size()]
-	yield(get_tree().create_timer(delay), "timeout")
-	
-	if is_inside_tree():
-		$DoorChime.stream = _chime_sounds[randi() % _chime_sounds.size()]
-		$DoorChime.play()
-		yield(get_tree().create_timer(0.5), "timeout")
-	
-	if is_inside_tree():
-		play_hello_voice()
 
 
 """
@@ -147,9 +97,13 @@ func _on_Creature_food_eaten() -> void:
 func _on_Creature_creature_arrived() -> void:
 	if Engine.is_editor_hint():
 		# Skip the sound effects if we're using this as an editor tool
-		pass
-	else:
-		if _suppress_one_chime:
-			_suppress_one_chime = false
-		else:
-			play_door_chime()
+		return
+	if not $SuppressSfxTimer.is_stopped():
+		# suppress greeting for first few seconds of a level
+		return
+	
+	$HelloTimer.start()
+
+
+func _on_HelloTimer_timeout() -> void:
+	play_hello_voice()

src/main/world/creature/creature.gd
@@ -159,10 +159,6 @@ func set_orientation(new_orientation: int) -> void:
 	emit_signal("orientation_changed", orientation)
 
 
-func set_door_sound_position(door_sound_position: Vector2) -> void:
-	$CreatureSfx.set_door_sound_position(door_sound_position)
-
-
 """
 If you make Creature a tool and play with the 'creature_preset' editor setting, you can view a creature in the editor.
 
@@ -270,6 +266,13 @@ func show_food_effects(delay := 0.0) -> void:
 
 
 """
+Plays a 'hello!' voice sample, for when a creature enters the restaurant
+"""
+func play_hello_voice(force: bool = false) -> void:
+	$CreatureSfx.play_hello_voice(force)
+
+
+"""
 Plays a 'mmm!' voice sample, for when a player builds a big combo.
 """
 func play_combo_voice() -> void:
@@ -279,8 +282,8 @@ func play_combo_voice() -> void:
 """
 Plays a 'check please!' voice sample, for when a creature is ready to leave
 """
-func play_goodbye_voice() -> void:
-	$CreatureSfx.play_goodbye_voice()
+func play_goodbye_voice(force: bool = false) -> void:
+	$CreatureSfx.play_goodbye_voice(force)
 
 
 """

src/main/world/ebe.gd
@@ -7,12 +7,8 @@ func _ready() -> void:
 	set_meta("chat_id", "ebe")
 	set_meta("chat_name", "Ebe")
 	set_meta("chat_theme_def", {"accent_scale":0.87,"accent_swapped":true,"accent_texture":15,"color":"b47922"})
-
-"""
-Ebe is brown with a beak mouth.
-"""
-func get_creature_def() -> Dictionary:
-	return {
+	
+	# Ebe is brown with a beak mouth.
+	set_creature_def({
 		"line_rgb": "6c4331", "body_rgb": "b47922", "eye_rgb": "7d4c21 e5cd7d", "horn_rgb": "f1e398",
-		"ear": "1", "horn": "2", "mouth": "1", "eye": "2"
-	}
+		"ear": "1", "horn": "2", "mouth": "1", "eye": "2"})

src/main/world/restaurant/CreatureView.tscn
@@ -6,7 +6,6 @@
 [ext_resource path="res://src/main/world/restaurant/creature-view.gd" type="Script" id=4]
 [ext_resource path="res://src/main/world/creature/fat-player.gd" type="Script" id=5]
 
-
 [sub_resource type="Animation" id=1]
 resource_name = "fat"
 length = 10.0

src/main/world/restaurant/RestaurantScene.tscn
@@ -1,7 +1,8 @@
-[gd_scene load_steps=12 format=2]
+[gd_scene load_steps=13 format=2]
 
 [ext_resource path="res://src/main/world/creature/Creature.tscn" type="PackedScene" id=1]
 [ext_resource path="res://src/main/world/restaurant/Seat.tscn" type="PackedScene" id=2]
+[ext_resource path="res://src/main/world/restaurant/door-chime.gd" type="Script" id=3]
 [ext_resource path="res://assets/main/world/restaurant/wall-metal.png" type="Texture" id=5]
 [ext_resource path="res://assets/main/world/restaurant/wall-metal-shadow.png" type="Texture" id=7]
 [ext_resource path="res://assets/main/world/shadow-oval-2.png" type="Texture" id=11]
@@ -11,7 +12,6 @@
 [ext_resource path="res://assets/main/world/restaurant/poster1.png" type="Texture" id=15]
 [ext_resource path="res://assets/main/world/restaurant/poster0.png" type="Texture" id=16]
 [ext_resource path="res://src/main/world/restaurant/restaurant-scene.gd" type="Script" id=24]
-
 
 [node name="RestaurantScene" type="Node2D"]
 light_mask = 2
@@ -169,6 +169,25 @@ z_index = 20
 
 [node name="Seat3" parent="." instance=ExtResource( 2 )]
 position = Vector2( 2380, 585 )
+
+[node name="DoorChime" type="AudioStreamPlayer2D" parent="."]
+position = Vector2( -120, 200 )
+volume_db = -4.0
+max_distance = 4000.0
+bus = "Sound Bus"
+script = ExtResource( 3 )
+
+[node name="ChimeTimer" type="Timer" parent="DoorChime"]
+wait_time = 0.5
+one_shot = true
+
+[node name="SuppressSfxTimer" type="Timer" parent="DoorChime"]
+one_shot = true
+autostart = true
+[connection signal="creature_arrived" from="Creature1" to="DoorChime" method="_on_Creature_creature_arrived"]
 [connection signal="food_eaten" from="Creature1" to="." method="_on_Creature_food_eaten"]
+[connection signal="creature_arrived" from="Creature2" to="DoorChime" method="_on_Creature_creature_arrived"]
 [connection signal="food_eaten" from="Creature2" to="." method="_on_Creature_food_eaten"]
+[connection signal="creature_arrived" from="Creature3" to="DoorChime" method="_on_Creature_creature_arrived"]
 [connection signal="food_eaten" from="Creature3" to="." method="_on_Creature_food_eaten"]
+[connection signal="timeout" from="DoorChime/ChimeTimer" to="DoorChime" method="_on_ChimeTimer_timeout"]

src/main/world/restaurant/door-chime.gd
@@ -1,0 +1,25 @@
+extends AudioStreamPlayer2D
+"""
+Door chime which rings when a customer enters the restaurant.
+"""
+
+# sounds which get played when a creature shows up
+onready var _chime_sounds := [
+	preload("res://assets/main/world/door-chime0.wav"),
+	preload("res://assets/main/world/door-chime1.wav"),
+	preload("res://assets/main/world/door-chime2.wav"),
+	preload("res://assets/main/world/door-chime3.wav"),
+	preload("res://assets/main/world/door-chime4.wav"),
+]
+
+func _on_Creature_creature_arrived() -> void:
+	if not $SuppressSfxTimer.is_stopped():
+		# suppress door chime at the start of a scenario
+		return
+	
+	$ChimeTimer.start()
+
+
+func _on_ChimeTimer_timeout() -> void:
+	stream = _chime_sounds[randi() % _chime_sounds.size()]
+	play()

src/main/world/restaurant/restaurant-scene.gd
@@ -21,7 +21,6 @@ onready var _creatures := [$Creature1, $Creature2, $Creature3]
 func _ready() -> void:
 	for i in range(3):
 		_get_seat(i).set_creature(_creatures[i])
-		_get_seat(i).set_door_sound_position(Vector2(-500 - 1000 * i, 0))
 		_get_seat(i).refresh()
 
 

src/main/world/restaurant/seat.gd
@@ -7,7 +7,6 @@ This script contains logic for scaling an injected 'creature shadow' object as t
 """
 
 var _creature: Creature setget set_creature
-var _door_sound_position: Vector2
 
 func set_creature(creature: Creature) -> void:
 	_creature = creature
@@ -21,18 +20,6 @@ func refresh() -> void:
 	if _creature and _creature.is_visible_in_tree():
 		# draw a shadow on the creature's stool
 		$Stool0L.texture = preload("res://assets/main/world/restaurant/stool-occupied.png")
-		_creature.set_door_sound_position(_door_sound_position)
 	else:
 		# remove the shadow from the creature's stool
 		$Stool0L.texture = preload("res://assets/main/world/restaurant/stool.png")
-
-
-"""
-Sets the relative position of sound effects related to the restaurant door. Each seat has a different position
-relative to the restaurant's entrance; some are close to the door, some are far away.
-
-Parameter: 'position' is the position of the door relative to this seat, in world coordinates.
-"""
-func set_door_sound_position(door_sound_position: Vector2) -> void:
-	_door_sound_position = door_sound_position
-	refresh()

src/main/world/spira.gd
@@ -75,6 +75,11 @@ func _ready() -> void:
 	._ready()
 	$CollisionShape.disabled = false
 	ChattableManager.set_spira(self)
+	
+	# Spira is dark red with black eyes.
+	set_creature_def({
+		"line_rgb": "6c4331", "body_rgb": "b23823", "eye_rgb": "282828 dedede", "horn_rgb": "f1e398",
+		"ear": "1", "horn": "1", "mouth": "2", "eye": "1"})
 
 
 func _physics_process(delta) -> void:
@@ -117,16 +122,6 @@ func _unhandled_input(_event: InputEvent) -> void:
 			# only start the jump buffer when pressing the button, not when holding it
 			$JumpBuffer.start()
 		get_tree().set_input_as_handled()
-
-
-"""
-Spira is dark red with black eyes.
-"""
-func get_creature_def() -> Dictionary:
-	return {
-		"line_rgb": "6c4331", "body_rgb": "b23823", "eye_rgb": "282828 dedede", "horn_rgb": "f1e398",
-		"ear": "1", "horn": "1", "mouth": "2", "eye": "1"
-	}
 
 
 """