eafc1cd93f193be0b116de6b49a1abcaf56b3d2b
Poobslag <aaron.pieper@gmail.com>
2020-06-18 09:18:29 -0400

    Added android export. export_presets.cfg is now generated via a script.
    export_presets.cfg include keystore passwords, so it cannot be in version
    control. These passwords are now generated via a shell script. This shell
    script also updates the version numbers for the project. It should be run
    before a release.
    
    Developers who wish to export the project will need to run a shell script
    to generate this export_presets.cfg file. They will also need to maintain
    a /secrets/ folder which contains keystores and keystore passwords. This
    process will be documented in the wiki.

---
+++ generate_export_presets.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+################################################################################
+# This script generates our export_presets.cfg and project.godot files. It
+# updates version numbers and sensitive properties which cannot be kept in
+# version control.
+#
+# Further info is available in the Turbo Fat wiki:
+# https://github.com/Poobslag/turbofat/wiki/release-process
+
+
+# Calculate version string
+yy=$(date +%Y)
+mmdd=$(date +%m%d)
+mmdd=$(echo "$mmdd" | sed 's/^0*//')
+version=$(((yy - 2020) * 2000 + mmdd))
+version=$(printf 0.%04d $version)
+echo "version=$version"
+
+# Load sensitive information from secrets files
+. secrets/android.properties
+
+# Update export presets
+cp export_presets.cfg.template export_presets.cfg
+sed -i "s/##VERSION##/$version/g" export_presets.cfg
+sed -i "s/##ANDROID_KEYSTORE_RELEASE_PASSWORD##/$ANDROID_KEYSTORE_RELEASE_PASSWORD/g" export_presets.cfg
+echo "Updated export_presets.cfg"
+
+# Update project.godot
+sed -i "s/^config\/version=\".*\"$/config\/version=\"$version\"/g" project.godot
+echo "Updated project.godot"
