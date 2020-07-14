e7aae9db1f25ca2f83ee72482c1dda6ea3d4eb08
Poobslag <aaron.pieper@gmail.com>
2020-07-14 14:24:34 -0400

    Creatures look happy if you feed them five times.
    When you feed them, their head starts bouncing, they blush and have a
    little heart bubble over their heads.
    
    Added new PuzzleScore.creature_line_clears property for creature comfort.

--- project/src/demo/world/restaurant/creature-view-demo.gd
+++ project/src/demo/world/restaurant/creature-view-demo.gd
@@ -7,6 +7,7 @@ Keys:
 	[D]: Ring the doorbell
 	[V]: Say something
 	[1-9,0]: Change the creature's size from 10% to 100%
+	SHIFT+[1-9,0]: Change the creature's comfort from 0.0 -> 1.0 -> -1.0
 	[Q,W,E]: Switch to the 1st, 2nd or 3rd creature.
 	arrows: Change the creature's orientation
 	brace keys: Change the creature's appearance
@@ -24,7 +25,22 @@ func _input(event: InputEvent) -> void:
 			Global.greetiness = 2
 			_creature_2d().get_node("CreatureSfx").play_goodbye_voice()
 		KEY_0, KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9:
-			$CreatureView.get_creature_2d().set_fatness(FATNESS_KEYS[Utils.key_num(event)])
+			if Input.is_key_pressed(KEY_SHIFT):
+				# shift pressed; change creature's comfort
+				match Utils.key_scancode(event):
+					KEY_1: $CreatureView.get_creature_2d().set_comfort(0.00) # hasn't eaten
+					KEY_2: $CreatureView.get_creature_2d().set_comfort(0.30)
+					KEY_3: $CreatureView.get_creature_2d().set_comfort(0.60)
+					KEY_4: $CreatureView.get_creature_2d().set_comfort(1.00) # ate enough
+					KEY_5: $CreatureView.get_creature_2d().set_comfort(-0.10) # starting to overeat
+					KEY_6: $CreatureView.get_creature_2d().set_comfort(-0.30)
+					KEY_7: $CreatureView.get_creature_2d().set_comfort(-0.50) # ate too much
+					KEY_8: $CreatureView.get_creature_2d().set_comfort(-0.70)
+					KEY_9: $CreatureView.get_creature_2d().set_comfort(-0.90)
+					KEY_0: $CreatureView.get_creature_2d().set_comfort(-1.00) # ate way too much
+			else:
+				# shift not pressed; change creature's fatness
+				$CreatureView.get_creature_2d().set_fatness(FATNESS_KEYS[Utils.key_num(event)])
 		KEY_Q: $CreatureView.set_current_creature_index(0)
 		KEY_W: $CreatureView.set_current_creature_index(1)
 		KEY_E: $CreatureView.set_current_creature_index(2)
--- project/src/main/puzzle/puzzle-score.gd
+++ project/src/main/puzzle/puzzle-score.gd
@@ -46,6 +46,9 @@ var scenario_performance := PuzzlePerformance.new()
 # The scores for each creature in the current scenario (bonuses and line clears)
 var creature_scores := [0]
 
+# The number of line clears for each creature in the current scenario
+var creature_line_clears := [0]
+
 # Bonus points awarded during the current combo. This only includes bonuses
 # and should be a round number like +55 or +130 for visual aesthetics.
 var bonus_score := 0
@@ -126,6 +129,7 @@ func add_line_score(combo_score: int, box_score: int) -> void:
 		scenario_performance.leftover_score += combo_score + box_score + 1
 	
 	_add_creature_score(1 + combo_score + box_score)
+	_add_creature_line_clear()
 	_add_bonus_score(combo_score + box_score)
 	_add_score(1)
 
@@ -144,6 +148,7 @@ func end_combo() -> void:
 		pass
 	else:
 		creature_scores.append(0)
+		creature_line_clears.append(0)
 	
 	_add_score(bonus_score)
 	bonus_score = 0
@@ -157,6 +162,7 @@ Reset all score data, such as when starting a scenario over.
 """
 func reset() -> void:
 	creature_scores = [0]
+	creature_line_clears = [0]
 	bonus_score = 0
 	scenario_performance = PuzzlePerformance.new()
 	level_index = 0
@@ -179,6 +185,10 @@ func get_bonus_score() -> int:
 
 func get_creature_score() -> int:
 	return creature_scores[creature_scores.size() - 1]
+
+
+func get_creature_line_clears() -> int:
+	return creature_line_clears[creature_line_clears.size() - 1]
 
 
 func end_result() -> int:
@@ -228,3 +238,7 @@ func _add_line() -> void:
 
 func _add_creature_score(delta: int) -> void:
 	creature_scores[creature_scores.size() - 1] += delta
+
+
+func _add_creature_line_clear() -> void:
+	creature_line_clears[creature_line_clears.size() - 1] += 1
