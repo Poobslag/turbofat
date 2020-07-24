Poobslag/turbofat

commit 32321ab4a092415a6f6e47d3808a77bbe1612a42

Mobile controls appear over 'black bars'. Changed overworld scaling.
This required rewriting a lot of the scaling code for things like
positioning buttons.

The overworld now scales to fill in the edges, so it doesn't have 'black
bars'. This required rewriting the shadow/background logic to expand
dynamically.

Closes #390.

project/src/main/puzzle/frosting-globs.gd
@@ -22,7 +22,8 @@ onready var _puzzle_tile_map: PuzzleTileMap = get_node(puzzle_tile_map_path)
 onready var _puzzle_areas: PuzzleAreas
 
 # relative position of the PuzzleTileMap, used for positioning frosting
-onready var _puzzle_tile_map_position: Vector2 = _puzzle_tile_map.get_global_transform().origin
+onready var _puzzle_tile_map_position: Vector2 = _puzzle_tile_map.get_global_transform().origin \
+		- get_global_transform().origin
 
 func _ready() -> void:
 	_puzzle_areas = PuzzleAreas.new()
@@ -50,6 +51,7 @@ func _spawn_globs(cell_pos: Vector2, color_int: int, glob_count: int, glob_alpha
 	for _i in range(glob_count):
 		var glob: FrostingGlob = _instance_glob(viewport)
 		var glob_position := _puzzle_tile_map.somewhere_near_cell(cell_pos) + _puzzle_tile_map_position
+		print("glob position=%s _puzzle_tile_map_position=%s" % [glob_position, _puzzle_tile_map_position])
 		glob.initialize(color_int, glob_position)
 		glob.modulate.a = glob_alpha
 		glob.fall()

project/src/main/puzzle/puzzle-touch-buttons.gd
@@ -54,6 +54,8 @@ onready var _close_pressed := preload("res://assets/main/ui/touch/close-pressed.
 onready var _duck := preload("res://assets/main/ui/touch/duck.png")
 onready var _duck_pressed := preload("res://assets/main/ui/touch/duck-pressed.png")
 
+onready var _menu_button := $MenuButtonHolder/MenuButton
+
 func _ready() -> void:
 	if OS.has_touchscreen_ui_hint():
 		PlayerData.touch_settings.connect("settings_changed", self, "_on_TouchSettings_settings_changed")
@@ -80,15 +82,15 @@ func _refresh_emit_actions() -> void:
 		$ButtonsSw.emit_actions = emit_actions
 		$ButtonsSe.emit_actions = emit_actions
 		if emit_actions:
-			$MenuButton.action = "ui_menu"
-			$MenuButton.normal = _close
-			$MenuButton.pressed = _close_pressed
+			_menu_button.action = "ui_menu"
+			_menu_button.normal = _close
+			_menu_button.pressed = _close_pressed
 		else:
 			# when the player is testing buttons, we replace the icon so we don't confuse users trying who are trying
 			# to quit. (there's an argument that replacing the close button with a duck might confuse them more...)
-			$MenuButton.action = ""
-			$MenuButton.normal = _duck
-			$MenuButton.pressed = _duck_pressed
+			_menu_button.action = ""
+			_menu_button.normal = _duck
+			_menu_button.pressed = _duck_pressed
 
 
 """
@@ -98,14 +100,14 @@ This updates their location and size.
 """
 func _refresh_button_positions() -> void:
 	$ButtonsSw.rect_scale = Vector2(1.0, 1.0) * PlayerData.touch_settings.size
-	$ButtonsSw.rect_position.y = 600 - 10 - $ButtonsSw.rect_size.y * $ButtonsSw.rect_scale.y
+	$ButtonsSw.rect_position.y = rect_size.y - 10 - $ButtonsSw.rect_size.y * $ButtonsSw.rect_scale.y
 	
 	$ButtonsSe.rect_scale = Vector2(1.0, 1.0) * PlayerData.touch_settings.size
-	$ButtonsSe.rect_position.x = 1024 - 10 - $ButtonsSw.rect_size.x * $ButtonsSw.rect_scale.x
-	$ButtonsSe.rect_position.y = 600 - 10 - $ButtonsSw.rect_size.y * $ButtonsSw.rect_scale.y
+	$ButtonsSe.rect_position.x = rect_size.x - 10 - $ButtonsSw.rect_size.x * $ButtonsSw.rect_scale.x
+	$ButtonsSe.rect_position.y = rect_size.y - 10 - $ButtonsSw.rect_size.y * $ButtonsSw.rect_scale.y
 	
-	$MenuButton.scale = Vector2(0.375, 0.375) * PlayerData.touch_settings.size
-	$MenuButton.position.x = 1024 - 10 - $MenuButton.pressed.get_size().x * $MenuButton.scale.x
+	_menu_button.scale = Vector2(0.375, 0.375) * PlayerData.touch_settings.size
+	$MenuButtonHolder.rect_position.x = rect_size.x - 20 - _menu_button.pressed.get_size().x * _menu_button.scale.x
 
 
 """

project/project.godot
@@ -556,7 +556,7 @@ gdscript/warnings/return_value_discarded=false
 [display]
 
 window/stretch/mode="2d"
-window/stretch/aspect="keep"
+window/stretch/aspect="expand"
 
 [editor_plugins]
 
@@ -685,6 +685,7 @@ soft_drop={
 [rendering]
 
 threads/thread_model=2
+environment/default_clear_color=Color( 0.12549, 0.12549, 0.12549, 1 )
 quality/filters/msaa=2
 quality/depth/hdr=false
 quality/2d/gles2_use_nvidia_rect_flicker_workaround=true

project/src/main/puzzle/Playfield.tscn
@@ -276,13 +276,13 @@ bus = "Sound Bus"
 [node name="BuildSnackBoxSound" type="AudioStreamPlayer" parent="BuildBoxSfx"]
 bus = "Sound Bus"
 [connection signal="after_piece_written" from="." to="ComboTracker" method="_on_Playfield_after_piece_written"]
-[connection signal="before_line_cleared" from="." to="ComboTracker" method="_on_Playfield_before_line_cleared"]
-[connection signal="before_line_cleared" from="." to="LineClearSfx" method="_on_Playfield_before_line_cleared"]
-[connection signal="before_line_cleared" from="." to="TileMapClip/LeafPoofs" method="_on_Playfield_before_line_cleared"]
 [connection signal="before_line_cleared" from="." to="TileMapClip/PlayfieldFx" method="_on_Playfield_before_line_cleared"]
+[connection signal="before_line_cleared" from="." to="TileMapClip/LeafPoofs" method="_on_Playfield_before_line_cleared"]
+[connection signal="before_line_cleared" from="." to="LineClearSfx" method="_on_Playfield_before_line_cleared"]
+[connection signal="before_line_cleared" from="." to="ComboTracker" method="_on_Playfield_before_line_cleared"]
+[connection signal="box_built" from="." to="TileMapClip/PlayfieldFx" method="_on_Playfield_box_built"]
 [connection signal="box_built" from="." to="ComboTracker" method="_on_Playfield_box_built"]
 [connection signal="box_built" from="." to="BuildBoxSfx" method="_on_Playfield_box_built"]
-[connection signal="box_built" from="." to="TileMapClip/PlayfieldFx" method="_on_Playfield_box_built"]
 [connection signal="line_cleared" from="." to="ComboTracker" method="_on_Playfield_line_cleared"]
 [connection signal="line_erased" from="." to="LineClearSfx" method="_on_Playfield_line_erased"]
 [connection signal="combo_break_changed" from="ComboTracker" to="TileMapClip/PlayfieldFx" method="_on_ComboTracker_combo_break_changed"]

project/src/main/puzzle/Puzzle.tscn
@@ -117,8 +117,15 @@ border_width_bottom = 2
 border_color = Color( 1, 1, 1, 1 )
 
 [node name="Puzzle" type="Control"]
-anchor_right = 1.0
-anchor_bottom = 1.0
+anchor_left = 0.5
+anchor_top = 0.5
+anchor_right = 0.5
+anchor_bottom = 0.5
+margin_left = -512.0
+margin_top = -300.0
+margin_right = 512.0
+margin_bottom = 300.0
+rect_min_size = Vector2( 1024, 600 )
 script = ExtResource( 21 )
 __meta__ = {
 "_edit_use_anchors_": false
@@ -218,9 +225,11 @@ size = Vector2( 1024, 600 )
 transparent_bg = true
 
 [node name="TextureRect" type="TextureRect" parent="FrostingGlobs"]
+modulate = Color( 1, 1, 1, 0.768627 )
 material = SubResource( 6 )
 margin_right = 1024.0
 margin_bottom = 600.0
+rect_min_size = Vector2( 1024, 600 )
 texture = SubResource( 7 )
 flip_v = true
 __meta__ = {
@@ -231,6 +240,7 @@ __meta__ = {
 material = SubResource( 10 )
 margin_right = 1024.0
 margin_bottom = 600.0
+rect_min_size = Vector2( 1024, 600 )
 texture = SubResource( 11 )
 flip_v = true
 __meta__ = {
@@ -250,6 +260,7 @@ transparent_bg = true
 material = ExtResource( 22 )
 margin_right = 1024.0
 margin_bottom = 600.0
+rect_min_size = Vector2( 1024, 600 )
 texture = SubResource( 12 )
 flip_v = true
 __meta__ = {
@@ -262,7 +273,21 @@ scale = Vector2( 0.5, 0.5 )
 
 [node name="Hud" type="CanvasLayer" parent="."]
 
-[node name="Chalkboard" type="TextureRect" parent="Hud"]
+[node name="HudUi" type="Control" parent="Hud"]
+anchor_left = 0.5
+anchor_top = 0.5
+anchor_right = 0.5
+anchor_bottom = 0.5
+margin_left = -512.0
+margin_top = -300.0
+margin_right = 512.0
+margin_bottom = 300.0
+rect_min_size = Vector2( 1024, 600 )
+__meta__ = {
+"_edit_use_anchors_": false
+}
+
+[node name="Chalkboard" type="TextureRect" parent="Hud/HudUi"]
 margin_left = -58.0
 margin_top = 27.0
 margin_right = 284.0
@@ -274,7 +299,7 @@ __meta__ = {
 "_edit_use_anchors_": false
 }
 
-[node name="Chalkboard Shadow" type="Polygon2D" parent="Hud/Chalkboard"]
+[node name="Chalkboard Shadow" type="Polygon2D" parent="Hud/HudUi/Chalkboard"]
 show_behind_parent = true
 position = Vector2( 57.4473, -143.371 )
 scale = Vector2( 0.444724, 0.444458 )
@@ -282,13 +307,13 @@ color = Color( 0.345098, 0.14902, 0.0705882, 1 )
 antialiased = true
 polygon = PoolVector2Array( -129.116, 322.644, -115.686, 309.085, 653.374, 309.084, 653.35, 905.357, 639.906, 918.809, -129.063, 918.761 )
 
-[node name="Score" parent="Hud/Chalkboard" instance=ExtResource( 61 )]
+[node name="Score" parent="Hud/HudUi/Chalkboard" instance=ExtResource( 61 )]
 margin_left = 58.0
 margin_top = 25.0
 margin_right = 308.0
 margin_bottom = 85.0
 
-[node name="MilestoneHud" type="Control" parent="Hud/Chalkboard"]
+[node name="MilestoneHud" type="Control" parent="Hud/HudUi/Chalkboard"]
 margin_left = 58.0
 margin_top = 153.0
 margin_right = 308.0
@@ -299,7 +324,7 @@ __meta__ = {
 "_edit_use_anchors_": false
 }
 
-[node name="ProgressBar" type="ProgressBar" parent="Hud/Chalkboard/MilestoneHud"]
+[node name="ProgressBar" type="ProgressBar" parent="Hud/HudUi/Chalkboard/MilestoneHud"]
 margin_left = 150.0
 margin_top = 30.0
 margin_right = 250.0
@@ -308,7 +333,7 @@ custom_styles/fg = SubResource( 13 )
 custom_styles/bg = SubResource( 14 )
 percent_visible = false
 
-[node name="Desc" type="Label" parent="Hud/Chalkboard/MilestoneHud"]
+[node name="Desc" type="Label" parent="Hud/HudUi/Chalkboard/MilestoneHud"]
 anchor_right = 1.0
 margin_bottom = 30.0
 theme = ExtResource( 1 )
@@ -318,7 +343,7 @@ __meta__ = {
 "_edit_use_anchors_": false
 }
 
-[node name="Value" type="Label" parent="Hud/Chalkboard/MilestoneHud"]
+[node name="Value" type="Label" parent="Hud/HudUi/Chalkboard/MilestoneHud"]
 margin_left = 150.0
 margin_top = 30.0
 margin_right = 250.0
@@ -336,9 +361,9 @@ __meta__ = {
 }
 fonts = [ ExtResource( 13 ), ExtResource( 3 ), ExtResource( 14 ) ]
 
-[node name="PuzzleMessages" parent="Hud" instance=ExtResource( 62 )]
+[node name="PuzzleMessages" parent="Hud/HudUi" instance=ExtResource( 62 )]
 
-[node name="PuzzleTrace" type="Label" parent="Hud"]
+[node name="PuzzleTrace" type="Label" parent="Hud/HudUi"]
 visible = false
 margin_left = 10.0
 margin_top = 10.0
@@ -350,13 +375,13 @@ script = ExtResource( 5 )
 __meta__ = {
 "_edit_use_anchors_": false
 }
-puzzle_path = NodePath("../..")
+puzzle_path = NodePath("../../..")
 
-[node name="ResultsHud" parent="Hud" instance=ExtResource( 6 )]
+[node name="ResultsHud" parent="Hud/HudUi" instance=ExtResource( 6 )]
 
-[node name="TutorialHud" parent="Hud" instance=ExtResource( 2 )]
+[node name="TutorialHud" parent="Hud/HudUi" instance=ExtResource( 2 )]
 visible = false
-puzzle_path = NodePath("../..")
+puzzle_path = NodePath("../../..")
 
 [node name="TouchButtons" parent="Hud" instance=ExtResource( 10 )]
 
@@ -450,5 +475,5 @@ script = ExtResource( 19 )
 [connection signal="hit_playfield" from="FrostingGlobs" to="FrostingSfx" method="_on_FrostingGlobs_hit_playfield"]
 [connection signal="hit_wall" from="FrostingGlobs" to="WallGlobViewports" method="_on_FrostingGlobs_hit_wall"]
 [connection signal="hit_wall" from="FrostingGlobs" to="FrostingSfx" method="_on_FrostingGlobs_hit_wall"]
-[connection signal="start_button_pressed" from="Hud/PuzzleMessages" to="." method="_on_Hud_start_button_pressed"]
-[connection signal="cheat_detected" from="CheatCodeDetector" to="Hud/PuzzleTrace" method="_on_CheatCodeDetector_cheat_detected"]
+[connection signal="start_button_pressed" from="Hud/HudUi/PuzzleMessages" to="." method="_on_Hud_start_button_pressed"]
+[connection signal="cheat_detected" from="CheatCodeDetector" to="Hud/HudUi/PuzzleTrace" method="_on_CheatCodeDetector_cheat_detected"]

project/src/main/puzzle/PuzzleTouchButtons.tscn
@@ -37,8 +37,17 @@ margin_top = -250.0
 margin_right = -10.0
 margin_bottom = -10.0
 
-[node name="MenuButton" type="TouchScreenButton" parent="."]
-position = Vector2( 918, 10 )
+[node name="MenuButtonHolder" type="Control" parent="."]
+anchor_left = 1.0
+anchor_right = 1.0
+margin_left = -116.0
+margin_bottom = 116.0
+__meta__ = {
+"_edit_use_anchors_": false
+}
+
+[node name="MenuButton" type="TouchScreenButton" parent="MenuButtonHolder"]
+position = Vector2( 10, 10 )
 scale = Vector2( 0.375, 0.375 )
 normal = ExtResource( 3 )
 pressed = ExtResource( 4 )

project/src/main/puzzle/editor/LevelEditor.tscn
@@ -26,8 +26,15 @@ shader = ExtResource( 12 )
 shader_param/mix_color = Color( 1, 1, 1, 0 )
 
 [node name="LevelEditor" type="Control"]
-anchor_right = 1.0
-anchor_bottom = 1.0
+anchor_left = 0.5
+anchor_top = 0.5
+anchor_right = 0.5
+anchor_bottom = 0.5
+margin_left = -512.0
+margin_top = -300.0
+margin_right = 512.0
+margin_bottom = 300.0
+rect_min_size = Vector2( 1024, 600 )
 script = ExtResource( 8 )
 __meta__ = {
 "_edit_use_anchors_": false

project/src/main/puzzle/food-items.gd
@@ -11,7 +11,8 @@ onready var _playfield: Playfield = get_node(playfield_path)
 onready var _puzzle_tile_map := _playfield.tile_map
 
 # relative position of the PuzzleTileMap, used for positioning food
-onready var _puzzle_tile_map_position: Vector2 = _puzzle_tile_map.get_global_transform().origin
+onready var _puzzle_tile_map_position: Vector2 = _puzzle_tile_map.get_global_transform().origin \
+		- get_global_transform().origin
 
 """
 Adds a food item in the specified cell.

commit 189169d289059542bd4f817e5156f293a3aa826e

Cleanup minor regressions introduced in 32321ab

project/src/main/puzzle/frosting-globs.gd
@@ -51,7 +51,6 @@ func _spawn_globs(cell_pos: Vector2, color_int: int, glob_count: int, glob_alpha
 	for _i in range(glob_count):
 		var glob: FrostingGlob = _instance_glob(viewport)
 		var glob_position := _puzzle_tile_map.somewhere_near_cell(cell_pos) + _puzzle_tile_map_position
-		print("glob position=%s _puzzle_tile_map_position=%s" % [glob_position, _puzzle_tile_map_position])
 		glob.initialize(color_int, glob_position)
 		glob.modulate.a = glob_alpha
 		glob.fall()

project/src/main/puzzle/puzzle-touch-buttons.gd
@@ -64,7 +64,7 @@ func _ready() -> void:
 		show()
 	else:
 		hide()
-	$MenuButton.connect("pressed", self, "_on_MenuButton_pressed")
+	_menu_button.connect("pressed", self, "_on_MenuButton_pressed")
 
 
 func set_emit_actions(new_emit_actions: bool) -> void: