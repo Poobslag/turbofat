5f91d680c69585a8dddc289c84a1070d6b8f2932
Poobslag <aaron.pieper@gmail.com>
2020-07-24 08:34:41 -0400

    Mobile controls appear over 'black bars'

---
+++ project/src/main/ui/menu/scene-camera.gd
@@ -0,0 +1,17 @@
+extends Camera2D
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	print("Viewport: %s" % get_viewport())
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
---
+++ project/src/main/world/overworld-bg.gd
@@ -0,0 +1,9 @@
+extends ColorRect
+
+func _ready() -> void:
+	get_tree().get_root().connect("size_changed", self, "_on_Viewport_size_changed")
+	rect_size = get_viewport_rect().size
+
+
+func _on_Viewport_size_changed() -> void:
+	rect_size = get_viewport_rect().size
