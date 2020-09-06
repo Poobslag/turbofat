be7d395bc91c5708907829a17fbd77dbfea1d5a1
Poobslag <aaron.pieper@gmail.com>
2020-09-06 10:42:23 -0400

    ChatIcons are generated for all chattables. Lowered squirrel chat icons.
    Creatures no longer have a chat icon -- instead, a separate ChatIcons node
    generates chat icons for all creatures, and anything else that is in the
    chattables group. Instead of customizing the chat icon by changing the
    chat icon's properties, nodes can define metadata which affects their chat
    icon appearance, such as the 'chat_bubble_type' metadata
    
    This is a slightly cleaner design as scaling/rotating a creature won't
    mess with their chat icon, and we don't need to worry about chat icons
    being hidden by the environment.
    
    These chat icons can't go in the UI layer because they're positioned using
    RemoteTransform2D. But they're in a Node2D separate from the creatures and the
    environment.
    
    Lowered chat bubbles for small squirrel creatures. Their heads are lower,
    so the chat icons should be lower as well.

--- project/src/main/world/glowy-sphere.gd
+++ project/src/main/world/glowy-sphere.gd
@@ -5,3 +5,4 @@ Script for showing a placeholder chattable object in the overworld.
 
 func _ready() -> void:
 	set_meta("chat_path", "res://assets/main/dialog/glowy-sphere.json")
+	set_meta("chat_bubble_type", ChatIcon.THOUGHT)
