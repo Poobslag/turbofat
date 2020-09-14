85ada73e0619dbc7267c730fda233b2fee8e8bb7
Poobslag <aaron.pieper@gmail.com>
2020-09-14 19:41:37 -0400

    Added error dialog when saving/loading files in HTML5.
    While HTML5 supports saving/loading files, it only gives you access to a
    user sandbox and doesn't let you access any files on the filesystem. This
    makes saving/loading pretty useless. We now pop up an error.
    
    Closes #580.
    
    Fixed issue where DialogBackdrop didn't reach the edges of the screen if
    the screen was resized.

--- project/src/main/editor/puzzle/dialogs.gd
+++ project/src/main/editor/puzzle/dialogs.gd
@@ -5,6 +5,11 @@ Shows popup dialogs for the level editor.
 
 onready var _level_editor: LevelEditor = get_parent()
 
+func _show_save_load_not_supported_error() -> void:
+	$Error.dialog_text = "Saving/loading files isn't supported over the web. Sorry!"
+	$Error.popup_centered()
+
+
 func _on_OpenResourceDialog_file_selected(path: String) -> void:
 	_level_editor.load_scenario(path)
 
@@ -18,6 +23,10 @@ func _on_SaveDialog_file_selected(path: String) -> void:
 
 
 func _on_OpenFile_pressed() -> void:
+	if OS.has_feature("web"):
+		_show_save_load_not_supported_error()
+		return
+	
 	$OpenFileDialog.popup_centered()
 
 
@@ -26,6 +35,10 @@ func _on_OpenResource_pressed() -> void:
 
 
 func _on_Save_pressed() -> void:
+	if OS.has_feature("web"):
+		_show_save_load_not_supported_error()
+		return
+	
 	var file_root := StringUtils.sanitize_file_root(_level_editor.scenario_name.text)
 	$SaveDialog.current_file = ScenarioSettings.scenario_filename(file_root)
 	$SaveDialog.popup_centered()
