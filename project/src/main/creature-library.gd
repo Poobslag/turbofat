Poobslag/turbofat

commit a0d9389e6eb2b3de80a49fa9ece7ec347c5c9cf5

Added nametags to puzzle mode.
All customers have names now; their names were previously blank.

CreatureQueue and CreatureLoader.random_def now deal with CreatureDef
objects instead of DNA dictionaries. This is necessary because DNA doesn't
include name or fatness data.

Changed instructor name from 'Master Fat' to 'Turbo' for story reasons.

Moved chat_theme_def generation logic from CreatureEditor into
CreatureLoader.

project/src/main/world/creature/creature-loader.gd
@@ -59,11 +59,31 @@ var _fat_sprite_mover_scenes := {
 	"2": preload("res://src/main/world/creature/FatSpriteMover2.tscn"),
 }
 
+var _name_generator := NameGenerator.new()
+
+func _init() -> void:
+	_name_generator.load_american_animals()
+
+
 """
 Returns a random creature definition.
 """
-func random_def() -> Dictionary:
-	return DnaUtils.random_creature_palette()
+func random_def() -> CreatureDef:
+	var result := CreatureDef.new()
+	result.dna = DnaUtils.random_creature_palette()
+	result.creature_name = _name_generator.generate_name()
+	result.creature_short_name = NameUtils.sanitize_short_name(result.creature_name)
+	result.chat_theme_def = chat_theme_def(result.dna)
+	return result
+
+
+"""
+Returns a chat theme definition for a generated creature.
+"""
+func chat_theme_def(dna: Dictionary) -> Dictionary:
+	var new_def: Dictionary = PlayerData.creature_library.player_def.chat_theme_def.duplicate()
+	new_def.color = dna.body_rgb
+	return new_def
 
 
 """

project/src/main/world/creature/creature.gd
@@ -258,11 +258,11 @@ func restart_idle_timer() -> void:
 func set_creature_def(new_creature_def: CreatureDef) -> void:
 	creature_id = new_creature_def.creature_id
 	set_dna(new_creature_def.dna)
+	set_chat_theme_def(new_creature_def.chat_theme_def)
 	set_creature_name(new_creature_def.creature_name)
 	creature_short_name = new_creature_def.creature_short_name
 	set_chat_selectors(new_creature_def.chat_selectors)
 	dialog = new_creature_def.dialog
-	set_chat_theme_def(new_creature_def.chat_theme_def)
 	set_fatness(new_creature_def.fatness)
 	creature_visuals.set_visual_fatness(new_creature_def.fatness)
 	feed_count = 0

project/src/main/world/restaurant/customer-camera-mover.gd
@@ -22,12 +22,9 @@ onready var _restaurant_scene: RestaurantScene = get_node(restaurant_scene_path)
 onready var _customer_camera: Camera2D = get_node(customer_camera_path)
 
 func _ready() -> void:
-	_restaurant_scene.get_customer(0).connect("visual_fatness_changed", self, \
-			"_on_Creature_visual_fatness_changed", [0])
-	_restaurant_scene.get_customer(1).connect("visual_fatness_changed", self, \
-			"_on_Creature_visual_fatness_changed", [1])
-	_restaurant_scene.get_customer(2).connect("visual_fatness_changed", self, \
-			"_on_Creature_visual_fatness_changed", [2])
+	var customers := _restaurant_scene.get_customers()
+	for i in range(0, customers.size()):
+		customers[i].connect("visual_fatness_changed", self, "_on_Creature_visual_fatness_changed", [0])
 	_refresh_target_camera_position()
 	_customer_camera.position = target_camera_position
 

project/src/demo/world/restaurant/restaurant-scene-demo.gd
@@ -11,26 +11,13 @@ Keys:
 
 const FATNESS_KEYS = [10.0, 1.0, 1.5, 2.0, 3.0, 5.0, 6.0, 7.0, 8.0, 9.0]
 
-var _current_color_index := -1
-
 func _input(event: InputEvent) -> void:
 	match Utils.key_scancode(event):
 		KEY_F: $RestaurantScene.get_customer().feed(Playfield.FOOD_COLORS[0])
 		KEY_0, KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9:
 			$RestaurantScene.get_customer().set_fatness(FATNESS_KEYS[Utils.key_num(event)])
-		KEY_BRACELEFT:
-			if _current_color_index == -1:
-				_current_color_index = 0
-			else:
-				_current_color_index += DnaUtils.CREATURE_PALETTES.size()
-				_current_color_index = (_current_color_index - 1) % DnaUtils.CREATURE_PALETTES.size()
-			$RestaurantScene.summon_creature(DnaUtils.CREATURE_PALETTES[_current_color_index])
+		KEY_BRACELEFT, KEY_BRACERIGHT:
+			$RestaurantScene.summon_creature(CreatureLoader.random_def())
 		KEY_Q: $RestaurantScene.current_creature_index = 0
 		KEY_W: $RestaurantScene.current_creature_index = 1
 		KEY_E: $RestaurantScene.current_creature_index = 2
-		KEY_BRACERIGHT:
-			if _current_color_index == -1:
-				_current_color_index = 0
-			else:
-				_current_color_index = (_current_color_index + 1) % DnaUtils.CREATURE_PALETTES.size()
-			$RestaurantScene.summon_creature(DnaUtils.CREATURE_PALETTES[_current_color_index])

project/src/demo/world/restaurant/restaurant-view-demo.gd
@@ -7,6 +7,7 @@ Keys:
 	[F]: Feed the creature
 	[I]: Launch an idle animation
 	[V]: Say something
+	[N]: Change the nametag names
 	[1-9,0]: Change the creature's size from 10% to 100%
 	SHIFT+[1-9,0]: Change the creature's comfort from 0.0 -> 1.0 -> -1.0
 	[Q,W,E]: Switch to the 1st, 2nd or 3rd creature.
@@ -15,8 +16,22 @@ Keys:
 """
 
 const FATNESS_KEYS = [10.0, 1.0, 1.5, 2.0, 3.0, 5.0, 6.0, 7.0, 8.0, 9.0]
+const NAMES = [
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore" \
+		+ " magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea" \
+		+ " commodo consequat.",
+	"",
+	"Lo",
+	"Lorem",
+	"Lorem ipsum",
+	"Lorem ipsum dolor",
+	"Lorem ipsum dolor sit amet",
+	"Lorem ipsum dolor sit amet, consectetur",
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed",
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidid",
+]
 
-var _current_color_index := -1
+var _current_name_index := 4
 
 func _ready() -> void:
 	$RestaurantView.summon_creature()
@@ -27,6 +42,10 @@ func _input(event: InputEvent) -> void:
 		KEY_D: $RestaurantView/RestaurantViewport/Scene.get_node("DoorChime").play_door_chime()
 		KEY_F: _customer().feed(Playfield.FOOD_COLORS[0])
 		KEY_I: _customer().creature_visuals.get_node("IdleTimer").start(0.01)
+		KEY_N:
+			_current_name_index = (_current_name_index + 1) % NAMES.size()
+			_customer().creature_name = NAMES[_current_name_index]
+			_player().creature_name = NAMES[_current_name_index]
 		KEY_V:
 			Global.greetiness = 2
 			_customer().get_node("CreatureSfx").play_goodbye_voice()
@@ -50,20 +69,7 @@ func _input(event: InputEvent) -> void:
 		KEY_Q: $RestaurantView.set_current_creature_index(0)
 		KEY_W: $RestaurantView.set_current_creature_index(1)
 		KEY_E: $RestaurantView.set_current_creature_index(2)
-		KEY_BRACELEFT:
-			if _current_color_index == -1:
-				_current_color_index = 0
-			else:
-				_current_color_index += DnaUtils.CREATURE_PALETTES.size()
-				_current_color_index = (_current_color_index - 1) % DnaUtils.CREATURE_PALETTES.size()
-			Global.creature_queue.push_front(DnaUtils.CREATURE_PALETTES[_current_color_index])
-			$RestaurantView.summon_creature()
-		KEY_BRACERIGHT:
-			if _current_color_index == -1:
-				_current_color_index = 0
-			else:
-				_current_color_index = (_current_color_index + 1) % DnaUtils.CREATURE_PALETTES.size()
-			Global.creature_queue.push_front(DnaUtils.CREATURE_PALETTES[_current_color_index])
+		KEY_BRACELEFT, KEY_BRACERIGHT:
 			$RestaurantView.summon_creature()
 		KEY_RIGHT:
 			$RestaurantView.get_customer().set_orientation(CreatureVisuals.SOUTHEAST)
@@ -77,3 +83,7 @@ func _input(event: InputEvent) -> void:
 
 func _customer() -> Creature:
 	return $RestaurantView/RestaurantViewport/Scene.get_customer()
+
+
+func _player() -> Creature:
+	return $RestaurantView/RestaurantViewport/Scene.get_player()

project/src/main/editor/creature/HookableNametag.tscn
@@ -1,8 +1,16 @@
-[gd_scene load_steps=2 format=2]
+[gd_scene load_steps=3 format=2]
 
 [ext_resource path="res://src/main/ui/NametagPanel.tscn" type="PackedScene" id=1]
 
+[sub_resource type="StyleBoxFlat" id=1]
+resource_local_to_scene = true
+bg_color = Color( 0.188235, 0.188235, 0.376471, 1 )
+corner_radius_top_left = 25
+corner_radius_top_right = 25
+corner_radius_bottom_right = 25
+corner_radius_bottom_left = 25
 
 [node name="HookableNametag" type="Node2D"]
 
 [node name="Nametag" parent="." instance=ExtResource( 1 )]
+custom_styles/panel = SubResource( 1 )

project/src/main/editor/creature/creature-editor.gd
@@ -53,12 +53,7 @@ onready var _reroll_ui := $Ui/Reroll
 
 func _ready() -> void:
 	_name_generator = NameGenerator.new()
-	_name_generator.add_seed_resource("res://assets/main/editor/creature/animals.txt")
-	_name_generator.add_seed_resource("res://assets/main/editor/creature/american-male-given-names.txt")
-	_name_generator.add_seed_resource("res://assets/main/editor/creature/american-female-given-names.txt")
-	_name_generator.order = 2.7
-	_name_generator.min_length = 5
-	_name_generator.max_length = 11
+	_name_generator.load_american_animals()
 	
 	Breadcrumb.connect("trail_popped", self, "_on_Breadcrumb_trail_popped")
 	
@@ -73,15 +68,6 @@ func _ready() -> void:
 	center_creature.creature_def = PlayerData.creature_library.player_def
 	emit_signal("center_creature_changed")
 	mutate_all_creatures()
-
-
-"""
-Returns a chat theme definition for a generated creature.
-"""
-func _chat_theme_def(dna: Dictionary) -> Dictionary:
-	var new_def := PlayerData.creature_library.player_def.chat_theme_def.duplicate()
-	new_def.color = dna.body_rgb
-	return new_def
 
 
 """
@@ -115,7 +101,7 @@ func _mutate_creature(creature: Creature) -> void:
 		_mutate_allele(creature, dna, new_palette, allele)
 	
 	creature.dna = dna
-	creature.chat_theme_def = _chat_theme_def(dna)
+	creature.chat_theme_def = CreatureLoader.chat_theme_def(dna)
 
 
 """
@@ -311,7 +297,7 @@ func _tweak_creature(creature: Creature, allele: String, color_mode: int) -> voi
 				_recent_tweaked_allele_values[allele].append(dna[allele])
 	
 	creature.dna = dna
-	creature.chat_theme_def = _chat_theme_def(dna)
+	creature.chat_theme_def = CreatureLoader.chat_theme_def(dna)
 
 
 """

project/src/main/name-generator.gd
@@ -23,6 +23,20 @@ var _seed_paths := []
 # seed names loaded from the resource files
 var _seed_word_lists := []
 
+"""
+Configures the name generator with 'American given names' and 'animals'.
+
+This generates names like 'Boala', 'Badgehog' and 'Coyce'.
+"""
+func load_american_animals() -> void:
+	add_seed_resource("res://assets/main/editor/creature/animals.txt")
+	add_seed_resource("res://assets/main/editor/creature/american-male-given-names.txt")
+	add_seed_resource("res://assets/main/editor/creature/american-female-given-names.txt")
+	set_order(2.7)
+	set_min_length(4)
+	set_max_length(11)
+
+
 func reset() -> void:
 	markov_model.clear()
 	_generated_names.clear()

commit 685cd69330e49a4a664f01234f37b3ce153cde94

Creatures maintain their weight. Fixed camera listener bug.
Overworld creatures like Bort, Ebe and Boatricia appear fat on the
overworld and during puzzles if they eat too much. Secondary creatures
like Pandon, Mocha and Nardine maintain their weight from puzzle to puzzle
as well.

Fixed camera listener bug introduced in a0d9389e. Instead of registering a
listener on all three creatures, it was only being registered on the
first.

project/src/main/creature-library.gd
@@ -41,6 +41,12 @@ const DEFAULT_NAME := "Spira"
 
 var player_def: CreatureDef
 
+# fatnesses by creature id
+var _fatnesses: Dictionary
+
+# fatnesses saved to roll the tile map back to a previous state
+var _saved_fatnesses: Dictionary
+
 func reset() -> void:
 	# default player appearance and name
 	player_def = CreatureDef.new()
@@ -51,9 +57,45 @@ func reset() -> void:
 	player_def.chat_theme_def = DEFAULT_CHAT_THEME_DEF.duplicate()
 
 
+"""
+Saves the current fatness state so that we can roll back later.
+"""
+func save_fatness_state() -> void:
+	_saved_fatnesses = _fatnesses.duplicate()
+
+
+"""
+Restores the previously saved fatness state.
+
+This prevents a creature from gaining weight when retrying a level over and over.
+Thematically, we're turning back time.
+"""
+func restore_fatness_state() -> void:
+	_fatnesses = _saved_fatnesses.duplicate()
+
+
+func has_fatness(creature_id: String) -> bool:
+	if not creature_id:
+		return false
+	
+	return _fatnesses.has(creature_id)
+
+
+func get_fatness(creature_id: String) -> float:
+	return _fatnesses[creature_id]
+
+
+func set_fatness(creature_id: String, fatness: float) -> void:
+	if not creature_id:
+		return
+	
+	_fatnesses[creature_id] = fatness
+
+
 func to_json_dict() -> Dictionary:
 	return {
 		"#player#": player_def.to_json_dict(),
+		"fatnesses": _fatnesses,
 	}
 
 
@@ -61,3 +103,5 @@ func from_json_dict(json: Dictionary) -> void:
 	reset()
 	if json.has("#player#"):
 		player_def.from_json_dict(json.get("#player#", {}))
+	if json.has("fatnesses"):
+		_fatnesses = json.get("fatnesses")

project/src/main/puzzle/puzzle.gd
@@ -68,8 +68,10 @@ Parameters:
 	'fatness_pct' A percent from [0.0-1.0] of how much fatter the creature should get from this bite of food.
 """
 func _feed_creature(fatness_pct: float, food_color: Color) -> void:
-	var old_fatness: float = $RestaurantView.get_customer().get_fatness()
-	var target_fatness := sqrt(1 + PuzzleScore.get_creature_score() / 50.0)
+	var customer: Creature = $RestaurantView.get_customer()
+	var old_fatness: float = customer.get_fatness()
+	var base_score := Creature.fatness_to_score(customer.base_fatness)
+	var target_fatness := Creature.score_to_fatness(base_score + PuzzleScore.get_creature_score())
 
 	var comfort := 0.0
 	# ate five things; comfortable
@@ -79,9 +81,9 @@ func _feed_creature(fatness_pct: float, food_color: Color) -> void:
 	# overate; uncomfortable
 	comfort -= clamp(inverse_lerp(600, 1200, PuzzleScore.get_creature_score()), 0.0, 1.0)
 
-	$RestaurantView.get_customer().set_comfort(comfort)
-	$RestaurantView.get_customer().set_fatness(lerp(old_fatness, target_fatness, fatness_pct))
-	$RestaurantView.get_customer().feed(food_color)
+	customer.set_comfort(comfort)
+	customer.set_fatness(lerp(old_fatness, target_fatness, fatness_pct))
+	customer.feed(food_color)
 
 
 """
@@ -114,12 +116,17 @@ func _on_Hud_start_button_pressed() -> void:
 	if _start_button_click_count > 1:
 		# restart puzzle; reset customers
 		CreatureLoader.reset_secondary_creature_queue()
+		if Global.creature_queue:
+			# Reset fatness. Playing the same puzzle over and over shouldn't
+			# make a creature super fat. Thematically, we're turning back time.
+			PlayerData.creature_library.restore_fatness_state()
 		Global.creature_queue_index = 0
 		$RestaurantView.start_suppress_sfx_timer()
 		for i in range(3):
 			$RestaurantView.summon_creature(i)
 		$RestaurantView.set_current_creature_index(0)
 	
+	PlayerData.creature_library.save_fatness_state()
 	PuzzleScore.prepare_and_start_game()
 
 

project/src/main/world/creature/creature-loader.gd
@@ -192,7 +192,7 @@ func load_details(dna: Dictionary) -> void:
 
 
 func load_creature_def_by_id(id: String) -> CreatureDef:
-	return load_creature_def("res://assets/main/primary/%s/creature.json" % id)
+	return load_creature_def("res://assets/main/creatures/primary/%s/creature.json" % id)
 
 
 func load_creature_def(path: String) -> CreatureDef:
@@ -225,7 +225,9 @@ func _load_secondary_creatures() -> void:
 		if not file:
 			break
 		else:
-			_secondary_creature_queue.append(load_creature_def("%s/%s" % [dir.get_current_dir(), file.get_file()]))
+			var creature_def := load_creature_def("%s/%s" % [dir.get_current_dir(), file.get_file()])
+			creature_def.creature_id = file.get_file().get_basename()
+			_secondary_creature_queue.append(creature_def)
 	dir.list_dir_end()
 	_secondary_creature_queue.shuffle()
 

project/src/main/world/creature/creature.gd
@@ -65,6 +65,14 @@ var non_iso_walk_direction := Vector2.ZERO setget set_non_iso_walk_direction
 
 # the number of times the creature was fed during this puzzle
 var feed_count := 0
+var box_feed_count := 0
+
+# the minimum fatness; some creatures never get very thin
+var min_fatness := 1.0
+
+# the base fatness when the creature enters the restaurant
+# player score is added to this to determine their new fatness
+var base_fatness := 1.0
 
 # 'true' if the creature is being slowed by friction while stopping or turning
 var _friction := false
@@ -148,6 +156,7 @@ func set_dna(new_dna: Dictionary) -> void:
 	set_meta("dna", dna)
 	refresh_dna()
 	feed_count = 0
+	box_feed_count = 0
 
 
 func set_chat_selectors(new_chat_selectors: Array) -> void:
@@ -223,6 +232,23 @@ func get_orientation() -> int:
 
 
 """
+Stores this creature's fatness in the CreatureLibrary.
+
+This allows the creature to maintain their fatness if we see them again. They lose a little weight in between.
+"""
+func store_fatness() -> void:
+	if not creature_id:
+		# randomly-generated creatures have no creature id; their fatness isn't stored
+		return
+	
+	var stored_fatness := get_fatness()
+	# weight decreases by 10% if they ate normally; 25% if they ate only vegetables
+	stored_fatness *= 0.9 if box_feed_count > 0 else 0.75
+	stored_fatness = clamp(stored_fatness, min_fatness, CreatureLoader.MAX_FATNESS)
+	PlayerData.creature_library.set_fatness(creature_id, stored_fatness)
+
+
+"""
 Plays a 'hello!' voice sample, for when a creature enters the restaurant
 """
 func play_hello_voice(force: bool = false) -> void:
@@ -248,7 +274,10 @@ func play_goodbye_voice(force: bool = false) -> void:
 
 func feed(food_color: Color) -> void:
 	feed_count += 1
+	if food_color != Playfield.VEGETABLE_COLOR:
+		box_feed_count += 1
 	creature_visuals.feed(food_color)
+	store_fatness()
 
 
 func restart_idle_timer() -> void:
@@ -263,9 +292,15 @@ func set_creature_def(new_creature_def: CreatureDef) -> void:
 	creature_short_name = new_creature_def.creature_short_name
 	set_chat_selectors(new_creature_def.chat_selectors)
 	dialog = new_creature_def.dialog
-	set_fatness(new_creature_def.fatness)
-	creature_visuals.set_visual_fatness(new_creature_def.fatness)
+	min_fatness = new_creature_def.fatness
+	if PlayerData.creature_library.has_fatness(creature_id):
+		set_fatness(PlayerData.creature_library.get_fatness(creature_id))
+	else:
+		set_fatness(min_fatness)
+	base_fatness = get_fatness()
+	creature_visuals.set_visual_fatness(get_fatness())
 	feed_count = 0
+	box_feed_count = 0
 
 
 func get_creature_def() -> CreatureDef:
@@ -434,3 +469,17 @@ func _on_CreatureVisuals_movement_mode_changed(_old_mode: int, new_mode: int) ->
 		set_elevation(35)
 	else:
 		set_elevation(0)
+
+
+"""
+Converts a score in the range [0.0, 5000.0] to a fatness in the range [1.0, 10.0]
+"""
+static func score_to_fatness(in_score: float) -> float:
+	return sqrt(1 + in_score / 50.0)
+
+
+"""
+Converts a fatness in the range [1.0, 10.0] to a score in the range [0.0, 5000.0]
+"""
+static func fatness_to_score(in_fatness: float) -> float:
+	return 50 * (pow(in_fatness, 2) - 1)

project/src/main/world/restaurant/customer-camera-mover.gd
@@ -24,7 +24,7 @@ onready var _customer_camera: Camera2D = get_node(customer_camera_path)
 func _ready() -> void:
 	var customers := _restaurant_scene.get_customers()
 	for i in range(0, customers.size()):
-		customers[i].connect("visual_fatness_changed", self, "_on_Creature_visual_fatness_changed", [0])
+		customers[i].connect("visual_fatness_changed", self, "_on_Creature_visual_fatness_changed", [i])
 	_refresh_target_camera_position()
 	_customer_camera.position = target_camera_position
 