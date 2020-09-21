5c69df56adc6129163311f6832f35dfa5fd0cae5
Poobslag <aaron.pieper@gmail.com>
2020-09-21 08:33:19 -0400

    Added lock/unlock level logic.
    Locked levels have locks on them, and can't be played through the level
    select screen. Some levels have keys on them, which represent levels which
    will contribute to unlocking other levels.
    
    Selecting a lock level shows a message like, 'beat one more level to
    unlock this!' These unlock messages aren't 100% useful or accurate, since
    you have to beat one specific level or the level will stay locked. ...But, we
    can make them better later.
    
    Moved 'scenario' assets into 'scenarios', to match 'creatures'. The idea of
    'code to represent the Creature object' is independent from the idea of 'this
    directory has a ton of creatures in it'. The latter I think makes more sense to
    be plural

--- project/src/main/ui/level-select/level-buttons.gd
+++ project/src/main/ui/level-select/level-buttons.gd
@@ -3,8 +3,11 @@ extends Control
 Creates and arranges level buttons for the level select screen.
 """
 
-# Emitted when the player highlights a level to show information and statistics.
-signal level_selected(settings)
+# Emitted when the player highlights an unlocked level to show more information.
+signal unlocked_level_selected(settings)
+
+# Emitted when the player highlights a locked level to show more information.
+signal locked_level_selected(level_lock)
 
 # Emitted when the player highlights the 'overall' button.
 signal overall_selected(ranks)
@@ -30,31 +33,32 @@ var _column_width := COLUMN_WIDTH_SMALL
 var _scenario_settings_by_id: Dictionary
 
 var _duration_calculator := DurationCalculator.new()
-var _level_ids: Array
-var _level_details: Dictionary
+
+# tracks which levels unlock other levels
+var _world_locks := WorldLocks.new()
 
 func _ready() -> void:
-	_add_level("boatricia1", "boatricia", 1)
-	_add_level("boatricia2", "boatricia", 2)
-	_add_level("five-customers-no-vegetables", "ebe", 1)
-	_add_level("placeholder01", "ebe")
-	_add_level("placeholder02", "bort")
-	_add_level("placeholder03", "bort")
-	_add_level("placeholder04", "boatricia")
-	_add_level("placeholder05", "boatricia")
-	_add_level("placeholder06", "ebe")
-	_add_level("placeholder07", "ebe")
-	_add_level("placeholder08", "bort")
-	_add_level("placeholder09", "bort")
-	_add_level("placeholder10", "bort")
-	_add_level("placeholder11", "ebe")
-	_add_level("placeholder12")
-	
+	_world_locks.initialize()
+	_clear_contents()
+	_add_level_buttons()
+	_add_overall_button()
+	_misalign_columns()
+
+
+"""
+Removes all buttons/placeholders in preparation for loading a new set of levels.
+"""
+func _clear_contents() -> void:
 	for child in get_children():
 		remove_child(child)
 		child.queue_free()
-	
-	var column_count := ceil(sqrt(_level_ids.size() + 1))
+
+
+"""
+Adds buttons representing levels the player can choose.
+"""
+func _add_level_buttons() -> void:
+	var column_count := ceil(sqrt(_world_locks.level_ids.size() + 1))
 	for _i in range(column_count):
 		var vbox_container := VBoxContainer.new()
 		vbox_container.alignment = BoxContainer.ALIGN_END
@@ -63,23 +67,15 @@ func _ready() -> void:
 		_columns.append(vbox_container)
 		add_child(vbox_container)
 	
-	for level_id in _level_ids:
+	for level_id in _world_locks.level_ids:
 		var scenario_settings := ScenarioSettings.new()
 		scenario_settings.load_from_resource(level_id)
 		_scenario_settings_by_id[level_id] = scenario_settings
 	
-	for i in range(_level_ids.size()):
-		var level_id: String = _level_ids[i]
+	for i in range(_world_locks.level_ids.size()):
+		var level_id: String = _world_locks.level_ids[i]
 		var column: VBoxContainer = _columns[(i + 1) % _columns.size()]
 		column.add_child(_level_select_button(level_id))
-	
-	_add_overall_button()
-	
-	for i in range(_columns.size()):
-		if i % 2 == 0:
-			var spacer := Control.new()
-			spacer.rect_min_size.y = _column_width * 0.25 - VERTICAL_SPACING * 0.5
-			_columns[i].add_child(spacer)
 
 
 """
@@ -89,11 +85,22 @@ func _add_overall_button() -> void:
 	var level_select_button: LevelSelectButton = LevelSelectButtonScene.instance()
 	level_select_button.level_column_width = _column_width
 	level_select_button.level_duration = LevelSelectButton.LONG
-	level_select_button.text = "(overall)"
+	level_select_button.level_title = "(overall)"
 	level_select_button.connect("focus_entered", self, "_on_OverallButton_focus_entered")
 	
 	_columns[0].add_child(level_select_button)
 	level_select_button.grab_focus()
+
+
+"""
+Adds spacers so that the columns aren't perfectly aligned.
+"""
+func _misalign_columns() -> void:
+	for i in range(_columns.size()):
+		if i % 2 == 0:
+			var spacer := Control.new()
+			spacer.rect_min_size.y = _column_width * 0.25 - VERTICAL_SPACING * 0.5
+			_columns[i].add_child(spacer)
 
 
 """
@@ -112,26 +119,29 @@ func _level_select_button(level_id: String) -> LevelSelectButton:
 	else:
 		level_select_button.level_duration = LevelSelectButton.LONG
 	
-	level_select_button.text = scenario_settings.title
+	var level_lock: LevelLock = _world_locks.level_locks[level_id]
+	level_select_button.lock_status = level_lock.status
+	level_select_button.keys_needed = level_lock.keys_needed
+	level_select_button.level_title = scenario_settings.title
 	level_select_button.connect("scenario_started", self,
 			"_on_LevelSelectButton_scenario_started", [scenario_settings])
 	level_select_button.connect("focus_entered", self, "_on_LevelSelectButton_focus_entered", [scenario_settings])
 	return level_select_button
 
 
-func _add_level(level_id: String, creature_id: String = "", level_num: int = -1) -> void:
-	_level_ids.append(level_id)
-	_level_details[level_id] = {"creature_id": creature_id, "level_num": level_num}
-
-
+"""
+When the player clicks a level button twice, we launch the selected scenario
+"""
 func _on_LevelSelectButton_scenario_started(settings: ScenarioSettings) -> void:
 	var creature_id := ""
-	if _level_details.has(settings.id):
-		creature_id = _level_details[settings.id].get("creature_id", "")
+	if _world_locks.level_locks.has(settings.id):
+		var level_lock: LevelLock = _world_locks.level_locks.get(settings.id)
+		creature_id = level_lock.creature_id
 	
 	var level_num := -1
-	if _level_details.has(settings.id):
-		level_num = _level_details[settings.id].get("level_num", -1)
+	if _world_locks.level_locks.has(settings.id):
+		var level_lock: LevelLock = _world_locks.level_locks.get(settings.id)
+		level_num = level_lock.level_num
 	
 	Scenario.set_launched_scenario(settings.id, creature_id, level_num)
 	
@@ -141,10 +151,19 @@ func _on_LevelSelectButton_scenario_started(settings: ScenarioSettings) -> void:
 		Scenario.push_scenario_trail()
 
 
+"""
+When the player clicks a level button once, we emit a signal to show more information.
+"""
 func _on_LevelSelectButton_focus_entered(settings: ScenarioSettings) -> void:
-	emit_signal("level_selected", settings)
+	if _world_locks.is_locked(settings.id):
+		emit_signal("locked_level_selected", _world_locks.level_locks[settings.id])
+	else:
+		emit_signal("unlocked_level_selected", settings)
 
 
+"""
+When the player clicks the 'overall' button once, we emit a signal to show more information.
+"""
 func _on_OverallButton_focus_entered() -> void:
 	# populate an array of level ranks. incomplete levels are treated as rank 999
 	var ranks := []
--- project/src/main/ui/level-select/level-description-panel.gd
+++ project/src/main/ui/level-select/level-description-panel.gd
@@ -4,12 +4,34 @@ A panel on the level select screen which shows level descriptions.
 """
 
 """
-Updates the description panel when a new level is selected.
+When an unlocked level is selected, we display the level's description.
 """
-func _on_LevelButtons_level_selected(settings: ScenarioSettings) -> void:
+func _on_LevelButtons_unlocked_level_selected(settings: ScenarioSettings) -> void:
 	$MarginContainer/Label.text = settings.description
 
 
+"""
+When a locked level is selected, we tell the player how to unlock it.
+"""
+func _on_LevelButtons_locked_level_selected(level_lock: LevelLock) -> void:
+	var text := ""
+	match level_lock.status:
+		LevelLock.STATUS_SOFT_LOCK:
+			var level_count := StringUtils.english_number(level_lock.keys_needed)
+			text += "Clear %s more levels to unlock this. You can do it!" % [level_count]
+			text = text.replace("one more levels", "one more level")
+		LevelLock.STATUS_HARD_LOCK:
+			text += "You can't unlock this level yet."
+			text += "\n\n...Go unlock some other levels first. Patience is key!"
+		_:
+			push_warning("Unexpected lock status: %s" % [level_lock.status])
+	
+	$MarginContainer/Label.text = text
+
+
+"""
+When the 'overall' button is selected, we summarize the player's progress.
+"""
 func _on_LevelButtons_overall_selected(ranks: Array) -> void:
 	# calculate the worst rank
 	var worst_rank := 0.0
@@ -17,12 +39,13 @@ func _on_LevelButtons_overall_selected(ranks: Array) -> void:
 		worst_rank = max(worst_rank, rank)
 	
 	var text := ""
-	if RankCalculator.grade(worst_rank) == RankCalculator.NO_GRADE:
-		text += "Try to get a B- or better on every level. Good luck!"
-	elif RankCalculator.grade(worst_rank) == RankCalculator.HIGHEST_GRADE:
-		text += "You are a master of Turbo Fat!"
-	else:
-		text += "Wow, you beat every level! Good job!"
-		text += "\n\nImprove your level scores to earn more stars and increase your grade. Good luck!"
+	match RankCalculator.grade(worst_rank):
+		RankCalculator.NO_GRADE:
+			text += "Try to get a B- or better on every level. Good luck!"
+		RankCalculator.HIGHEST_GRADE:
+			text += "You are a master of Turbo Fat!"
+		_:
+			text += "Wow, you beat every level! Good job!"
+			text += "\n\nImprove your level scores to earn more stars and increase your grade. Good luck!"
 	
 	$MarginContainer/Label.text = text
