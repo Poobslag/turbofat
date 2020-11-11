Poobslag/turbofat

commit 1a2308427dca358b178cad1b02037f9dd15a3c7a

Added Merrymellow Marsh world. Fixed 'customers goal' bug.
Customer goals (e.g Wedding Cake For One) were only ending when the player
dropped an extra piece. This regression was introduced by the previous
week's refactoring.

project/src/main/puzzle/milestone-manager.gd
@@ -116,3 +116,7 @@ func _on_PuzzleScore_after_piece_written() -> void:
 
 func _on_PuzzleScore_score_changed() -> void:
 	_check_for_speed_up()
+	
+	# most milestones end when the piece is written; but customer milestones (mostly through coincidence) end when the
+	# score changes
+	_check_for_finish()

commit 824c2c1723892c2dbcb12a59ac21d053145d6a28

Fixed bug where survival level ended prematurely.
Survival mode was ending immediately after the last line clear, instead of
waiting for the end-of-level 'big finish' clears.

This specific glitch was introduced in
1a230842, but I believe the genesis for these bugs was
cab934b0d3 which replaced a 'combo_ended' signal with a 'combo_changed'
signal. These are interchangeable sometimes but there are scenarios where
a combo can end without the value being reset to zero, such as at the end
of a 'customer limit' level.

Added new 'micro-' levels for quickly testing end-of-level glitches.

Moved test levels into main/puzzle/levels to mirror test/puzzle/levels

Updated path of default level editor level, 'ultra_normal'

project/src/main/editor/puzzle/level-editor.gd
@@ -7,7 +7,7 @@ Full instructions are available at https://github.com/Poobslag/turbofat/wiki/lev
 """
 
 # default to an empty level; players may be confused if it's not empty
-const DEFAULT_LEVEL := "ultra_normal"
+const DEFAULT_LEVEL := "practice/ultra_normal"
 
 export (PackedScene) var PuzzleScene: PackedScene
 

project/src/main/puzzle/milestone-manager.gd
@@ -12,6 +12,7 @@ func _ready() -> void:
 	PuzzleScore.connect("before_piece_written", self, "_on_PuzzleScore_before_piece_written")
 	PuzzleScore.connect("after_piece_written", self, "_on_PuzzleScore_after_piece_written")
 	PuzzleScore.connect("score_changed", self, "_on_PuzzleScore_score_changed")
+	PuzzleScore.connect("combo_ended", self, "_on_PuzzleScore_combo_ended")
 
 
 func _physics_process(_delta: float) -> void:
@@ -116,7 +117,9 @@ func _on_PuzzleScore_after_piece_written() -> void:
 
 func _on_PuzzleScore_score_changed() -> void:
 	_check_for_speed_up()
-	
-	# most milestones end when the piece is written; but customer milestones (mostly through coincidence) end when the
-	# score changes
-	_check_for_finish()
+
+
+func _on_PuzzleScore_combo_ended() -> void:
+	# most milestones end when the piece is written; but customer milestones end when the combo ends
+	if Level.settings.finish_condition.type == Milestone.CUSTOMERS:
+		_check_for_finish()

project/src/main/puzzle/puzzle-score.gd
@@ -44,7 +44,12 @@ signal after_piece_written
 
 signal score_changed
 
+# emitted when a combo value changes, when building or ending a combo
 signal combo_changed(value)
+
+# emitted when a combo ends -- this will usually coincide with the combo resetting to zero, but at the end of a level
+# it's possible for a combo to end without being reset to zero.
+signal combo_ended
 
 # emitted when the current piece can't be placed in the _playfield
 signal topped_out
@@ -245,6 +250,7 @@ func end_combo() -> void:
 	emit_signal("score_changed")
 	if old_combo != combo:
 		emit_signal("combo_changed", combo)
+	emit_signal("combo_ended")
 
 
 """

project/src/main/puzzle/tutorial/tutorial-combo-module.gd
@@ -28,7 +28,7 @@ var _combo_diagram := preload("res://assets/main/puzzle/tutorial/combo-diagram.p
 func _ready() -> void:
 	PuzzleScore.connect("after_game_prepared", self, "_on_PuzzleScore_after_game_prepared")
 	PuzzleScore.connect("after_piece_written", self, "_on_PuzzleScore_after_piece_written")
-	PuzzleScore.connect("combo_changed", self, "_on_PuzzleScore_combo_changed")
+	PuzzleScore.connect("combo_ended", self, "_on_PuzzleScore_combo_ended")
 	
 	playfield.connect("line_cleared", self, "_on_Playfield_line_cleared")
 	playfield.connect("box_built", self, "_on_Playfield_box_built")
@@ -247,9 +247,8 @@ func _on_Playfield_box_built(_rect: Rect2, color: int) -> void:
 		hud.skill_tally_item("CakeBox").increment()
 
 
-func _on_PuzzleScore_combo_changed(value: int) -> void:
-	if value == 0:
-		_did_end_combo = true
+func _on_PuzzleScore_combo_ended() -> void:
+	_did_end_combo = true
 
 
 func _on_PuzzleScore_after_game_prepared() -> void:

project/src/test/puzzle/level/test-level-settings.gd
@@ -10,7 +10,7 @@ func before_each() -> void:
 
 
 func test_load_1922_data() -> void:
-	var json_text := FileUtils.get_file_as_text("res://assets/test/puzzle/level/level-1922.json")
+	var json_text := FileUtils.get_file_as_text("res://assets/test/puzzle/levels/level-1922.json")
 	var json_dict: Dictionary = parse_json(json_text)
 	settings.from_json_dict("level-1922", json_dict)
 	