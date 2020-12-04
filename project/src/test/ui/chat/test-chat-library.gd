f7f094587e5d4b1586c163f3698d67e95d099d47
Poobslag <aaron.pieper@gmail.com>
2020-12-04 09:41:45 -0500

    Added/improved filler dialog for overworld.
    Instead of going to 'filler.json' or 'filler', it now cycles through
    'filler-000...filler-999' allowing creatures to have multiple filler
    conversations.
    
    Added 'drive by' filler conversation logic. If a conversation only has a
    single line, it doesn't zoom the camera in or turn the player.
    
    Chat history keys are consistently referenced as
    'dialog/bort/some_conversation'. They were being occasionally referenced
    as 'creatures/primary/bort/some-conversation' or other similar strings
    before.

--- project/src/test/ui/chat/test-chat-library.gd
+++ project/src/test/ui/chat/test-chat-library.gd
@@ -5,42 +5,40 @@ Unit test for the chat library.
 
 var _chat_selectors := [
 	{
-		"dialog": "level-001",
+		"dialog": "level_001",
 		"if_conditions": [
 			"current_level/level_1"
 		]
 	},
 	{
-		"dialog": "level-002",
+		"dialog": "level_002",
 		"if_conditions": [
 			"current_level/level_2"
 		]
 	},
 	{
-		"dialog": "greeting-001",
+		"dialog": "greeting_001",
 		"repeat": 999999
 	},
 	{
-		"dialog": "notable-001",
+		"dialog": "notable_001",
 		"if_conditions": [
 			"notable_chat"
 		],
 		"repeat": 999999
 	},
 	{
-		"dialog": "notable-002",
+		"dialog": "notable_002",
 		"if_conditions": [
 			"notable_chat"
 		],
 		"repeat": 999999
-	},
-	{
-		"dialog": "filler-001",
-		"repeat": 0
 	}
 ]
 
 var _state := {}
+
+var _filler_ids := ["filler_000", "filler_001"]
 
 func before_each() -> void:
 	PlayerData.chat_history.reset()
@@ -49,53 +47,58 @@ func before_each() -> void:
 	_state["level_num"] = 1
 	_state["notable_chat"] = true
 	
-	PlayerData.chat_history.add_history_item("dialog/gurus750/level-001")
-	PlayerData.chat_history.add_history_item("dialog/gurus750/level-002")
-	PlayerData.chat_history.add_history_item("dialog/gurus750/greeting-001")
-	PlayerData.chat_history.add_history_item("dialog/gurus750/notable-001")
-	PlayerData.chat_history.add_history_item("dialog/gurus750/notable-002")
+	PlayerData.chat_history.add_history_item("dialog/gurus750/level_001")
+	PlayerData.chat_history.add_history_item("dialog/gurus750/level_002")
+	PlayerData.chat_history.add_history_item("dialog/gurus750/greeting_001")
+	PlayerData.chat_history.add_history_item("dialog/gurus750/notable_001")
+	PlayerData.chat_history.add_history_item("dialog/gurus750/notable_002")
 
 
 func test_level_1() -> void:
-	PlayerData.chat_history.delete_history_item("dialog/gurus750/level-001")
-	PlayerData.chat_history.delete_history_item("dialog/gurus750/level-002")
+	PlayerData.chat_history.delete_history_item("dialog/gurus750/level_001")
+	PlayerData.chat_history.delete_history_item("dialog/gurus750/level_002")
 	
-	assert_eq(ChatLibrary.choose_dialog_from_chat_selectors(_chat_selectors, _state), "level-001")
+	assert_eq(ChatLibrary.choose_dialog_from_chat_selectors(_chat_selectors, _state, _filler_ids), "level_001")
 
 
 func test_level_2() -> void:
 	_state["level_num"] = 2
-	PlayerData.chat_history.delete_history_item("dialog/gurus750/level-001")
-	PlayerData.chat_history.delete_history_item("dialog/gurus750/level-002")
+	PlayerData.chat_history.delete_history_item("dialog/gurus750/level_001")
+	PlayerData.chat_history.delete_history_item("dialog/gurus750/level_002")
 	
-	assert_eq(ChatLibrary.choose_dialog_from_chat_selectors(_chat_selectors, _state), "level-002")
+	assert_eq(ChatLibrary.choose_dialog_from_chat_selectors(_chat_selectors, _state, _filler_ids), "level_002")
 
 
 func test_greeting() -> void:
-	PlayerData.chat_history.delete_history_item("dialog/gurus750/greeting-001")
+	PlayerData.chat_history.delete_history_item("dialog/gurus750/greeting_001")
 	
-	assert_eq(ChatLibrary.choose_dialog_from_chat_selectors(_chat_selectors, _state), "greeting-001")
+	assert_eq(ChatLibrary.choose_dialog_from_chat_selectors(_chat_selectors, _state, _filler_ids), "greeting_001")
 
 
 func test_chat_1() -> void:
-	PlayerData.chat_history.delete_history_item("dialog/gurus750/notable-001")
+	PlayerData.chat_history.delete_history_item("dialog/gurus750/notable_001")
 	
-	assert_eq(ChatLibrary.choose_dialog_from_chat_selectors(_chat_selectors, _state), "notable-001")
+	assert_eq(ChatLibrary.choose_dialog_from_chat_selectors(_chat_selectors, _state, _filler_ids), "notable_001")
 
 
 func test_chat_2() -> void:
-	PlayerData.chat_history.delete_history_item("dialog/gurus750/notable-002")
+	PlayerData.chat_history.delete_history_item("dialog/gurus750/notable_002")
 	
-	assert_eq(ChatLibrary.choose_dialog_from_chat_selectors(_chat_selectors, _state), "notable-002")
+	assert_eq(ChatLibrary.choose_dialog_from_chat_selectors(_chat_selectors, _state, _filler_ids), "notable_002")
 
 
 func test_chat_no_notable_chats_remaining() -> void:
-	assert_eq(ChatLibrary.choose_dialog_from_chat_selectors(_chat_selectors, _state), "filler-001")
+	assert_eq(ChatLibrary.choose_dialog_from_chat_selectors(_chat_selectors, _state, _filler_ids), "filler_000")
+
+
+func test_chat_avoid_repeat_filler() -> void:
+	PlayerData.chat_history.add_history_item("dialog/gurus750/filler_000")
+	assert_eq(ChatLibrary.choose_dialog_from_chat_selectors(_chat_selectors, _state, _filler_ids), "filler_001")
 
 
 func test_chat_non_notable() -> void:
 	_state["notable_chat"] = false
-	PlayerData.chat_history.delete_history_item("dialog/gurus750/notable-001")
-	PlayerData.chat_history.delete_history_item("dialog/gurus750/notable-002")
+	PlayerData.chat_history.delete_history_item("dialog/gurus750/notable_001")
+	PlayerData.chat_history.delete_history_item("dialog/gurus750/notable_002")
 	
-	assert_eq(ChatLibrary.choose_dialog_from_chat_selectors(_chat_selectors, _state), "filler-001")
+	assert_eq(ChatLibrary.choose_dialog_from_chat_selectors(_chat_selectors, _state, _filler_ids), "filler_000")
