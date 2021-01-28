Poobslag/turbofat

commit d06c8a83f2aed505c94deb640bd0bdd7feae1458

Added 'chat hierarchy' logic where creatures have many possible chats.
A creature has a series of 'chat selectors' which have rules. Rules
include things like not repeating the same conversation too frequently, or
some conversations which only precede certain levels, or only occur if the
player hasn't met the creature.

ChatUi now handles empty chat messages, advancing past them automatically
until it hits a choice. This is useful for chat events which set metadata
flags.

ChatTree can now parse version, metadata. This is no longer maintained in
ChatLibrary.

Increased CREATURE_DATA_VERSION from 170e to 187d. 'dialog' is now a
dictionary, not an array. I've added some backwards compatibility logic
for old saves.

Dialog can be in separate json files, or inlined into a creature's
'dialog' property. Either way works.

Instead of creature chat bubbles being contingent on some kind of 'chat'
property being set, they're now explicitly specified with a
'chat_bubble_type' property.

Universally added 'virtual property' comment to fields with setget keyword
which don't actually set/get the property's value

project/src/main/world/creature/creature.gd
@@ -39,8 +39,21 @@ export (bool) var suppress_sfx: bool = false setget set_suppress_sfx
 # how high the creature's torso is from the floor, such as when they're sitting on a stool or standing up
 export (int) var elevation: int setget set_elevation
 
+# virtual property; value is only exposed through getters/setters
+export (ChatIcon.BubbleType) var chat_bubble_type: int setget set_chat_bubble_type, get_chat_bubble_type
+
+# virtual property; value is only exposed through getters/setters
 var creature_def: CreatureDef setget set_creature_def, get_creature_def
-var chat_path: String setget set_chat_path
+
+# dictionaries containing metadata for which dialog sequences should be launched in which order
+var chat_selectors: Array setget set_chat_selectors
+
+# creature conversations which were embedded in the creature definition
+#
+# key: chat tree id (string)
+# value: chat tree json for a conversation the creature can have
+var dialog: Dictionary
+
 var creature_name: String setget set_creature_name
 var creature_short_name: String
 var chat_theme_def: Dictionary setget set_chat_theme_def
@@ -67,7 +80,6 @@ func _ready() -> void:
 		_refresh_creature_id()
 	else:
 		refresh_dna()
-		_refresh_chat_path()
 
 
 func _physics_process(delta: float) -> void:
@@ -80,6 +92,14 @@ func _physics_process(delta: float) -> void:
 	var old_non_iso_velocity := _non_iso_velocity
 	set_iso_velocity(move_and_slide(_iso_velocity))
 	_maybe_play_bonk_sound(old_non_iso_velocity)
+
+
+func set_chat_bubble_type(new_chat_bubble_type: int) -> void:
+	$ChatIcon.set_bubble_type(new_chat_bubble_type)
+
+
+func get_chat_bubble_type() -> int:
+	return $ChatIcon.bubble_type
 
 
 func set_suppress_sfx(new_suppress_sfx: bool) -> void:
@@ -133,10 +153,9 @@ func set_dna(new_dna: Dictionary) -> void:
 	refresh_dna()
 
 
-func set_chat_path(new_chat_path: String) -> void:
-	chat_path = new_chat_path
-	set_meta("chat_path", chat_path)
-	_refresh_chat_path()
+func set_chat_selectors(new_chat_selectors: Array) -> void:
+	chat_selectors = new_chat_selectors
+	set_meta("chat_selectors", chat_selectors)
 
 
 func set_creature_id(new_creature_id: String) -> void:
@@ -239,10 +258,12 @@ func restart_idle_timer() -> void:
 
 
 func set_creature_def(new_creature_def: CreatureDef) -> void:
+	creature_id = new_creature_def.creature_id
 	set_dna(new_creature_def.dna)
 	set_creature_name(new_creature_def.creature_name)
 	creature_short_name = new_creature_def.creature_short_name
-	set_chat_path(new_creature_def.chat_path)
+	set_chat_selectors(new_creature_def.chat_selectors)
+	dialog = new_creature_def.dialog
 	set_chat_theme_def(new_creature_def.chat_theme_def)
 	set_fatness(new_creature_def.fatness)
 	creature_visuals.set_visual_fatness(new_creature_def.fatness)
@@ -250,18 +271,26 @@ func set_creature_def(new_creature_def: CreatureDef) -> void:
 
 func get_creature_def() -> CreatureDef:
 	var result := CreatureDef.new()
+	result.creature_id = creature_id
 	result.dna = dna
 	result.creature_name = creature_name
 	result.creature_short_name = creature_short_name
-	result.chat_path = chat_path
+	result.dialog = dialog
 	result.chat_theme_def = chat_theme_def
 	result.fatness = get_fatness()
 	return result
 
 
-func _refresh_creature_id() -> void:
-	if is_inside_tree():
-		set_creature_def(CreatureLoader.load_creature_def_by_id(creature_id))
+"""
+Build a list of level names from the creature's chat selectors.
+"""
+func get_level_names() -> Array:
+	var level_names := []
+	for chat_selector_obj in chat_selectors:
+		var chat_selector: Dictionary = chat_selector_obj
+		if chat_selector.has("level"):
+			level_names.append(chat_selector["level"])
+	return level_names
 
 
 func refresh_dna() -> void:
@@ -273,9 +302,9 @@ func refresh_dna() -> void:
 			$CreatureOutline/TextureRect.material.set_shader_param("black", Color(dna.line_rgb))
 
 
-func _refresh_chat_path() -> void:
+func _refresh_creature_id() -> void:
 	if is_inside_tree():
-		$ChatIcon.bubble_type = ChatIcon.SPEECH if chat_path else ChatIcon.NONE
+		set_creature_def(CreatureLoader.load_creature_def_by_id(creature_id))
 
 
 func _apply_friction() -> void:

delint.sh
@@ -16,7 +16,7 @@ grep -R -n "^					.\{100,\}$" --include="*.gd" ./project/src
 # fields/variables missing type hint. includes a list of whitelisted type hint omissions
 grep -R -n "var [^:]* = " --include="*.gd" ./project/src \
   | grep -v " = parse_json(" \
-  | grep -v "chat-event.gd:73" \
+  | grep -v "chat-event.gd:74" \
   | grep -v "creature-visuals.gd.*var property_value =" \
   | grep -v "creature-visuals.gd.*var shader_value ="
 

project/project.godot
@@ -54,15 +54,15 @@ _global_script_classes=[ {
 "language": "GDScript",
 "path": "res://src/main/ui/chat/chat-frame.gd"
 }, {
+"base": "Reference",
+"class": "ChatHistory",
+"language": "GDScript",
+"path": "res://src/main/ui/chat/chat-history.gd"
+}, {
 "base": "Sprite",
 "class": "ChatIcon",
 "language": "GDScript",
 "path": "res://src/main/world/chat-icon.gd"
-}, {
-"base": "Reference",
-"class": "ChatLibrary",
-"language": "GDScript",
-"path": "res://src/main/ui/chat/chat-library.gd"
 }, {
 "base": "RectFitLabel",
 "class": "ChatLineLabel",
@@ -509,8 +509,8 @@ _global_script_class_icons={
 "ChatChoiceButton": "",
 "ChatEvent": "",
 "ChatFrame": "",
+"ChatHistory": "",
 "ChatIcon": "",
-"ChatLibrary": "",
 "ChatLineLabel": "",
 "ChatLinePanel": "",
 "ChatTheme": "",
@@ -614,6 +614,7 @@ default_bus_layout="res://src/main/ui/default_bus_layout.tres"
 [autoload]
 
 Breadcrumb="*res://src/main/breadcrumb.gd"
+ChatLibrary="*res://src/main/ui/chat/chat-library.gd"
 ChattableManager="*res://src/main/world/chattable-manager.gd"
 Global="*res://src/main/global.gd"
 MilestoneManager="*res://src/main/puzzle/milestone-manager.gd"

project/src/demo/ui/chat/chat-ui-demo.gd
@@ -6,6 +6,7 @@ Keys:
 	[0-9]: Changes chat line length; 1 = short, 9 = long, 0 = longest
 	[Control + 0-9]: Changes response length; 1 = short, 9 = long, 0 = longest
 	[Q, W, E]: Shows questions with more and more options.
+	[R]: Shows a dialog tree missing a lot of chat lines.
 	[A]: Shows a dialog line with no choices.
 """
 
@@ -67,6 +68,9 @@ func _input(event: InputEvent) -> void:
 		KEY_E:
 			_text_override = ""
 			_play_chat_tree("dialog-choices7")
+		KEY_R:
+			_text_override = ""
+			_play_chat_tree("dialog-blank")
 		KEY_A:
 			_text_override = ""
 			_play_chat_tree("dialog-unbranched")

project/src/main/editor/creature/mutate-ui.gd
@@ -5,6 +5,7 @@ Provides buttons/sliders for the player to control how the creatures mutate.
 """
 
 # A higher mutagen level means more alleles will be mutated.
+# Virtual property; value is only exposed through getters/setters
 var mutagen: float setget set_mutagen, get_mutagen
 
 onready var _mutagen_slider := $ScrollContainer/MarginContainer/VBoxContainer/Mutagen/HBoxContainer/HSlider

project/src/main/player-data.gd
@@ -8,6 +8,8 @@ This data includes how well they've done on each level and how much money they'v
 signal money_changed(value)
 
 var scenario_history := ScenarioHistory.new()
+var chat_history := ChatHistory.new()
+
 var creature_library := CreatureLibrary.new()
 
 var gameplay_settings := GameplaySettings.new()
@@ -21,6 +23,7 @@ Resets the player's in-memory data to a default state.
 """
 func reset() -> void:
 	scenario_history.reset()
+	chat_history.reset()
 	creature_library.reset()
 	
 	gameplay_settings.reset()

project/src/main/player-save.gd
@@ -124,6 +124,7 @@ func save_player_data() -> void:
 		for rank_result in PlayerData.scenario_history.results(scenario_name):
 			rank_results_json.append(rank_result.to_json_dict())
 		save_json.append(named_data("scenario-history", scenario_name, rank_results_json).to_json_dict())
+	save_json.append(generic_data("chat-history", PlayerData.chat_history.to_json_dict()).to_json_dict())
 	save_json.append(generic_data("creature-library", PlayerData.creature_library.to_json_dict()).to_json_dict())
 	save_json.append(generic_data("successful-scenarios",
 			PlayerData.scenario_history.successful_scenarios).to_json_dict())
@@ -281,6 +282,9 @@ func _load_line(type: String, key: String, json_value) -> void:
 				rank_result.from_json_dict(rank_result_json)
 				PlayerData.scenario_history.add(key, rank_result)
 			PlayerData.scenario_history.prune(key)
+		"chat-history":
+			var value: Dictionary = json_value
+			PlayerData.chat_history.from_json_dict(value)
 		"creature-library":
 			var value: Dictionary = json_value
 			PlayerData.creature_library.from_json_dict(value)

project/src/main/puzzle/restaurant-view.gd
@@ -5,6 +5,7 @@ Showing the player's chef character and active customer in a restaurant scene.
 As the player drops blocks and scores points, the characters animate and react.
 """
 
+# virtual property; value is only exposed through getters/setters
 var current_creature_index: int setget set_current_creature_index, get_current_creature_index
 
 # bonus points scored for recent lines; used for determining when the chef should smile

commit 28300c8e3443e64adbe66ad0a19a0b880f60ddac

Fixed bug where chat_selectors were no longer populated properly
d06c8a83 introduced a Creature.chat_selectors field, but did not populate
it appropriately in get_creature_def(). As a result the most recent chat
refactoring broke the chat selector logic, meaning special dialog would
never show up.

project/src/main/world/creature/creature.gd
@@ -307,10 +307,11 @@ func get_creature_def() -> CreatureDef:
 	var result := CreatureDef.new()
 	result.creature_id = creature_id
 	result.dna = DnaUtils.trim_dna(dna)
+	result.chat_theme_def = chat_theme_def
 	result.creature_name = creature_name
 	result.creature_short_name = creature_short_name
+	result.chat_selectors = chat_selectors
 	result.dialog = dialog
-	result.chat_theme_def = chat_theme_def
 	result.min_fatness = min_fatness
 	return result
 