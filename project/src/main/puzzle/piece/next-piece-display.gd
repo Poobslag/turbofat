ed39b1b4a4a1a8604dc41da7eabbc45778794f47
Poobslag <aaron.pieper@gmail.com>
2021-04-25 18:06:51 -0400

    Cleaned up some rotation code in NextPieceDisplay

--- project/src/main/puzzle/piece/next-piece-display.gd
+++ project/src/main/puzzle/piece/next-piece-display.gd
@@ -5,12 +5,10 @@ Contains logic for a single 'next piece display'. A single display might only di
 pieces from now. Several displays are shown at once.
 """
 
-const UNROTATED := PieceType.Orientation.UNROTATED
-
 # how far into the future this display should look; 0 = show the next piece, 10 = show the 11th piece
 var _piece_index := 0
 
-var _displayed_piece: NextPiece
+var _displayed_type: PieceType
 var _displayed_orientation: int
 
 onready var _tile_map: PuzzleTileMap = $TileMap
@@ -21,7 +19,7 @@ func initialize(piece_index: int) -> void:
 
 func _process(_delta: float) -> void:
 	var next_piece: NextPiece = PieceQueue.get_next_piece(_piece_index)
-	if next_piece != _displayed_piece or next_piece.orientation != _displayed_orientation:
+	if next_piece.type != _displayed_type or next_piece.orientation != _displayed_orientation:
 		_tile_map.clear()
 		if next_piece != PieceTypes.piece_null:
 			var bounding_box := Rect2( \
@@ -38,5 +36,5 @@ func _process(_delta: float) -> void:
 			_tile_map.position = _tile_map.cell_size \
 					* (Vector2(1.5, 1.5) - (bounding_box.position + bounding_box.size / 2.0)) / 2.0
 		_tile_map.corner_map.dirty = true
-		_displayed_piece = next_piece
+		_displayed_type = next_piece.type
 		_displayed_orientation = next_piece.orientation
