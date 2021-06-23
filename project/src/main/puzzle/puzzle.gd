Poobslag/turbofat

commit 22f4e7a07e832239066eba74c38f2eebb0a80c3b

Improve starting customer logic, food targeting efficiency
The old starting customer logic had conflicting logic in the RestaurantView and
Puzzle scripts. 'Puzzle' would reload all the new creatures, and then
'RestaurantView' would try to scroll to a new creature if they had been fed, but
since all creatures had just been reloaded, it would always just scroll to a new
creature.

The new starting customer logic is more robust, and handles edge cases
such as restarting a puzzle when no creatures have been fed, restarting
a story puzzle when the camera is on the starting creature, and so on.

FoodItems target calculations are now cached, instead of being
recalculated 20 times for 20 different food items. They're all going to
the same location so it's silly to perform multiple lerps/transforms
over and over to get the same answer.

project/src/main/puzzle/puzzle.gd
@@ -4,9 +4,9 @@ extends Control
 A puzzle scene where a player drops pieces into a playfield of blocks.
 """
 
-# The number of times the start button has been pressed. Certain cleanup steps
-# are only necessary when starting a puzzle for the second time.
-var _start_button_click_count := 0
+onready var _restaurant_view: RestaurantView = $RestaurantView
+
+onready var _settings_menu: SettingsMenu = $SettingsMenu
 
 func _ready() -> void:
 	ResourceCache.substitute_singletons()
@@ -16,10 +16,12 @@ func _ready() -> void:
 	PuzzleScore.connect("after_game_ended", self, "_on_PuzzleScore_after_game_ended")
 	$Playfield/TileMapClip/TileMap/Viewport/ShadowMap.piece_tile_map = $PieceManager/TileMap
 	
+	# set a baseline fatness state
+	PlayerData.creature_library.save_fatness_state()
 	PlayerData.creature_queue.primary_index = 0
 	PlayerData.creature_queue.reset_secondary_creature_queue()
 	for i in range(3):
-		$RestaurantView.summon_creature(i)
+		_restaurant_view.summon_creature(i)
 	
 	get_customer().play_hello_voice(true)
 	
@@ -36,7 +38,7 @@ func _exit_tree() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_menu") and not $Hud/HudUi/PuzzleMessages.is_settings_button_visible():
 		# if the player presses the 'menu' button during a puzzle, we pop open the settings panel
-		$SettingsMenu.show()
+		_settings_menu.show()
 		get_tree().set_input_as_handled()
 
 
@@ -57,7 +59,7 @@ func show_buttons() -> void:
 
 
 func scroll_to_new_creature() -> void:
-	$RestaurantView.scroll_to_new_creature()
+	_restaurant_view.scroll_to_new_creature()
 
 
 """
@@ -75,7 +77,7 @@ func start_level_countdown() -> void:
 
 
 func get_customer() -> Creature:
-	return $RestaurantView.get_customer()
+	return _restaurant_view.get_customer()
 
 
 """
@@ -101,22 +103,50 @@ func feed_creature(customer: Creature, food_type: int) -> void:
 Starts or restarts the puzzle, loading new customers and preparing the level.
 """
 func _start_puzzle() -> void:
-	_start_button_click_count += 1
+	PlayerData.creature_queue.reset_secondary_creature_queue()
 	
-	if _start_button_click_count > 1:
-		# restart puzzle; reset customers
-		PlayerData.creature_queue.reset_secondary_creature_queue()
-		if PlayerData.creature_queue.primary_queue:
-			# Reset fatness. Playing the same puzzle over and over shouldn't
-			# make a creature super fat. Thematically, we're turning back time.
-			PlayerData.creature_library.restore_fatness_state()
-		PlayerData.creature_queue.primary_index = 0
-		$RestaurantView.start_suppress_sfx_timer()
+	if not CurrentLevel.keep_retrying:
+		# Reset everyone's fatness. Replaying a puzzle in story mode shouldn't
+		# make a creature super fat. Thematically we're turning back time.
+		PlayerData.creature_library.restore_fatness_state()
+	
+	PlayerData.creature_queue.primary_index = 0
+	_restaurant_view.start_suppress_sfx_timer()
+	
+	if PlayerData.creature_queue.primary_queue:
+		var starting_creature_id: String = PlayerData.creature_queue.primary_queue[0].creature_id
+		var starting_creature_index: int = _restaurant_view.find_creature_index_with_id(starting_creature_id)
+		if starting_creature_index == -1:
+			# starting creature isn't found; load them to a different index and advance the primary queue
+			starting_creature_index = _restaurant_view.next_creature_index()
+			_restaurant_view.summon_creature(starting_creature_index)
+		else:
+			# starting creature is found; advance the primary creature queue so we don't see them twice
+			PlayerData.creature_queue.pop_primary_creature()
+			
+			# restore their fatness so they start skinny again when replaying a puzzle
+			var fatness := PlayerData.creature_library.get_fatness(starting_creature_id)
+			_restaurant_view.get_customer(starting_creature_index).set_fatness(fatness)
+		
+		# summon the other creatures
 		for i in range(3):
-			$RestaurantView.summon_creature(i)
-		$RestaurantView.set_current_creature_index(0)
+			if i != starting_creature_index:
+				_restaurant_view.summon_creature(i)
+		
+		# scroll to the starting creature
+		_restaurant_view.current_creature_index = starting_creature_index
+	else:
+		var current_creature_feed_count: int = _restaurant_view.get_customer().feed_count
+		
+		# fill the seats if the creatures ate
+		for i in range(3):
+			if _restaurant_view.get_customer(i).feed_count:
+				_restaurant_view.summon_creature(i)
+		
+		# calculate the starting creature; stay on the same creature if they didn't eat
+		if current_creature_feed_count > 0:
+			_restaurant_view.current_creature_index = _restaurant_view.next_creature_index()
 	
-	PlayerData.creature_library.save_fatness_state()
 	PuzzleScore.prepare_and_start_game()
 
 
@@ -137,7 +167,7 @@ func _on_Hud_start_button_pressed() -> void:
 
 
 func _on_Hud_settings_button_pressed() -> void:
-	$SettingsMenu.show()
+	_settings_menu.show()
 
 
 func _on_Hud_back_button_pressed() -> void:
@@ -168,7 +198,7 @@ func _on_Playfield_line_cleared(_y: int, total_lines: int, remaining_lines: int,
 
 
 func _on_PuzzleScore_game_started() -> void:
-	$SettingsMenu.quit_type = SettingsMenu.GIVE_UP
+	_settings_menu.quit_type = SettingsMenu.GIVE_UP
 
 
 """
@@ -179,7 +209,7 @@ func _on_PuzzleScore_game_ended() -> void:
 		# null check to avoid errors when launching Puzzle.tscn standalone
 		return
 	
-	$SettingsMenu.quit_type = SettingsMenu.QUIT
+	_settings_menu.quit_type = SettingsMenu.QUIT
 	var rank_result := RankCalculator.new().calculate_rank()
 	PlayerData.level_history.add(CurrentLevel.level_id, rank_result)
 	PlayerData.level_history.prune(CurrentLevel.level_id)
@@ -206,7 +236,7 @@ func _on_PuzzleScore_after_game_ended() -> void:
 
 
 func _on_SettingsMenu_quit_pressed() -> void:
-	if $SettingsMenu.quit_type == SettingsMenu.GIVE_UP:
+	if _settings_menu.quit_type == SettingsMenu.GIVE_UP:
 		PuzzleScore.make_player_lose()
 	else:
 		if not MusicPlayer.is_playing_chill_bgm():

project/src/main/puzzle/food-item.gd
@@ -54,9 +54,8 @@ var _total_time := 0.0
 # Tweens the food's position to fly into the customer's mouth.
 var _flying_tween: Tween
 
-# This object, method and arg_array correspond to a callback which return the position of the customer's mouth.
-var _target_pos_object: Object
-var _target_pos_method: String
+# This func ref and array correspond to a callback which return the position of the customer's mouth.
+var _get_target_pos: FuncRef
 var _target_pos_arg_array: Array
 
 # The position the food is flying from
@@ -87,9 +86,9 @@ func _physics_process(delta: float) -> void:
 	
 	_total_time += delta
 	
-	if _target_pos_object and _flight_percent > 0.0:
+	if _get_target_pos and _flight_percent > 0.0:
 		_source_pos += velocity * delta
-		_target_pos = _target_pos_object.callv(_target_pos_method, _target_pos_arg_array)
+		_target_pos = _get_target_pos.call_funcv(_target_pos_arg_array)
 		
 		# The y coordinate changes at a constant rate while the x coordinate follows a parabolic path
 		position.y = lerp(_source_pos.y, _target_pos.y, _flight_percent)
@@ -123,16 +122,13 @@ The object, method and arg_array parameters correspond to a callback which retur
 mouth.
 
 Parameters:
-	'target_pos_object': The target of the callback function which returns the position of the customer's mouth.
-	
-	'target_pos_method': The name of the callback function which returns the position of the customer's mouth.
+	'new_get_target_pos': The callback function which returns the position of the customer's mouth.
 	
 	'target_pos_arg_array': The parameters of the callback function which returns the position of the customer's mouth.
 """
-func fly_to_target(target_pos_object: Object, target_pos_method: String, target_pos_arg_array: Array,
+func fly_to_target(new_get_target_pos: FuncRef, target_pos_arg_array: Array,
 			duration: float) -> void:
-	_target_pos_object = target_pos_object
-	_target_pos_method = target_pos_method
+	_get_target_pos = new_get_target_pos
 	_target_pos_arg_array = target_pos_arg_array
 	
 	velocity = Vector2(0.0, 0.0)

project/src/main/puzzle/food-items.gd
@@ -27,6 +27,9 @@ var _food_flight_duration := 1.0
 # Maximum time in seconds between eaten food
 var _max_food_repeat_delay := 1.0
 
+# Caches the coordinate of the customer's mouth relative to the FoodItems viewport
+var _target_pos_cache := {}
+
 onready var _puzzle: Puzzle = get_node(puzzle_path)
 onready var _puzzle_tile_map: PuzzleTileMap = _puzzle.get_playfield().tile_map
 
@@ -48,6 +51,12 @@ onready var _customer_change_timer := $CustomerChangeTimer
 
 func _ready() -> void:
 	PuzzleScore.connect("speed_index_changed", self, "_on_PuzzleScore_speed_index_changed")
+
+
+func _physics_process(_delta: float) -> void:
+	if _target_pos_cache:
+		# purge any cached target_pos values; creatures are constantly moving and bobbing their heads
+		_target_pos_cache.clear()
 
 
 """
@@ -76,26 +85,30 @@ func add_food_item(cell: Vector2, food_type: int) -> void:
 
 """
 Callback function which returns the coordinate of the customer's mouth relative to the FoodItems viewport.
+
+This is an expensive calculation which is needed once per frame per food item, so we employ a cache.
 """
 func get_target_pos(customer: Creature) -> Vector2:
-	var target_pos: Vector2 = _restaurant_view.get_customer_mouth_position(customer)
+	if not _target_pos_cache.has(customer):
+		var target_pos: Vector2 = _restaurant_view.get_customer_mouth_position(customer)
+		
+		# if the target customer goes offscreen the food targets a point on the
+		# screen's edge, not a point far outside the boundaries of the screen.
+		if customer != _puzzle.get_customer():
+			if customer.position.x < _puzzle.get_customer().position.x:
+				target_pos.x = lerp(0, target_pos.x,
+						_customer_change_timer.time_left / _customer_change_timer.wait_time)
+			else:
+				target_pos.x = lerp(_window_width, target_pos.x,
+						_customer_change_timer.time_left / _customer_change_timer.wait_time)
+		
+		# calculate the position within the global viewport
+		target_pos = get_global_transform_with_canvas().xform_inv(target_pos)
+		# calculate the position within the FoodItems viewport texture
+		target_pos = target_pos / _texture_rect.rect_scale
+		_target_pos_cache[customer] = target_pos
 	
-	# if the target customer goes offscreen the food targets a point on the screen's edge, not a point far outside the
-	# boundaries of the screen.
-	if customer != _puzzle.get_customer():
-		if customer.position.x < _puzzle.get_customer().position.x:
-			target_pos.x = lerp(0, target_pos.x,
-					_customer_change_timer.time_left / _customer_change_timer.wait_time)
-		else:
-			target_pos.x = lerp(_window_width, target_pos.x,
-					_customer_change_timer.time_left / _customer_change_timer.wait_time)
-	
-	# calculate the position within the global viewport
-	target_pos = get_global_transform_with_canvas().xform_inv(target_pos)
-	# calculate the position within the FoodItems viewport texture
-	target_pos = target_pos / _texture_rect.rect_scale
-	
-	return target_pos
+	return _target_pos_cache[customer]
 
 
 """
@@ -121,7 +134,7 @@ func _on_FoodItem_float_done(food_item: FoodItem) -> void:
 
 
 func _on_FoodItem_ready_to_fly(food_item: FoodItem) -> void:
-	food_item.fly_to_target(self, "get_target_pos", [food_item.customer], _food_flight_duration)
+	food_item.fly_to_target(funcref(self, "get_target_pos"), [food_item.customer], _food_flight_duration)
 	
 	# trigger the eating animation just before we arrive at the creature's mouth
 	var adjusted_flight_duration := _food_flight_duration - food_item.customer.get_eating_delay()

project/src/main/puzzle/restaurant-view.gd
@@ -34,7 +34,6 @@ func _ready() -> void:
 	_customer_view_viewport.size = _customer_view.rect_size * 4
 	
 	PuzzleScore.connect("game_ended", self, "_on_PuzzleScore_game_ended")
-	PuzzleScore.connect("game_prepared", self, "_on_PuzzleScore_game_prepared")
 	PuzzleScore.connect("combo_changed", self, "_on_PuzzleScore_combo_changed")
 	PuzzleScore.connect("topped_out", self, "_on_PuzzleScore_topped_out")
 	PuzzleScore.connect("added_line_score", self, "_on_PuzzleScore_added_line_score")
@@ -90,6 +89,24 @@ func get_customer_mouth_position(customer: Creature) -> Vector2:
 
 
 """
+Returns the creature index for the specified creature id.
+
+This is useful for determining if a creature is seated at a table, and if so, which table they're seated at. Returns
+-1 if the specified creature is not a customer in the restaurant.
+"""
+func find_creature_index_with_id(creature_id: String) -> int:
+	var creature_index := -1
+	var customers := get_customers()
+	
+	for i in range(customers.size()):
+		if customers[i].creature_def.creature_id == creature_id:
+			creature_index = i
+			break
+	
+	return creature_index
+
+
+"""
 Recolors the creature according to the specified creature definition. This involves updating shaders and sprite
 properties.
 """
@@ -110,11 +127,18 @@ Scroll to a new creature and replace the old creature.
 func scroll_to_new_creature(new_creature_index: int = -1) -> void:
 	var old_creature_index: int = get_current_creature_index()
 	if new_creature_index == -1:
-		new_creature_index = (old_creature_index + randi() % 2 + 1) % 3
+		new_creature_index = next_creature_index()
 	set_current_creature_index(new_creature_index)
 	_restaurant_viewport_scene.get_customer().restart_idle_timer()
 	yield(get_tree().create_timer(0.5), "timeout")
 	summon_creature(old_creature_index)
+
+
+"""
+Returns a random creature index different from the current creature index.
+"""
+func next_creature_index() -> int:
+	return (get_current_creature_index() + randi() % 2 + 1) % 3
 
 
 """
@@ -136,14 +160,6 @@ func _refresh_chef_name() -> void:
 
 func _on_Chef_creature_name_changed() -> void:
 	_refresh_chef_name()
-
-
-"""
-If they ended the previous game while serving a creature, we scroll to a new one
-"""
-func _on_PuzzleScore_game_prepared() -> void:
-	if get_customer().feed_count > 0:
-		scroll_to_new_creature()
 
 
 """

commit 126721dcf0f37cde4c00bd76d8e1ce162aa423dc

Fixed 'missing sensei fatness' error when starting new game
This regression was introduced in 22f4e7a0. Restarting a puzzle
restores a creature's fatness, but this logic shouldn't be applied to
the tutorial creature who has no fatness.

project/src/main/puzzle/puzzle.gd
@@ -124,9 +124,10 @@ func _start_puzzle() -> void:
 			# starting creature is found; advance the primary creature queue so we don't see them twice
 			PlayerData.creature_queue.pop_primary_creature()
 			
-			# restore their fatness so they start skinny again when replaying a puzzle
-			var fatness := PlayerData.creature_library.get_fatness(starting_creature_id)
-			_restaurant_view.get_customer(starting_creature_index).set_fatness(fatness)
+			if PlayerData.creature_library.has_fatness(starting_creature_id):
+				# restore their fatness so they start skinny again when replaying a puzzle
+				var fatness := PlayerData.creature_library.get_fatness(starting_creature_id)
+				_restaurant_view.get_customer(starting_creature_index).set_fatness(fatness)
 		
 		# summon the other creatures
 		for i in range(3):