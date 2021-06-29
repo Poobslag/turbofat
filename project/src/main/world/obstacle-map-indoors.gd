3a97e6ef3e0a9492d6f05b1619110652394abb6b
Poobslag <aaron.pieper@gmail.com>
2021-06-29 10:11:25 -0400

    Indoor isometric tiles are sorted with a transform.
    By applying an unnoticable y component to the tilemap transformation,
    tiles on the right are rendered in front of tiles on the left. 0.007
    appears to be the smallest value which works. Values smaller than that
    result in the tiles being left unsorted.
    
    This is a workaround for the absence of x-sorting in tilemaps, a feature
    requested by Godot proposal #2838
    https://github.com/godotengine/godot-proposals/issues/2838

--- project/src/main/world/obstacle-map-indoors.gd
+++ project/src/main/world/obstacle-map-indoors.gd
@@ -171,60 +171,11 @@ const SINK_AUTOTILE_COORDS_BY_BINDING := {
 	[RIGHT, BIND_BOTTOM]: Vector2(2, 3),
 }
 
-# An editor toggle which sorts tiles by their y coordinate and x coordinate.
-#
-# Godot has no way to sort TileMaps by their x coordinate. See Godot-Proposals #2838
-# https://github.com/godotengine/godot-proposals/issues/2838
-export (bool) var _xsort: bool setget xsort
-
 # An editor toggle which manually applies autotiling.
 #
 # Godot has no way of automatically reacting to GridMap/TileMap changes. See Godot #11855
 # https://github.com/godotengine/godot/issues/11855
 export (bool) var _autotile: bool setget autotile
-
-func _ready() -> void:
-	xsort(true)
-
-
-"""
-Sorts tiles by their x coordinate.
-
-TileMaps render tiles from oldest to newest. To get Godot to render objects by their x coordinate, this method removes
-all tiles from the TileMap and adds them again from left to right.
-"""
-func xsort(value: bool) -> void:
-	if not value:
-		return
-	
-	# create an X-sorted list of used cells in the tilemap
-	var x_sorted_cells := get_used_cells().duplicate()
-	x_sorted_cells.sort_custom(self, "_compare_by_x")
-	
-	# we cache the previous x coordinate to track if the x coordinate increases during the loop
-	var prev_cell_x: int
-	if x_sorted_cells:
-		prev_cell_x = x_sorted_cells[0].x
-	
-	# replace the tiles from left to right
-	for cell in x_sorted_cells:
-		# cache the tilemap's metadata for this tile
-		var tile: int = get_cell(cell.x, cell.y)
-		var flip_x: bool = is_cell_x_flipped(cell.x, cell.y)
-		var flip_y: bool = is_cell_y_flipped(cell.x, cell.y)
-		var transpose: bool = is_cell_transposed(cell.x, cell.y)
-		var autotile_coord: Vector2 = get_cell_autotile_coord(cell.x, cell.y)
-		
-		# remove the cell from the tilemap
-		set_cell(cell.x, cell.y, -1)
-		
-		# we must pause when advancing the x coordinate or the render order will not change
-		if cell.x != prev_cell_x:
-			prev_cell_x = cell.x
-			yield(get_tree(), "idle_frame")
-		
-		# add the tile to the tilemap
-		set_cell(cell.x, cell.y, tile, flip_x, flip_y, transpose, autotile_coord)
 
 
 """
@@ -360,7 +311,3 @@ func _adjacencies(cell: Vector2, tile_indexes: Array) -> int:
 	binding |= BIND_LEFT if get_cellv(cell + Vector2.LEFT) in tile_indexes else 0
 	binding |= BIND_RIGHT if get_cellv(cell + Vector2.RIGHT) in tile_indexes else 0
 	return binding
-
-
-func _compare_by_x(a: Vector2, b: Vector2) -> bool:
-	return a.x < b.x
