5097c54947a771cd0820fa5ac2071b63f7b628f4
Poobslag <aaron.pieper@gmail.com>
2021-07-07 13:13:55 -0400

    CutsceneDemo improvements; 'flags', save state
    Added 'flags' field to CutsceneDemo. This allows developers to specify
    things like 'chat_finished foo' 'not level_finished bar' to test
    cutscenes under specific circumstances.
    
    CutsceneDemo stores its state using a new 'DemoSave' script. This script
    is generic and can save state for other demos as well.

---
+++ project/src/demo/demo-save.gd
@@ -0,0 +1,42 @@
+extends Node
+"""
+Reads and writes data about demos from a file.
+
+This allows demos to remember a developer's selections, so that they don't need to keep retyping or reselecting the
+same choices.
+
+This data is stored independently of the player data to prevent demos from overwriting the developer's game data
+with invalid data. Demos often put the game into a weird state temporarily, and we don't want to persist this weird
+temporary data.
+"""
+
+# the path to the save file
+const FILENAME := "user://demo-data.json"
+
+# the json data read from the save file
+var demo_data := {}
+
+func _ready() -> void:
+	demo_data.clear()
+	load_demo_data()
+
+
+"""
+Writes the developer's in-memory data to a save file.
+"""
+func save_demo_data() -> void:
+	FileUtils.write_file(FILENAME, Utils.print_json(demo_data))
+
+
+"""
+Populates the player's in-memory data based on their save files.
+"""
+func load_demo_data() -> void:
+	var file := File.new()
+	var open_result := file.open(FILENAME, File.READ)
+	if open_result == OK:
+		var save_json_text := FileUtils.get_file_as_text(FILENAME)
+		demo_data = parse_json(save_json_text)
+	else:
+		# validation failed; couldn't open file
+		push_warning("Couldn't open file '%s' for reading: %s" % [FILENAME, open_result])
---
+++ project/src/demo/world/cutscene-demo-flags.gd
@@ -0,0 +1,47 @@
+extends VBoxContainer
+"""
+UI input for specifying flags to assign or unassign during cutscenes.
+"""
+
+# Describes flags to assign or unassign during cutscenes
+# Virtual property; value is only exposed through getters/setters
+var value: String setget set_value, get_value
+
+"""
+Applies the text contents to the player's data.
+
+This involves assigning and unassigning conversations they've had and levels they've played based on the input. The
+input field supports the following flags:
+	
+	'chat_finished foo'
+	'not chat_finished foo'
+	'level_finished foo'
+	'not level_finished foo'
+"""
+func apply_flags() -> void:
+	for line_obj in $TextEdit.text.split("\n"):
+		var line: String = line_obj
+		if not line:
+			pass
+		elif line.begins_with("chat_finished "):
+			var history_key := StringUtils.substring_after(line, "chat_finished ")
+			PlayerData.chat_history.add_history_item(history_key)
+		elif line.begins_with("not chat_finished "):
+			var history_key := StringUtils.substring_after(line, "chat_finished ")
+			PlayerData.chat_history.delete_history_item(history_key)
+		elif line.begins_with("level_finished "):
+			var level_key := StringUtils.substring_after(line, "level_finished ")
+			PlayerData.level_history.finished_levels[level_key] = OS.get_datetime()
+		elif line.begins_with("not level_finished "):
+			var level_key := StringUtils.substring_after(line, "level_finished ")
+			PlayerData.level_history.finished_levels.erase(level_key)
+		else:
+			push_warning("Unrecognized flag: %s" % [line])
+
+
+func set_value(new_value: String) -> void:
+	$TextEdit.text = new_value
+
+
+func get_value() -> String:
+	return $TextEdit.text
