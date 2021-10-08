a6b0bca086dfa9db37fed11ff0c6a2e3cc8c21f4
Poobslag <aaron.pieper@gmail.com>
2021-10-08 13:47:23 -0400

    Replaced overworld goop shader with static textures.
    The overworld goop shader worked OK on my PC, but greatly impacted FPS
    on Android devices. It required executing a complex shader on
    approximately 200-300 nodes every frame. This requires a reasonably
    high-end graphics card.
    
    Removing the shader improved overworld performance on my Samsung Galaxy
    Note 9 from 41 fps to 60 fps. Performance on desktop targets was unaffected.
    
    Added AutotileFixer node for reapplying autotiles to TileMaps.

---
+++ project/src/test/world/autotile-fixer.gd
@@ -0,0 +1,26 @@
+extends Node
+tool
+"""
+Applies autotiling rules to all cells in a TileMap.
+
+After editing a TileSet, there's no easy way to reapply its autotile rules across an entire TileMap. This
+AutotileFixer node makes it easier.
+
+Instructions: Attach this node to a TileMap and toggle this node's 'autotile' property. This will apply autotiling
+rules to all tiles in the TileMap.
+
+"""
+
+# An editor toggle which manually applies autotiling.
+export (bool) var _autotile: bool setget autotile
+
+"""
+Applies autotiling rules to all cells in a TileMap.
+"""
+func autotile(_value: bool) -> void:
+	if not _value:
+		return
+	
+	var tile_map: TileMap = get_parent()
+	var used_rect := tile_map.get_used_rect()
+	tile_map.update_bitmask_region(used_rect.position, used_rect.end)
