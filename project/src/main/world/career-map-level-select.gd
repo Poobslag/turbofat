5d11174b395c67bd21e2dcc1e401fb1fe4bc7974
Poobslag <aaron.pieper@gmail.com>
2021-12-08 10:10:01 -0500

    Career mode now shows an overworld scene.
    The overworld scene is mostly empty and only includes the player and
    sensei. But eventually, it will include customers and scenery.

---
+++ project/src/main/world/career-map-level-select.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+## UI components for career mode's level select buttons.
+
+signal level_button_focused(button_index)
+
+onready var _level_select_buttons := $Control/Top/LevelButtons.get_children()
+
+func _ready() -> void:
+	for i in range(_level_select_buttons.size()):
+		var button: Button = _level_select_buttons[i]
+		button.connect("focus_entered", self, "_on_LevelSelectButton_focus_entered", [i])
+
+
+func _on_LevelSelectButton_focus_entered(button_index: int) -> void:
+	emit_signal("level_button_focused", button_index)
