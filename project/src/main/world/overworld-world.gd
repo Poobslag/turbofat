deeabe1c0b1c5b8293d37f35cb69fd42fcf1010c
Poobslag <aaron.pieper@gmail.com>
2021-12-08 14:10:03 -0500

    Extracted ObstacleManager from OverworldUi.
    Both the overworld and career map will need to add obstacles and creatures, so
    this common functionality should be shared with a script.

--- project/src/main/world/overworld-world.gd
+++ project/src/main/world/overworld-world.gd
@@ -2,15 +2,8 @@ class_name OverworldWorld
 extends Node
 ## Populates/unpopulates the creatures and obstacles on the overworld.
 
-export (NodePath) var shadow_caster_shadows_path: NodePath
-export (NodePath) var creature_shadows_path: NodePath
-export (NodePath) var chat_icons_path: NodePath
-export (PackedScene) var CreatureScene: PackedScene
-
-onready var _shadow_caster_shadows: ShadowCasterShadows = get_node(shadow_caster_shadows_path)
-onready var _creature_shadows: CreatureShadows = get_node(creature_shadows_path)
-onready var _chat_icons: ChatIcons = get_node(chat_icons_path)
 onready var _overworld_ui: OverworldUi = Global.get_overworld_ui()
+onready var _obstacle_manager: ObstacleManager = $ObstacleManager
 
 func _ready() -> void:
 	if not Breadcrumb.trail:
@@ -25,10 +18,10 @@ func _ready() -> void:
 		_launch_cutscene()
 	else:
 		if Global.player_spawn_id:
-			move_creature_to_spawn(ChattableManager.player, Global.player_spawn_id)
+			_obstacle_manager.move_creature_to_spawn(ChattableManager.player, Global.player_spawn_id)
 		
 		if Global.sensei_spawn_id:
-			move_creature_to_spawn(ChattableManager.sensei, Global.sensei_spawn_id)
+			_obstacle_manager.move_creature_to_spawn(ChattableManager.sensei, Global.sensei_spawn_id)
 	
 	$Camera.position = ChattableManager.player.position
 
@@ -52,7 +45,7 @@ func _launch_cutscene() -> void:
 	var cutscene_creature: Creature
 	if CurrentLevel.creature_id:
 		# add the cutscene creature
-		cutscene_creature = add_creature(CurrentLevel.creature_id)
+		cutscene_creature = _obstacle_manager.add_creature(CurrentLevel.creature_id)
 	
 	# get the location, spawn location data
 	var chat_tree := CutsceneManager.pop_chat_tree()
@@ -65,72 +58,16 @@ func _launch_cutscene() -> void:
 		cutscene_creature.position += Vector2(60, 0)
 	else:
 		for creature_id in chat_tree.spawn_locations:
-			var creature := _find_creature(creature_id)
+			var creature: Creature = _obstacle_manager.find_creature(creature_id)
 			if not creature:
-				creature = add_creature(creature_id)
+				creature = _obstacle_manager.add_creature(creature_id)
 			creature.set_collision_disabled(true)
 			
 			# move the creature to its spawn location
-			move_creature_to_spawn(creature, chat_tree.spawn_locations[creature_id])
+			_obstacle_manager.move_creature_to_spawn(creature, chat_tree.spawn_locations[creature_id])
 	
 	yield(get_tree(), "idle_frame")
 	_overworld_ui.start_chat(chat_tree, cutscene_creature)
 	
 	if not chat_tree.spawn_locations and cutscene_creature:
 		_overworld_ui.make_chatters_face_eachother()
-
-
-## Relocate a creature to a spawn point.
-func move_creature_to_spawn(creature: Creature, spawn_id: String) -> void:
-	var target_spawn: Spawn
-	for spawn_obj in get_tree().get_nodes_in_group("spawns"):
-		var spawn: Spawn = spawn_obj
-		if spawn.id == spawn_id:
-			target_spawn = spawn
-		elif spawn.id == spawn_id.trim_prefix("!"):
-			# Spawn locations prefixed with a '!' indicate that the creature should spawn invisible.
-			creature.visible = false
-			target_spawn = spawn
-
-	if target_spawn:
-		target_spawn.move_creature(creature)
-	else:
-		push_warning("Could not locate spawn with id '%s'" % [spawn_id])
-
-
-## Creates a new creature with the specified creature_id and adds it to the scene.
-func add_creature(creature_id: String, chattable: bool = true) -> Creature:
-	var creature: Creature = CreatureScene.instance()
-	creature.creature_id = creature_id
-	$Obstacles.add_child(creature)
-	if chattable:
-		creature.add_to_group("chattables")
-		var chat_bubble_type := ChatLibrary.chat_icon_for_creature(creature)
-		creature.set_meta("chat_bubble_type", chat_bubble_type)
-		ChattableManager.register_creature(creature)
-	process_new_obstacle(creature)
-	return creature
-
-
-func process_new_obstacle(obstacle: Node2D) -> void:
-	# create chat icon
-	if obstacle.is_in_group("chattables"):
-		_chat_icons.create_icon(obstacle)
-	
-	# create shadow
-	if obstacle is Creature:
-		_creature_shadows.create_shadow(obstacle)
-	else:
-		_shadow_caster_shadows.create_shadow(obstacle)
-
-
-## Locates the creature with the specified creature_id.
-func _find_creature(creature_id: String) -> Creature:
-	var creature: Creature
-	
-	for creature_node in get_tree().get_nodes_in_group("creatures"):
-		if creature_node.creature_id == creature_id:
-			creature = creature_node
-			break
-	
-	return creature
