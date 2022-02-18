Poobslag/turbofat

commit 8c356a5d4229f415fdefc411d35b2cbcfd51c481

Added 'banked steps' logic for career mode.
If the player hits a boss level they haven't cleared, instead of the
steps being thrown away, they're 'banked' and applied the next time the
player advances forward.

project/src/main/ui/career/career-distance-label.gd
@@ -22,10 +22,8 @@ func _refresh_label() -> void:
 	var distance_penalty: int = PlayerData.career.distance_penalties()[focused_level_button_index]
 	
 	# Display the distance travelled with the distance penalty applied
-	_label.text = StringUtils.comma_sep(PlayerData.career.distance_travelled - distance_penalty)
-	
-	# Append the distance_option with the distance penalty
 	var distance_earned_value := PlayerData.career.distance_earned - distance_penalty
+	_label.text = StringUtils.comma_sep(distance_earned_value)
 	
 	if distance_earned_value > 0:
 		# distance_earned is positive; prefix distance_option with a '+'

project/src/main/career-data.gd
@@ -47,6 +47,9 @@ const RANK_MILESTONES := [
 ## The rank milestone to display when the player fails a boss level.
 const RANK_MILESTONE_FAIL := {"rank": 64.0, "distance": 0, "color": Color("bababa")}
 
+## The number of steps the player couldn't take because they were blocked by a boss level.
+var banked_steps := 0
+
 ## The distance the player has travelled in the current career session.
 var distance_travelled := 0 setget set_distance_travelled
 
@@ -92,6 +95,7 @@ func is_day_over() -> bool:
 
 
 func reset() -> void:
+	banked_steps = 0
 	distance_travelled = 0
 	distance_earned = 0
 	hours_passed = 0
@@ -109,6 +113,7 @@ func reset() -> void:
 
 
 func from_json_dict(json: Dictionary) -> void:
+	banked_steps = int(json.get("banked_steps", 0))
 	distance_travelled = int(json.get("distance_travelled", 0))
 	distance_earned = int(json.get("distance_earned", 0))
 	hours_passed = int(json.get("hours_passed", 0))
@@ -127,6 +132,7 @@ func from_json_dict(json: Dictionary) -> void:
 
 func to_json_dict() -> Dictionary:
 	var results := {}
+	results["banked_steps"] = banked_steps
 	results["distance_travelled"] = distance_travelled
 	results["distance_earned"] = distance_earned
 	results["hours_passed"] = hours_passed
@@ -280,6 +286,11 @@ func advance_clock(new_distance_earned: int, success: bool) -> void:
 			# if they fail a boss level, they lose 1-2 days worth of progress
 			distance_earned = -int(max(boss_region.length * rand_range(0.125, 0.25), 2))
 	
+	if distance_earned > 0:
+		# if they make forward progress, they also spend their banked steps
+		distance_earned += banked_steps
+		banked_steps = 0
+	
 	var unapplied_distance_earned := distance_earned
 	while unapplied_distance_earned != 0:
 		unapplied_distance_earned = _apply_distance_earned(unapplied_distance_earned)
@@ -299,6 +310,7 @@ func advance_calendar() -> void:
 	# Put the player at the start of their current region.
 	distance_travelled = CareerLevelLibrary.region_for_distance(distance_travelled).distance
 	
+	banked_steps = 0
 	distance_earned = 0
 	hours_passed = 0
 	daily_customers = 0
@@ -336,7 +348,7 @@ func process_puzzle_result() -> void:
 		skip_remaining_cutscenes = true
 	
 	if skip_remaining_cutscenes:
-		# skip career cutscenes if they fail a boss level
+		# skip career cutscenes if they skip a level, or if they fail a boss level
 		CutsceneManager.reset()
 
 ## Applies some of the player's distance earned, either advancing the player or throwing it away.
@@ -353,7 +365,7 @@ func process_puzzle_result() -> void:
 ## 		throwing it away.
 func _apply_distance_earned(unapplied_distance_earned: int) -> int:
 	var region: CareerRegion = CareerLevelLibrary.region_for_distance(distance_travelled)
-	var newly_discarded_steps := 0
+	var newly_banked_steps := 0
 	var newly_travelled_distance := unapplied_distance_earned
 	
 	if distance_travelled + unapplied_distance_earned >= region.distance + region.length:
@@ -366,11 +378,12 @@ func _apply_distance_earned(unapplied_distance_earned: int) -> int:
 		else:
 			# The player can't cross into the next region, they haven't cleared the boss level. Move them to the end
 			# of this region and forbid movement.
-			newly_discarded_steps = unapplied_distance_earned - distance_to_next_region + 1
+			newly_banked_steps = unapplied_distance_earned - distance_to_next_region + 1
 	
-	# if the player hit a wall, we throw away some of their steps and don't move them as far
-	newly_travelled_distance -= newly_discarded_steps
-	unapplied_distance_earned -= newly_discarded_steps
+	# if the player hit a wall, we bank their steps and don't move them as far this time
+	banked_steps += newly_banked_steps
+	newly_travelled_distance -= newly_banked_steps
+	unapplied_distance_earned -= newly_banked_steps
 	
 	distance_travelled += newly_travelled_distance
 	unapplied_distance_earned -= newly_travelled_distance

project/src/test/world/test-career-data.gd
@@ -22,40 +22,75 @@ func test_prev_daily_earnings() -> void:
 
 
 func test_advance_clock_stops_at_boss_level_0() -> void:
+	_data.max_distance_travelled = 0
 	_data.advance_clock(50, false)
 	assert_eq(_data.distance_earned, 50)
 	assert_eq(_data.distance_travelled, 9)
+	assert_eq(_data.banked_steps, 41)
 
 
 func test_advance_clock_stops_at_boss_level_1() -> void:
+	_data.max_distance_travelled = 0
 	_data.distance_travelled = 8 # just before a boss level
 	_data.advance_clock(3, true)
 	assert_eq(_data.distance_earned, 3)
 	assert_eq(_data.distance_travelled, 9)
+	assert_eq(_data.banked_steps, 2)
 
 
 ## The player skips one boss level, but gets stopped by the next boss level
 func test_advance_clock_skips_boss_level() -> void:
-	PlayerData.career.max_distance_travelled = 10 # they've only cleared the first boss level
+	_data.max_distance_travelled = 10 # they've only cleared the first boss level
 	_data.advance_clock(50, false)
 	assert_eq(_data.distance_earned, 50)
 	assert_eq(_data.distance_travelled, 19)
+	assert_eq(_data.banked_steps, 31)
 
 
 func test_advance_clock_clears_boss_level() -> void:
+	_data.max_distance_travelled = 0
 	_data.distance_travelled = 9 # boss level
 	_data.advance_clock(4, true)
 	assert_eq(_data.distance_earned, 4)
 	assert_eq(_data.distance_travelled, 13)
+	assert_eq(_data.banked_steps, 0)
 
 
 func test_advance_clock_fails_boss_level() -> void:
+	_data.max_distance_travelled = 0
 	_data.distance_travelled = 9 # boss level
 	_data.advance_clock(4, false)
 	assert_true(_data.distance_earned < 0,
 			"distance_earned should be less than 0 but was %s" % [_data.distance_earned])
 	assert_true(_data.distance_travelled < 9,
 			"distance_travelled should be less than 9 but was %s" % [_data.distance_travelled])
+	assert_eq(_data.banked_steps, 0)
+
+
+func test_advance_clock_banked_steps_for_failed_boss_level() -> void:
+	_data.max_distance_travelled = 0
+	_data.banked_steps = 5
+	_data.distance_travelled = 9 # boss level
+	_data.advance_clock(4, false)
+	assert_true(_data.distance_earned < 0,
+			"distance_earned should be less than 0 but was %s" % [_data.distance_earned])
+	assert_eq(_data.banked_steps, 5)
+
+
+func test_advance_clock_banked_steps_for_failed_level() -> void:
+	_data.banked_steps = 5
+	_data.advance_clock(0, false)
+	assert_eq(_data.distance_earned, 0)
+	assert_eq(_data.distance_travelled, 0)
+	assert_eq(_data.banked_steps, 5)
+
+
+func test_advance_clock_banked_steps_for_finished_level() -> void:
+	_data.banked_steps = 5
+	_data.advance_clock(1, false)
+	assert_eq(_data.distance_earned, 6)
+	assert_eq(_data.distance_travelled, 6)
+	assert_eq(_data.banked_steps, 0)
 
 
 func test_distance_penalties_short() -> void:
@@ -103,5 +138,6 @@ func test_distance_penalties_negative() -> void:
 
 
 func test_distance_penalties_boss_level() -> void:
+	_data.max_distance_travelled = 0
 	_data.advance_clock(50, false)
 	assert_eq(_data.distance_penalties(), [0, 0, 0])

commit 890c3a09d336d37894ce71aec69696a635fe8af2

Fixed career distance label, breadcrumb trail
Distance label was incorrectly displaying values like '4 (+4) instead of
'53 (+4)'. This was broken in 8c356a5d when adding the 'banked steps'
logic.

Career map now initializes a breadcrumb trail, if one is not already
initialized. This avoids bugs when launnching CareerMap directly from
the editor.

project/src/main/ui/career/career-distance-label.gd
@@ -21,9 +21,9 @@ func _refresh_label() -> void:
 	var focused_level_button_index := _level_select.focused_level_button_index()
 	var distance_penalty: int = PlayerData.career.distance_penalties()[focused_level_button_index]
 	
-	# Display the distance travelled with the distance penalty applied
+	# Display the distance travelled and distance earned with the distance penalty applied
+	_label.text = StringUtils.comma_sep(PlayerData.career.distance_travelled - distance_penalty)
 	var distance_earned_value := PlayerData.career.distance_earned - distance_penalty
-	_label.text = StringUtils.comma_sep(distance_earned_value)
 	
 	if distance_earned_value > 0:
 		# distance_earned is positive; prefix distance_option with a '+'

project/src/main/world/career-map.gd
@@ -21,6 +21,12 @@ onready var _level_select_control := $LevelSelect
 
 func _ready() -> void:
 	ResourceCache.substitute_singletons()
+	
+	if not Breadcrumb.trail:
+		# For developers accessing the Overworld scene directly, we initialize a default Breadcrumb trail.
+		# For regular players the Breadcrumb trail will already be initialized by the menus.
+		Breadcrumb.initialize_trail()
+	
 	MusicPlayer.play_chill_bgm()
 	PlayerData.career.connect("distance_travelled_changed", self, "_on_CareerData_distance_travelled_changed")
 	