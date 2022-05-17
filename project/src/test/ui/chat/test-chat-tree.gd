727ba8279892ac6f8ec7ef6ded5e4dcafc4339b5
Poobslag <aaron.pieper@gmail.com>
2022-05-17 09:57:24 -0400

    Conditionally exclude cutscenes with sensei/restaurant
    Added logic to conditionally exclude cutscenes with Fat Sensei and the
    restaurant. Early chapters (and perhaps later chapters) will have the
    player by themselves, so we don't want a random cutscene which doesn't
    make sense where Fat Sensei shows up.

---
+++ project/src/test/ui/chat/test-chat-tree.gd
@@ -0,0 +1,22 @@
+extends "res://addons/gut/test.gd"
+
+var chat_tree := ChatTree.new()
+
+func test_inside_restaurant() -> void:
+	assert_eq(chat_tree.inside_restaurant(), false)
+	
+	chat_tree.location_id = "marsh"
+	assert_eq(chat_tree.inside_restaurant(), false)
+	
+	chat_tree.location_id = "marsh/inside_turbo_fat"
+	assert_eq(chat_tree.inside_restaurant(), true)
+
+
+func test_has_sensei() -> void:
+	assert_eq(chat_tree.has_sensei(), false)
+	
+	chat_tree.spawn_locations[CreatureLibrary.PLAYER_ID] = "kitchen_7"
+	assert_eq(chat_tree.has_sensei(), false)
+	
+	chat_tree.spawn_locations[CreatureLibrary.SENSEI_ID] = "kitchen_5"
+	assert_eq(chat_tree.has_sensei(), true)
