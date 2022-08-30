5b00047be3294b60aa6225d45eacabd0578fee1b
Poobslag <aaron.pieper@gmail.com>
2022-08-30 11:05:39 -0400

    New loading screen.
    New loading screen has an orb which floats around chucking pieces at a
    loading bar. The pieces can be rotated or aimed with the arrow keys.

---
+++ project/src/main/ui/loading-piece.gd
@@ -0,0 +1,97 @@
+extends Sprite
+## A puzzle piece which appears on the loading screen.
+##
+## The puzzle piece has a 3-part lifecycle:
+## 	1. Flying towards the loading bar from the 'source' to the 'target'
+## 	2. Crawling along the loading bar at a constant rate
+## 	3. Fading out and deleting itself
+
+## The speed at which pieces are launched from the orb
+const INITIAL_SPEED := 600
+
+## The duration for pieces to float towards the loading bar
+const PIECE_FLIGHT_DURATION := 1.0
+
+## The speed at which pieces crawl along the loading bar
+const CRAWL_SPEED := 120
+
+## The X coordinate which pieces crawl past on the loading bar, which makes them fade out
+const FADE_OUT_X := 80
+
+## The duration it takes pieces to fade out when they crawl off left side of the loading bar
+const FADE_OUT_DURATION := 0.3
+
+## The pieces's position/velocity/rotation as launched from the orb, without homing them towards the loading bar
+var _source_position: Vector2
+var _source_velocity: Vector2
+var _source_rotation: float
+
+## The pieces's target position/rotation on the loading bar, ignoring how they were originally launched
+var _target_position: Vector2
+var _target_rotation: float
+
+## The number of seconds elapsed since the piece was launched
+var _total_time: float
+
+## How the piece should wobble on the loading bar
+var _wobble_amount := rand_range(0.02, 0.08)
+var _wobble_period := rand_range(0.98, 1.63)
+var _wobble_offset := rand_range(0, 10)
+
+## 'true' if the piece is fading out, having crawled off the left side of the loading bar
+var _fading := false
+
+## the progress bar the piece should home in on
+var _progress_bar: LoadingProgressBar
+
+## Parameters:
+## 	'init_orb': The orb which is launching this puzzle piece
+##
+## 	'init_progress_bar': The progress bar this puzzle piece should home in on
+func initialize(init_orb: LoadingOrb, init_progress_bar: LoadingProgressBar) -> void:
+	_progress_bar = init_progress_bar
+	scale = init_orb.scale
+	modulate = init_orb.modulate
+	_source_position = init_orb.position
+	_source_velocity = init_orb.pop_launch_dir() * INITIAL_SPEED
+	_source_rotation = init_orb.rotation
+	frame = init_orb.frame
+	
+	_refresh()
+
+
+func _process(delta: float) -> void:
+	_total_time += delta
+	
+	# update source position/rotation
+	_source_position += _source_velocity * delta
+	
+	# update target position/rotation
+	if _total_time > PIECE_FLIGHT_DURATION:
+		_target_position += Vector2.LEFT * CRAWL_SPEED * delta
+	else:
+		_target_position = _progress_bar.progress_point()
+	
+	_refresh()
+
+
+## Recalculates our position/rotation based on whether we're flying/crawling and how much time has elapsed.
+func _refresh() -> void:
+	_target_rotation = _source_rotation + _wobble_amount * sin(_wobble_period * (_total_time + _wobble_offset))
+	
+	# lerp between source/target
+	var flight_amount := clamp(_total_time / PIECE_FLIGHT_DURATION, 0, 1)
+	flight_amount = pow(flight_amount, 1.5)
+	position = lerp(_source_position, _target_position, flight_amount)
+	rotation = lerp(_source_rotation, _target_rotation, flight_amount)
+	
+	if _total_time > PIECE_FLIGHT_DURATION and position.x < FADE_OUT_X and not _fading:
+		# if we're at our target position and it's below a threshold, fade out
+		_fading = true
+		var tween := create_tween()
+		tween.tween_property(self, "modulate", Utils.to_transparent(modulate), FADE_OUT_DURATION)
+		tween.connect("finished", self, "_on_FadeTween_finished")
+
+
+func _on_FadeTween_finished() -> void:
+	queue_free()
---
+++ project/src/main/ui/menu/loading-orb.gd
@@ -0,0 +1,99 @@
+class_name LoadingOrb
+extends Sprite
+## An orb which floats around the loading screen, launching puzzle pieces.
+##
+## The player can control the launched puzzle pieces with the direction/rotate buttons.
+
+## The number of frames in our animation. Each frame launches a different puzzle piece.
+const FRAME_COUNT := 8
+
+## The rate at which to launch puzzle pieces.
+const PIECES_PER_SECOND := 3.37
+
+## The loading orb rotates and moves. This field is used to calculate the rotation/position
+var _total_time := 0.0
+
+## Sequential pieces launch in different directions. This field influences the launch direction.
+var _launched_piece_count := randi() % 4
+
+## The most recent direction the player pressed.
+var _pressed_dir := Vector2.ZERO
+var _orientation: float
+
+func _ready() -> void:
+	# initialize total_time so the wobble/position isn't always the same
+	_total_time = rand_range(0, 100)
+	
+	if randf() < 0.5:
+		# 50% of the time, the pieces are oriented to spell out 'T' 'u' 'r' 'b' 'o'...
+		_orientation = 0.0
+	else:
+		# 50% of the time, their orientation is randomized
+		_orientation = Utils.rand_value([0.0 * PI, 0.5 * PI, 1.0 * PI, 1.5 * PI])
+	
+	if randf() < 0.5:
+		# 50% of the time, the pieces are ordered with the 'T' piece first.
+		frame = 0
+	else:
+		# 50% of the time, their order is random
+		frame = randi() % FRAME_COUNT
+	
+	_refresh()
+
+
+## The player can control the direction/orientation of launched puzzle pieces with the rotate buttons.
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		# If the player presses one or more directions, the pieces are launched in different directions.
+		var new_pressed_dir := Vector2.ZERO
+		if event.is_action_pressed("ui_right"): new_pressed_dir += Vector2.RIGHT
+		if event.is_action_pressed("ui_left"): new_pressed_dir += Vector2.LEFT
+		if event.is_action_pressed("ui_up"): new_pressed_dir += Vector2.UP
+		if event.is_action_pressed("ui_down"): new_pressed_dir += Vector2.DOWN
+		if new_pressed_dir != Vector2.ZERO:
+			_pressed_dir = new_pressed_dir
+		
+		# If the player presses one or more rotate keys, the orb rotates.
+		if event.is_action_pressed("rotate_cw") or event.is_action_pressed("rotate_ccw"):
+			var should_rotate_cw := event.is_action_pressed("rotate_cw")
+			
+			# The player can press both rotate buttons to flip the orb, just like in the game.
+			if Input.is_action_pressed("rotate_cw") and Input.is_action_pressed("rotate_ccw"):
+				should_rotate_cw = not should_rotate_cw
+			
+			_orientation = fmod(_orientation + (PI / 2 if should_rotate_cw else -PI / 2), 2 * PI)
+
+
+func _process(delta: float) -> void:
+	_total_time += delta
+	
+	_refresh()
+
+
+## Calculates the direction to launch the piece.
+##
+## If the orb is currently launching pieces in clockwise order, this also advances to the next sequential direction.
+func pop_launch_dir() -> Vector2:
+	var launch_dir: Vector2 = _pressed_dir
+	if launch_dir == Vector2.ZERO:
+		# launch pieces in a clockwise order
+		launch_dir = [Vector2.UP, Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT][_launched_piece_count % 4]
+	
+	_launched_piece_count += 1
+	return launch_dir
+
+
+## Recalculates the orb's position, rotation and frame based on the elapsed time.
+func _refresh() -> void:
+	position = Global.window_size * 0.5
+	
+	# The orb's path follows a small circle within a big circle like a spirograph
+	position += Vector2(40 * sin(2.3 * _total_time), 40 * cos(2.3 * _total_time)) # small circle
+	position += Vector2(120 * sin(0.8 * _total_time), -60 * cos(0.8 * _total_time)) # big circle
+	
+	var wobble_amount := 0.09 + 0.06 * sin(0.97 * _total_time)
+	rotation = PI * wobble_amount * sin(1.3 * _total_time) + _orientation
+	
+	var new_frame := int(_total_time * PIECES_PER_SECOND) % FRAME_COUNT
+	if frame != new_frame:
+		frame = new_frame
