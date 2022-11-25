Poobslag/turbofat

commit 52c1784ede1176842116a5e7f6c25e0010ef3518

Fixed bugs when interrupting/restarting tutorials.
When restarting tutorials after the 'Good!' message showed up, the piece
would never appear, softlocking the tutorial. This is because
PieceManager set physics_process to false during the level transition
but did not set it back to true. It now sets physics_process to true
at the start of a new game.

Tutorials would demonstrate weird behavior when interrupted, because
'yield' statements would continue from the previously run tutorial.
These 'yield' statements have been replaced with timers and listeners which are
cleaned up when a new tutorial is started.

The choices which appear when the player is watching tutorial diagrams
were lingering even if the player gave up on the tutorial. This let them
do strange things like choose tutorial options when they weren't playing
anymore. The choices are now hidden when the tutorial ends.

The TutorialModule framework now includes utility methods to make it
easier for tutorials to do something after a delay, or do something
after all messages are shown to the player.

Closes #1180.

project/src/main/puzzle/puzzle-state.gd
@@ -53,7 +53,7 @@ signal combo_ended
 ## emitted when the current piece can't be placed in the playfield
 signal topped_out
 
-const DELAY_NONE := 0.00
+const DELAY_NONE := 0.01
 const DELAY_SHORT := 2.05
 const DELAY_LONG := 3.30
 
@@ -171,12 +171,8 @@ func end_game() -> void:
 	emit_signal("after_game_ended")
 
 
-func change_level(level_id: String, delay_between_levels: float = DELAY_SHORT) -> void:
+func change_level(level_id: String) -> void:
 	emit_signal("before_level_changed", level_id)
-	
-	if delay_between_levels:
-		yield(get_tree().create_timer(delay_between_levels), "timeout")
-	
 	var settings := LevelSettings.new()
 	settings.load_from_resource(level_id)
 	CurrentLevel.switch_level(settings)

project/src/main/puzzle/tutorial/tutorial-basics-module.gd
@@ -63,7 +63,7 @@ func _advance_level() -> void:
 		# the player did something crazy; skip the tutorial entirely
 		
 		# change the level immediately; don't wait for dialog to finish
-		PuzzleState.change_level("tutorial/oh_my", false)
+		PuzzleState.change_level("tutorial/oh_my")
 		
 		hud.set_big_message(ChatLibrary.add_mega_lull_characters(tr("OH, MY!!!")))
 		hud.enqueue_pop_out()

project/src/main/puzzle/tutorial/tutorial-module.gd
@@ -4,11 +4,19 @@ extends Node
 ##
 ## Subclasses can show messages and advances the player through the tutorial as they complete tasks.
 
+## if 'true', the next level change will be accompanied by a 'Ready? Go!' countdown
+var _start_customer_countdown := false
+
 ## generic nodes used by tutorial module subclasses
 var hud: TutorialHud
 var puzzle: Puzzle
 var playfield: Playfield
 var piece_manager: PieceManager
+
+## Holds all temporary timers. These timers are not created by get_tree().create_timer() because we need to clean them
+## up if the tutorial is interrupted. Otherwise for example, we might schedule a level change 3 seconds from now, but
+## then the player restarts the tutorial, and the level change happens after they've restarted.
+onready var _timers := $Timers
 
 func _ready() -> void:
 	puzzle = hud.puzzle
@@ -16,6 +24,8 @@ func _ready() -> void:
 	piece_manager = puzzle.get_piece_manager()
 	
 	PuzzleState.connect("after_level_changed", self, "_on_PuzzleState_after_level_changed")
+	PuzzleState.connect("game_prepared", self, "_on_PuzzleState_game_prepared")
+	PuzzleState.connect("game_ended", self, "_on_PuzzleState_game_ended")
 	
 	for skill_tally_item in $SkillTallyItems.get_children():
 		if skill_tally_item is SkillTallyItem:
@@ -24,14 +34,22 @@ func _ready() -> void:
 			hud.add_skill_tally_item(new_item)
 
 
+## Cleanup any temporary timers and listeners.
+##
+## This prevents the tutorial from behaving unexpectedly if the player restarts at an unusual time.
+func cleanup_listeners() -> void:
+	for child in _timers.get_children():
+		child.queue_free()
+	
+	if hud.messages.is_connected("all_messages_shown", self, "_on_TutorialMessages_all_messages_shown_start_timer"):
+		hud.messages.disconnect("all_messages_shown", self, "_on_TutorialMessages_all_messages_shown_start_timer")
+
+
 ## Starts a countdown and switches from tutorial music to regular music.
 ##
 ## This is used at the end of each tutorial when customers come in.
 func start_customer_countdown() -> void:
-	yield(PuzzleState, "after_level_changed")
-	MusicPlayer.play_upbeat_bgm(false)
-	PuzzleState.game_active = true
-	puzzle.start_level_countdown()
+	_start_customer_countdown = true
 
 
 ## Resets the timer and scores and dismisses the sensei, so the player can serve some real customers.
@@ -68,18 +86,76 @@ func prepare_tutorial_level() -> void:
 func change_level(level_id: String, delay_between_levels: float = PuzzleState.DELAY_SHORT) -> void:
 	PuzzleState.emit_signal("before_level_changed", level_id)
 	
-	if not hud.messages.is_all_messages_visible():
-		yield(hud.messages, "all_messages_shown")
-	if delay_between_levels:
-		yield(get_tree().create_timer(delay_between_levels), "timeout")
+	start_timer_after_all_messages_shown(delay_between_levels) \
+			.connect("timeout", self, "_on_Timer_timeout_change_level", [level_id])
+
+
+## Creates and starts a timer after all tutorial messages are shown.
+##
+## Parameters:
+## 	'wait_time': The amount of time to wait after all messages are shown. A value of '0.0' will result in an error.
+##
+## Returns:
+## 	A timer which has been added to the scene tree. The timer may be active or inactive depending on whether the
+## 	tutorial messages are still being written to the user.
+func start_timer_after_all_messages_shown(wait_time: float) -> Timer:
+	var timer := Timer.new()
+	timer.one_shot = true
+	timer.wait_time = wait_time
+	_timers.add_child(timer)
 	
-	var settings := LevelSettings.new()
-	settings.load_from_resource(level_id)
-	CurrentLevel.switch_level(settings)
-	# initialize input_frame to allow for recording/replaying inputs
-	PuzzleState.input_frame = 0
-	PuzzleState.emit_signal("after_level_changed")
+	if not hud.messages.is_all_messages_visible():
+		hud.messages.connect("all_messages_shown", self, "_on_TutorialMessages_all_messages_shown_start_timer", [timer])
+	else:
+		timer.start()
+	
+	return timer
+
+
+## Creates and starts a timer.
+##
+## Parameters:
+## 	'wait_time': The amount of time to wait. A value of '0.0' will result in an error.
+##
+## Returns:
+## 	A timer which has been added to the scene tree, and is currently active.
+func start_timer(wait_time: float) -> Timer:
+	var timer := Timer.new()
+	_timers.add_child(timer)
+	timer.one_shot = true
+	timer.connect("timeout", self, "_on_Timer_timeout_queue_free", [timer])
+	timer.start(wait_time)
+	return timer
+
+
+func _on_TutorialMessages_all_messages_shown_start_timer(timer: Timer) -> void:
+	hud.messages.disconnect("all_messages_shown", self, "_on_TutorialMessages_all_messages_shown_start_timer")
+	timer.start()
+
+
+func _on_Timer_timeout_queue_free(timer: Timer) -> void:
+	timer.queue_free()
+
+
+func _on_Timer_timeout_change_level(level_id: String) -> void:
+	PuzzleState.change_level(level_id)
 
 
 func _on_PuzzleState_after_level_changed() -> void:
 	prepare_tutorial_level()
+	
+	if _start_customer_countdown:
+		_start_customer_countdown = false
+		MusicPlayer.play_upbeat_bgm(false)
+		PuzzleState.game_active = true
+		puzzle.start_level_countdown()
+
+
+func _on_PuzzleState_game_ended() -> void:
+	_start_customer_countdown = false
+	cleanup_listeners()
+
+
+func _on_PuzzleState_game_prepared() -> void:
+	_start_customer_countdown = false
+	cleanup_listeners()

project/src/demo/puzzle/PuzzleDemo.tscn
@@ -5,6 +5,6 @@
 
 [node name="PuzzleDemo" type="Node"]
 script = ExtResource( 2 )
-level_path = "res://assets/demo/puzzle/levels/experiment.json"
+level_path = "res://assets/main/puzzle/levels/tutorial/combo-0.json"
 
 [node name="Puzzle" parent="." instance=ExtResource( 1 )]

project/src/main/puzzle/piece/piece-manager.gd
@@ -305,6 +305,8 @@ func _on_States_entered_state(prev_state: State, state: State) -> void:
 
 
 func _on_PuzzleState_game_prepared() -> void:
+	# enable physics_process if it was temporarily disabled
+	set_physics_process(true)
 	_clear_piece()
 
 

project/src/main/puzzle/tutorial/TutorialBasicsModule.tscn
@@ -162,3 +162,5 @@ custom_styles/fg = SubResource( 1 )
 custom_styles/bg = SubResource( 2 )
 max_value = 2.0
 label_text = "Snack Stack"
+
+[node name="Timers" type="Node" parent="."]

project/src/main/puzzle/tutorial/TutorialCakesModule.tscn
@@ -54,3 +54,5 @@ custom_styles/fg = SubResource( 1 )
 custom_styles/bg = SubResource( 2 )
 max_value = 8.0
 label_text = "Cake Box"
+
+[node name="Timers" type="Node" parent="."]

project/src/main/puzzle/tutorial/TutorialComboModule.tscn
@@ -16,3 +16,5 @@ label_text = "Combo"
 visible = false
 max_value = 2.0
 label_text = "Cake Box"
+
+[node name="Timers" type="Node" parent="."]

commit ff1a60fe9842e68898a9e814d2c06006673437c7

Fixed redundant tutorial chimes.
Tutorial chimes were playing at the start and end of each section,
because the 'before_level_changed' signal was being emitted too
frequently. This bug was introduced in 52c1784 when fixing bugs related
to interrupting/restarting tutorials.

Closes #1782.

project/src/main/puzzle/puzzle-state.gd
@@ -17,6 +17,7 @@ signal game_started
 
 ## emitted during tutorials, when changing from one tutorial section to the next
 signal before_level_changed(new_level_id)
+
 signal after_level_changed
 
 ## emitted when the player survives until the end the level.
@@ -171,8 +172,14 @@ func end_game() -> void:
 	emit_signal("after_game_ended")
 
 
-func change_level(level_id: String) -> void:
+## Signals a level transition.
+##
+## During tutorials, this notifies the player that this section is over. It also pauses time-sensitive puzzle logic.
+func prepare_level_change(level_id: String) -> void:
 	emit_signal("before_level_changed", level_id)
+
+
+func change_level(level_id: String) -> void:
 	var settings := LevelSettings.new()
 	settings.load_from_resource(level_id)
 	CurrentLevel.switch_level(settings)

project/src/main/puzzle/tutorial/tutorial-basics-module.gd
@@ -63,7 +63,9 @@ func _advance_level() -> void:
 		# the player did something crazy; skip the tutorial entirely
 		
 		# change the level immediately; don't wait for dialog to finish
-		PuzzleState.change_level("tutorial/oh_my")
+		var level_id := "tutorial/oh_my"
+		PuzzleState.prepare_level_change(level_id)
+		PuzzleState.change_level(level_id)
 		
 		hud.set_big_message(ChatLibrary.add_mega_lull_characters(tr("OH, MY!!!")))
 		hud.enqueue_pop_out()

project/src/main/puzzle/tutorial/tutorial-module.gd
@@ -84,8 +84,7 @@ func prepare_tutorial_level() -> void:
 ##
 ## Copy/pasted from PuzzleState.change_level with an extra yield statement added.
 func change_level(level_id: String, delay_between_levels: float = PuzzleState.DELAY_SHORT) -> void:
-	PuzzleState.emit_signal("before_level_changed", level_id)
-	
+	PuzzleState.prepare_level_change(level_id)
 	start_timer_after_all_messages_shown(delay_between_levels) \
 			.connect("timeout", self, "_on_Timer_timeout_change_level", [level_id])
 