e00f67bbb84717d64f609125b53dfa12726320de
Poobslag <aaron.pieper@gmail.com>
2022-12-13 10:20:27 -0500

    Refactor: Extracted PagedRegionPanel. 'chosen' -> 'focused'
    Extracted PagedRegionPanel.tscn from PracticeMenu.tscn.  This provides symmetry
    with PagedLevelPanel and also better encapsulates the region panel's signals.
    Rather than needing to listen to RegionSubmenu/Panel/VBoxContainer/Top
    /RegionButtons, the panel listens to RegionSubmenu/Panel.
    
    Renamed 'level_chosen' signals to 'level_focused', to differentiate
    between buttons being highlighted and buttons being clicked twice to
    select the level.

---
+++ project/src/main/ui/menu/paged-region-panel.gd
@@ -0,0 +1,8 @@
+extends Panel
+## A region select panel which shows buttons and region info. Used in the practice menu.
+
+## Emitted when the player finishes choosing a region to play.
+signal region_chosen(region)
+
+func _on_RegionButtons_region_chosen(region: Object) -> void:
+	emit_signal("region_chosen", region)
