Poobslag/turbofat

commit c081854f632d4e26cc1f51c737ac5ab2c6ab7fe2

Player data is saved during scene transitions.
Writing the player save JSON file takes about 50 ms, which is enough to
cause a noticable stutter. When adding a 'swoosh' effect to end-of-level
messages, for example, this effect was interrupted by the save data
being written.

Player data saves are now scheduled to occur between levels. I think
this might also help the HTML5 builds, where these stutters would affect
music playback.

project/src/main/career/career-flow.gd
@@ -83,7 +83,7 @@ func _preapply_failure_penalties() -> void:
 	var temp_hours_passed := career_data.hours_passed
 	career_data.advance_clock(0, false)
 	career_data.skipped_previous_level = true
-	PlayerSave.save_player_data()
+	PlayerSave.schedule_save()
 	career_data.distance_earned = temp_distance_earned
 	career_data.distance_travelled = temp_distance_travelled
 	career_data.hours_passed = temp_hours_passed

project/src/main/player-save.gd
@@ -26,9 +26,14 @@ var legacy_filename := "user://turbofat0.save" setget set_legacy_filename
 ## Provides backwards compatibility with older save formats
 var _upgrader := PlayerSaveUpgrader.new().new_save_item_upgrader()
 
+## 'true' if player data will be saved during the next scene transition
+var _save_scheduled := false
+
 func _ready() -> void:
 	rolling_backups.data_filename = data_filename
 	rolling_backups.legacy_filename = legacy_filename
+	
+	Breadcrumb.connect("before_scene_changed", self, "_on_Breadcrumb_before_scene_changed")
 
 
 func get_corrupt_filenames() -> Array:
@@ -47,6 +52,14 @@ func set_data_filename(new_data_filename: String) -> void:
 func set_legacy_filename(new_legacy_filename: String) -> void:
 	legacy_filename = new_legacy_filename
 	rolling_backups.legacy_filename = legacy_filename
+
+
+## Schedules player data to be saved later.
+##
+## Serializing the player's in-memory data into a large JSON file takes about 50 milliseconds or longer. To prevent
+## stuttering or frame drops, we do this during scene transitions.
+func schedule_save() -> void:
+	_save_scheduled = true
 
 
 ## Writes the player's in-memory data to a save file.
@@ -216,3 +229,9 @@ func _load_line(type: String, key: String, json_value) -> void:
 			PlayerData.practice.from_json_dict(value)
 		_:
 			push_warning("Unrecognized save data type: '%s'" % type)
+
+
+func _on_Breadcrumb_before_scene_changed() -> void:
+	if _save_scheduled:
+		save_player_data()
+		_save_scheduled = false

project/src/main/editor/creature/dialogs.gd
@@ -68,4 +68,4 @@ func _on_SaveButton_pressed() -> void:
 ## Updates the player character and writes it to their save file.
 func _on_SaveConfirmation_confirmed() -> void:
 	PlayerData.creature_library.player_def = _creature_editor.center_creature.creature_def
-	PlayerSave.save_player_data()
+	PlayerSave.schedule_save()

project/src/main/puzzle/puzzle.gd
@@ -12,7 +12,6 @@ func _ready() -> void:
 	
 	PuzzleState.connect("game_started", self, "_on_PuzzleState_game_started")
 	PuzzleState.connect("game_ended", self, "_on_PuzzleState_game_ended")
-	PuzzleState.connect("after_game_ended", self, "_on_PuzzleState_after_game_ended")
 	PuzzleState.connect("after_level_changed", self, "_on_PuzzleState_after_level_changed")
 	$Fg/Playfield/TileMapClip/TileMap/Viewport/ShadowMap.piece_tile_map = $Fg/PieceManager/TileMap
 	$Fg/PieceManager.connect(
@@ -247,28 +246,23 @@ func _on_PuzzleState_game_ended() -> void:
 	if not CurrentLevel.level_id:
 		# null check to avoid errors when launching Puzzle.tscn standalone
 		return
-	
+
 	_settings_menu.quit_type = SettingsMenu.QUIT
 	var rank_result := RankCalculator.new().calculate_rank()
 	PlayerData.level_history.add_result(CurrentLevel.level_id, rank_result)
 	PlayerData.level_history.prune(CurrentLevel.level_id)
 	PlayerData.emit_signal("level_history_changed")
 	PlayerData.money = int(clamp(PlayerData.money + rank_result.score, 0, PlayerData.MAX_MONEY))
-	
+
 	if PlayerData.career.is_career_mode() and CurrentLevel.attempt_count == 0:
 		_update_career_data(rank_result)
-	
+
 	if not PuzzleState.level_performance.lost and _overall_rank(rank_result) < 24: $ApplauseSound.play()
-	
+
 	CurrentLevel.best_result = max(CurrentLevel.best_result, PuzzleState.end_result())
 	CurrentLevel.attempt_count += 1
-
-
-## Wait until after the game ends to save the player's data.
-##
-## This makes the stutter from writing to disk less noticable.
-func _on_PuzzleState_after_game_ended() -> void:
-	PlayerSave.save_player_data()
+	
+	PlayerSave.schedule_save()
 
 
 ## Briefly pause during level transitions.

project/src/main/ui/career/career-win.gd
@@ -14,7 +14,7 @@ func _ready() -> void:
 	_refresh_mood()
 	
 	PlayerData.career.advance_calendar()
-	PlayerSave.save_player_data()
+	PlayerSave.schedule_save()
 	
 	_button.grab_focus()
 

commit 2798f9c36b7c43d645e44943cd99249dea87044e

Fixed career savescum bug.
The game had a savescum prevention feature which sabotaged the player's
save data before starting a new puzzle. However, this was broken in c081854f632d4e26cc1f51c737ac5ab2c6ab7fe2
when the save data was replaced with an asynchronous system which didn't
save right away.

I've expanded this code so that it is asynchronous as well, and the
player's save data is temporarily sabotaged immediately before being
written to disk, and then restored.

project/src/main/career/career-flow.gd
@@ -79,12 +79,33 @@ func should_play_prologue() -> bool:
 
 ## Applies penalties for skipping a level to the player's save data, so they can't quit and retry.
 func _preapply_failure_penalties() -> void:
-	var temp_distance_earned := career_data.distance_earned
-	var temp_distance_travelled := career_data.distance_travelled
-	var temp_hours_passed := career_data.hours_passed
-	career_data.advance_clock(0, false)
-	career_data.skipped_previous_level = true
+	if PlayerSave.is_connected("before_save", self, "_on_PlayerSave_before_save"):
+		PlayerSave.disconnect("before_save", self, "_on_PlayerSave_before_save")
+	PlayerSave.connect("before_save", self, "_on_PlayerSave_before_save")
+	
+	if PlayerSave.is_connected("after_save", self, "_on_PlayerSave_after_save"):
+		PlayerSave.disconnect("after_save", self, "_on_PlayerSave_after_save")
+	PlayerSave.connect("after_save", self, "_on_PlayerSave_after_save",
+			[career_data.distance_earned, career_data.distance_travelled, career_data.hours_passed])
+	
 	PlayerSave.schedule_save()
-	career_data.distance_earned = temp_distance_earned
-	career_data.distance_travelled = temp_distance_travelled
-	career_data.hours_passed = temp_hours_passed
+
+
+func _on_PlayerSave_before_save() -> void:
+	# Temporarily sabotage the player's career progress to punish them if they try to savescum when losing a puzzle.
+	career_data.advance_clock(0, false)
+	
+	# Set the 'skipped_previous_level' flag; this will be reset to false if the player finishes a puzzle.
+	career_data.skipped_previous_level = true
+	
+	PlayerSave.disconnect("before_save", self, "_on_PlayerSave_before_save")
+
+
+func _on_PlayerSave_after_save(distance_earned, distance_travelled, hours_passed) -> void:
+	# Restore the player's distance earned, distance travelled and hours passed. The 'skipped_previous_level' flag is
+	# still set to true; this is reset to false after the player finishes a puzzle.
+	career_data.distance_earned = distance_earned
+	career_data.distance_travelled = distance_travelled
+	career_data.hours_passed = hours_passed
+	
+	PlayerSave.disconnect("after_save", self, "_on_PlayerSave_after_save")

project/src/main/data/player-save.gd
@@ -3,6 +3,10 @@ extends Node
 ##
 ## This data includes how well they've done on each level and how much money they've earned.
 
+signal before_save
+
+signal after_save
+
 ## Current version for saved player data. Should be updated if and only if the player format changes.
 ## This version number follows a 'ymdh' hex date format which is documented in issue #234.
 const PLAYER_DATA_VERSION := "38c3"
@@ -66,6 +70,7 @@ func schedule_save() -> void:
 
 ## Writes the player's in-memory data to a save file.
 func save_player_data() -> void:
+	emit_signal("before_save")
 	var save_json := []
 	save_json.append(_save_item("version", PLAYER_DATA_VERSION).to_json_dict())
 	var player_info := {}
@@ -87,6 +92,7 @@ func save_player_data() -> void:
 			PlayerData.level_history.finished_levels).to_json_dict())
 	FileUtils.write_file(data_filename, Utils.print_json(save_json))
 	rolling_backups.rotate_backups()
+	emit_signal("after_save")
 
 
 ## Populates the player's in-memory data based on their save files.