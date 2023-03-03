e8785e1d5f800dc91189e7095be8cb6feba1c3e9
Poobslag <aaron.pieper@gmail.com>
2023-03-03 14:51:00 -0500

    Fixed bugs with onion, nighttime when restarting levels.
    These bugs especially affected levels that begin at nighttime and toggle to
    daytime. When restarting in nighttime, sometimes the onion wouldn't
    launch, or it would launch even though it was already in the sky.
    
    The onion now has the ability to skip to the 'floating in the air' mode
    which is used when restarting a nighttime level.

--- project/src/demo/puzzle/critter/onion-demo.gd
+++ project/src/demo/puzzle/critter/onion-demo.gd
@@ -7,6 +7,7 @@ extends Node
 ## [2]: Dance End
 ## [3]: Night
 ## [4]: Disappear
+## [N]: Skip to night mode
 
 onready var _onion := $Onion
 
@@ -17,3 +18,4 @@ func _input(event: InputEvent) -> void:
 		KEY_2: _onion.state = OnionConfig.OnionState.DAY_END
 		KEY_3: _onion.state = OnionConfig.OnionState.NIGHT
 		KEY_4: _onion.state = OnionConfig.OnionState.NONE
+		KEY_N: _onion.skip_to_night_mode()
