Poobslag/turbofat

commit d6cdb627fb41e34805edf73ea1cc3d34a90f0f4b

Renamed restaurant creature_index -> customer_index
'customer_index' is more specific. There are creatures in the scene that
are not customers (the chef).

project/src/main/puzzle/restaurant-view.gd
@@ -17,7 +17,7 @@ const MOUTH_POSITIONS_BY_ORIENTATION := {
 const SWOOP_DURATION := 0.8
 
 ## virtual property; value is only exposed through getters/setters
-var current_creature_index: int setget set_current_creature_index, get_current_creature_index
+var current_customer_index: int setget set_current_customer_index, get_current_customer_index
 
 ## bonus points scored for recent lines; used for determining when the chef should smile
 var _recent_bonuses := []
@@ -28,10 +28,10 @@ var _customer_offscreen_rect_position: Vector2
 var _chef_onscreen_rect_position: Vector2
 var _chef_offscreen_rect_position: Vector2
 
-## Ordered list of int indexes which will be cycled to with the next_creature_index or scroll_to_new_creature methods.
+## Ordered list of int indexes which will be cycled to with the next_customer_index or scroll_to_new_customer methods.
 ##
 ## If empty, the ordering will be purely random.
-var _next_creature_indexes := []
+var _next_customer_indexes := []
 
 onready var _chef := $Chef
 onready var _chef_nametag_panel := $Chef/Nametag/Panel
@@ -44,7 +44,7 @@ onready var _customer_view := $Customer/View
 onready var _restaurant_viewport_scene := $RestaurantViewport/Scene
 onready var _swoop_tween: Tween = $SwoopTween
 onready var _hello_timer := $HelloTimer
-onready var _summon_creature_timers: TimerGroup = $SummonCreatureTimers
+onready var _summon_customer_timers: TimerGroup = $SummonCustomerTimers
 
 func _ready() -> void:
 	# Godot doesn't like when ViewportContainers have a different size from their Viewport, so we can't set
@@ -62,7 +62,7 @@ func _ready() -> void:
 		customer.connect("creature_name_changed", self, "_on_Customer_creature_name_changed")
 		customer.connect("dna_loaded", self, "_on_Customer_dna_loaded", [customer])
 	_refresh_chef_name()
-	_refresh_customer_name()
+	_refresh_creature_name()
 	
 	_reset_bubbles_offscreen()
 
@@ -70,7 +70,7 @@ func _ready() -> void:
 ## Resets any cached data to prepare for a new puzzle.
 func reset() -> void:
 	_recent_bonuses.clear()
-	_next_creature_indexes.clear()
+	_next_customer_indexes.clear()
 
 
 ## Moves the chef/customer bubbles offscreen, recording their position for later
@@ -105,18 +105,18 @@ func swoop_customer_bubble_offscreen() -> void:
 	_swoop_bubble(_customer, false)
 
 
-## Pans the camera to a new creature. This also changes which creature will be fed.
-func set_current_creature_index(new_index: int) -> void:
-	_restaurant_viewport_scene.current_creature_index = new_index
+## Pans the camera to a new customer. This also changes which customer will be fed.
+func set_current_customer_index(new_index: int) -> void:
+	_restaurant_viewport_scene.current_customer_index = new_index
 	emit_signal("customer_changed")
 
 
-func get_current_creature_index() -> int:
-	return _restaurant_viewport_scene.current_creature_index
+func get_current_customer_index() -> int:
+	return _restaurant_viewport_scene.current_customer_index
 
 
-func get_customer(creature_index: int = -1) -> Creature:
-	return _restaurant_viewport_scene.get_customer(creature_index)
+func get_customer(customer_index: int = -1) -> Creature:
+	return _restaurant_viewport_scene.get_customer(customer_index)
 
 
 func get_chef() -> Creature:
@@ -141,54 +141,58 @@ func get_customer_mouth_position(customer: Creature) -> Vector2:
 	return target_pos
 
 
-func find_creature_index_with_id(creature_id: String) -> int:
-	var creature_index := -1
+func find_customer_index_with_id(customer_id: String) -> int:
+	var customer_index := -1
 	var customers := get_customers()
 	
 	for i in range(customers.size()):
-		if customers[i].creature_def.creature_id == creature_id:
-			creature_index = i
+		if customers[i].customer_def.customer_id == customer_id:
+			customer_index = i
 			break
 	
-	return creature_index
+	return customer_index
 
 
-## Recolors the creature according to the specified creature definition. This involves updating shaders and sprite
-## properties.
-func summon_customer(creature_index: int = -1) -> void:
-	var creature_def := CreatureDef.new()
+## Dequeues a customer from the customer queue, summoning them to the restaurant.
+##
+## The specified customer has their appearance updated according to the next customer_def in the customer queue.
+##
+## Parameters:
+## 	'customer_index': (Optional) The customer to be altered. Defaults to the current customer.
+func summon_customer(customer_index: int = -1) -> void:
+	var customer_def := CreatureDef.new()
 	if PlayerData.customer_queue.has_priority_customer():
-		creature_def = PlayerData.customer_queue.pop_priority_customer()
-		_next_creature_indexes.append(creature_index)
+		customer_def = PlayerData.customer_queue.pop_priority_customer()
+		_next_customer_indexes.append(customer_index)
 	else:
-		creature_def = PlayerData.random_customer_def(true)
-	_restaurant_viewport_scene.summon_customer(creature_def, creature_index)
-	if creature_index == -1 or creature_index == get_current_creature_index():
+		customer_def = PlayerData.random_customer_def(true)
+	_restaurant_viewport_scene.summon_customer(customer_def, customer_index)
+	if customer_index == -1 or customer_index == get_current_customer_index():
 		emit_signal("customer_changed")
 
 
-## Scroll to a new creature and replace the old creature.
-func scroll_to_new_creature(new_creature_index: int = -1) -> void:
-	var old_creature_index: int = get_current_creature_index()
-	if new_creature_index == -1:
-		new_creature_index = next_creature_index()
-	set_current_creature_index(new_creature_index)
+## Scroll to a new customer and replace the old customer.
+func scroll_to_new_customer(new_customer_index: int = -1) -> void:
+	var old_customer_index: int = get_current_customer_index()
+	if new_customer_index == -1:
+		new_customer_index = next_customer_index()
+	set_current_customer_index(new_customer_index)
 	_restaurant_viewport_scene.get_customer().restart_idle_timer()
 	
-	var summon_creature_timer := _summon_creature_timers.start_timer(0.5)
-	summon_creature_timer.connect("timeout", self, "_on_Timer_timeout_summon_customer", [old_creature_index])
+	var summon_customer_timer := _summon_customer_timers.start_timer(0.5)
+	summon_customer_timer.connect("timeout", self, "_on_Timer_timeout_summon_customer", [old_customer_index])
 
 
-## Returns a random creature index different from the current creature index.
-func next_creature_index() -> int:
+## Returns a random customer index different from the current customer index.
+func next_customer_index() -> int:
 	var result: int
-	if _next_creature_indexes:
+	if _next_customer_indexes:
 		# return queued result
-		result = _next_creature_indexes.pop_front()
+		result = _next_customer_indexes.pop_front()
 	else:
 		# return random result
 		var customer_count := get_customers().size()
-		result = (get_current_creature_index() + randi() % (customer_count - 1) + 1) % customer_count
+		result = (get_current_customer_index() + randi() % (customer_count - 1) + 1) % customer_count
 	
 	return result
 
@@ -218,7 +222,7 @@ func _swoop_bubble(bubble: Control, onscreen: bool) -> void:
 	_swoop_tween.start()
 
 
-func _refresh_customer_name() -> void:
+func _refresh_creature_name() -> void:
 	_customer_nametag_panel.refresh_creature(get_customer())
 
 
@@ -252,7 +256,7 @@ func _on_PuzzleState_combo_changed(value: int) -> void:
 		pass
 	elif PuzzleState.game_active:
 		_hello_timer.maybe_play_goodbye_voice(get_customer())
-		scroll_to_new_creature()
+		scroll_to_new_customer()
 	
 	# losing your combo doesn't erase the 'recent bonus' value, but decreases it a lot
 	_recent_bonuses = _recent_bonuses.slice(3, _recent_bonuses.size() - 1)
@@ -300,11 +304,11 @@ func _on_PuzzleState_game_ended() -> void:
 
 
 func _on_Customer_creature_name_changed() -> void:
-	_refresh_customer_name()
+	_refresh_creature_name()
 
 
-func _on_RestaurantPuzzleScene_current_creature_index_changed(_value: int) -> void:
-	_refresh_customer_name()
+func _on_RestaurantPuzzleScene_current_customer_index_changed(_value: int) -> void:
+	_refresh_creature_name()
 
 
 func _on_Customer_dna_loaded(customer: Creature) -> void:
@@ -323,5 +327,5 @@ func _on_Playfield_all_lines_cleared() -> void:
 	get_chef().play_mood(Creatures.Mood.LAUGH1)
 
 
-func _on_Timer_timeout_summon_customer(creature_index: int) -> void:
-	summon_customer(creature_index)
+func _on_Timer_timeout_summon_customer(customer_index: int) -> void:
+	summon_customer(customer_index)

project/src/demo/world/environment/restaurant/restaurant-puzzle-scene-demo.gd
@@ -2,10 +2,10 @@ extends Node
 ## A demo which shows off the restaurant scene.
 ##
 ## Keys:
-## 	[F]: Feed the creature
-## 	[1-9,0]: Change the creature's size from 10% to 100%
-## 	[Q,W,E,R]: Switch to the 1st, 2nd or 3rd creature.
-## 	brace keys: Change the creature's appearance
+## 	[F]: Feed the customer
+## 	[1-9,0]: Change the customer's size from 10% to 100%
+## 	[Q,W,E,R]: Switch to the 1st, 2nd or 3rd customer.
+## 	brace keys: Change the customer's appearance
 
 const FATNESS_KEYS := [10.0, 1.0, 1.5, 2.0, 3.0, 5.0, 6.0, 7.0, 8.0, 9.0]
 
@@ -23,7 +23,7 @@ func _input(event: InputEvent) -> void:
 			_scene.get_customer().set_fatness(FATNESS_KEYS[Utils.key_num(event)])
 		KEY_BRACKETLEFT, KEY_BRACKETRIGHT:
 			_scene.summon_customer(CreatureLoader.random_customer_def())
-		KEY_Q: _scene.current_creature_index = 0
-		KEY_W: _scene.current_creature_index = 1
-		KEY_E: _scene.current_creature_index = 2
-		KEY_R: _scene.current_creature_index = 3
+		KEY_Q: _scene.current_customer_index = 0
+		KEY_W: _scene.current_customer_index = 1
+		KEY_E: _scene.current_customer_index = 2
+		KEY_R: _scene.current_customer_index = 3

project/src/demo/world/environment/restaurant/restaurant-view-demo.gd
@@ -3,17 +3,17 @@ extends Node
 ##
 ## Keys:
 ## 	[D]: Ring the doorbell
-## 	[F]: Feed the creature
+## 	[F]: Feed the customer
 ## 	[I]: Launch an idle animation
 ## 	[V]: Say something
 ## 	[N]: Change the nametag names
-## 	[1-9,0]: Change the creature's size from 10% to 100%
-## 	SHIFT+[1-9,0]: Change the creature's comfort from 0.0 -> 1.0 -> -1.0
-## 	[Q,W,E,R]: Switch to the 1st, 2nd, 3rd or 4th creature.
+## 	[1-9,0]: Change the customer's size from 10% to 100%
+## 	SHIFT+[1-9,0]: Change the customer's comfort from 0.0 -> 1.0 -> -1.0
+## 	[Q,W,E,R]: Switch to the 1st, 2nd, 3rd or 4th customer.
 ## 	[comma, period]: Swoop the customer/chef to be onscreen
 ## 	SHIFT+[comma, period]: Swoop the customer/chef to be offscreen
-## 	arrows: Change the creature's orientation
-## 	brace keys: Change the creature's appearance
+## 	arrows: Change the customer's orientation
+## 	brace keys: Change the customer's appearance
 
 const FATNESS_KEYS := [10.0, 1.0, 1.5, 2.0, 3.0, 5.0, 6.0, 7.0, 8.0, 9.0]
 const NAMES := [
@@ -54,7 +54,7 @@ func _input(event: InputEvent) -> void:
 			_customer().get_node("CreatureSfx").play_goodbye_voice()
 		KEY_0, KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9:
 			if Input.is_key_pressed(KEY_SHIFT):
-				# shift pressed; change creature's comfort
+				# shift pressed; change customer's comfort
 				match Utils.key_scancode(event):
 					KEY_1: _view.get_customer().set_comfort(0.00) # hasn't eaten
 					KEY_2: _view.get_customer().set_comfort(0.30)
@@ -67,12 +67,12 @@ func _input(event: InputEvent) -> void:
 					KEY_9: _view.get_customer().set_comfort(-0.90)
 					KEY_0: _view.get_customer().set_comfort(-1.00) # ate way too much
 			else:
-				# shift not pressed; change creature's fatness
+				# shift not pressed; change customer's fatness
 				_view.get_customer().set_fatness(FATNESS_KEYS[Utils.key_num(event)])
-		KEY_Q: _view.set_current_creature_index(0)
-		KEY_W: _view.set_current_creature_index(1)
-		KEY_E: _view.set_current_creature_index(2)
-		KEY_R: _view.set_current_creature_index(3)
+		KEY_Q: _view.set_current_customer_index(0)
+		KEY_W: _view.set_current_customer_index(1)
+		KEY_E: _view.set_current_customer_index(2)
+		KEY_R: _view.set_current_customer_index(3)
 		KEY_BRACKETLEFT, KEY_BRACKETRIGHT:
 			_view.summon_customer()
 		KEY_RIGHT:

project/src/main/puzzle/food-items.gd
@@ -153,7 +153,7 @@ func _on_FoodItem_flight_done(food_item: FoodItem) -> void:
 	
 	if _customer_index == food_item.customer_index:
 		# ensure the customer hasn't been replaced before fattening them
-		_puzzle.feed_creature(food_item.customer, food_item.food_type)
+		_puzzle.feed_customer(food_item.customer, food_item.food_type)
 		
 		var new_fatness: float = _pending_food_fatness.pop_front()
 		food_item.customer.set_fatness(new_fatness)

project/src/main/puzzle/puzzle.gd
@@ -104,8 +104,8 @@ func hide_buttons() -> void:
 	$Hud/Center/PuzzleMessages.hide_buttons()
 
 
-func scroll_to_new_creature() -> void:
-	_restaurant_view.scroll_to_new_creature()
+func scroll_to_new_customer() -> void:
+	_restaurant_view.scroll_to_new_customer()
 
 
 ## Start a countdown when transitioning between levels. Used during tutorials.
@@ -124,18 +124,18 @@ func get_customer() -> Creature:
 	return _restaurant_view.get_customer()
 
 
-## Triggers the eating animation and makes the creature fatter. Accepts a 'fatness_pct' parameter which defines how
-## much fatter the creature should get. We can calculate how fat they should be, and a value of 0.4 means the creature
+## Triggers the eating animation and makes the customer fatter. Accepts a 'fatness_pct' parameter which defines how
+## much fatter the customer should get. We can calculate how fat they should be, and a value of 0.4 means the customer
 ## should increase by 40% of the amount needed to reach that target.
 ##
 ## This 'fatness_pct' parameter is needed for the level where the player eliminates three lines at once. We don't
-## want the creature to suddenly grow full size. We want it to take 3 bites.
+## want the customer to suddenly grow full size. We want it to take 3 bites.
 ##
 ## Parameters:
-## 	'customer': The creature to feed
+## 	'customer': The customer to feed
 ##
 ## 	'food_type': An enum from FoodType corresponding to the food to show
-func feed_creature(customer: Creature, food_type: int) -> void:
+func feed_customer(customer: Creature, food_type: int) -> void:
 	var new_comfort := customer.score_to_comfort(PuzzleState.combo, PuzzleState.get_customer_score())
 	customer.set_comfort(new_comfort)
 	customer.feed(food_type)
@@ -159,7 +159,7 @@ func _start_puzzle() -> void:
 	PlayerData.customer_queue.pop_standard_customers(current_customer_ids)
 	
 	if not CurrentLevel.keep_retrying:
-		# Reset everyone's fatness. Replaying a puzzle in free roam mode shouldn't make a creature super fat.
+		# Reset everyone's fatness. Replaying a puzzle in free roam mode shouldn't make a customer super fat.
 		# Thematically we're turning back time.
 		PlayerData.creature_library.restore_fatness_state()
 	
@@ -167,39 +167,39 @@ func _start_puzzle() -> void:
 	_restaurant_view.briefly_suppress_sfx()
 	
 	if PlayerData.customer_queue.priority_queue:
-		var starting_creature_id: String = PlayerData.customer_queue.priority_queue[0].creature_id
-		var starting_creature_index: int = _restaurant_view.find_creature_index_with_id(starting_creature_id)
-		if starting_creature_index == -1:
-			# starting creature isn't found; load them to a different index and advance the priority queue
-			starting_creature_index = _restaurant_view.next_creature_index()
-			_restaurant_view.summon_customer(starting_creature_index)
+		var starting_customer_creature_id: String = PlayerData.customer_queue.priority_queue[0].creature_id
+		var starting_customer_index: int = _restaurant_view.find_customer_index_with_id(starting_customer_creature_id)
+		if starting_customer_index == -1:
+			# starting customer isn't found; load them to a different index and advance the priority queue
+			starting_customer_index = _restaurant_view.next_customer_index()
+			_restaurant_view.summon_customer(starting_customer_index)
 		else:
-			# starting creature is found; advance the priority customer queue so we don't see them twice
+			# starting customer is found; advance the priority customer queue so we don't see them twice
 			PlayerData.customer_queue.pop_priority_customer()
 			
-			if PlayerData.creature_library.has_fatness(starting_creature_id):
+			if PlayerData.creature_library.has_fatness(starting_customer_creature_id):
 				# restore their fatness so they start skinny again when replaying a puzzle
-				var fatness: float = PlayerData.creature_library.get_fatness(starting_creature_id)
-				_restaurant_view.get_customer(starting_creature_index).set_fatness(fatness)
+				var fatness: float = PlayerData.creature_library.get_fatness(starting_customer_creature_id)
+				_restaurant_view.get_customer(starting_customer_index).set_fatness(fatness)
 		
-		# summon the other creatures
+		# summon the other customers
 		for i in range(_restaurant_view.get_customers().size()):
-			if i != starting_creature_index:
+			if i != starting_customer_index:
 				_restaurant_view.summon_customer(i)
 		
-		# scroll to the starting creature
-		_restaurant_view.current_creature_index = starting_creature_index
+		# scroll to the starting customer
+		_restaurant_view.current_customer_index = starting_customer_index
 	else:
-		var current_creature_feed_count: int = _restaurant_view.get_customer().feed_count
+		var current_customer_feed_count: int = _restaurant_view.get_customer().feed_count
 		
-		# fill the seats if the creatures ate
+		# fill the seats if the customers ate
 		for i in range(_restaurant_view.get_customers().size()):
 			if _restaurant_view.get_customer(i).feed_count:
 				_restaurant_view.summon_customer(i)
 		
-		# calculate the starting creature; stay on the same creature if they didn't eat
-		if current_creature_feed_count > 0:
-			_restaurant_view.current_creature_index = _restaurant_view.next_creature_index()
+		# calculate the starting customer; stay on the same customer if they didn't eat
+		if current_customer_feed_count > 0:
+			_restaurant_view.current_customer_index = _restaurant_view.next_customer_index()
 	
 	PuzzleState.prepare_and_start_game()
 
@@ -289,7 +289,7 @@ func _on_Hud_back_button_pressed() -> void:
 	_quit_puzzle()
 
 
-## Triggers the 'creature feeding' animation.
+## Triggers the 'customer feeding' animation.
 func _on_Playfield_line_cleared(_y: int, total_lines: int, remaining_lines: int, _box_ints: Array) -> void:
 	var customer: Creature = get_customer()
 	
@@ -303,7 +303,7 @@ func _on_Playfield_line_cleared(_y: int, total_lines: int, remaining_lines: int,
 	if remaining_lines == 0 and PuzzleState.finish_triggered and not PuzzleState.game_ended:
 		PuzzleState.end_game()
 	
-	# Calculate whether or not the creature should say something positive about the combo.
+	# Calculate whether or not the customer should say something positive about the combo.
 	# They say something after clearing [6, 12, 18, 24...] lines.
 	if remaining_lines == 0 and PuzzleState.combo >= 6 and total_lines > PuzzleState.combo % 6:
 		yield(get_tree().create_timer(0.5), "timeout")

project/src/main/puzzle/tutorial/tutorial-module.gd
@@ -50,7 +50,7 @@ func start_customer_countdown() -> void:
 ## 	'messages': Array of string messages to be shown when the sensei is dismissed.
 func dismiss_sensei(messages: Array) -> void:
 	PuzzleState.reset()
-	puzzle.scroll_to_new_creature()
+	puzzle.scroll_to_new_customer()
 	hud.set_messages(messages)
 	hud.enqueue_pop_out()
 

project/src/main/world/environment/restaurant/RestaurantView.tscn
@@ -317,7 +317,7 @@ custom_styles/panel = SubResource( 4 )
 one_shot = true
 script = ExtResource( 12 )
 
-[node name="SummonCreatureTimers" parent="." instance=ExtResource( 14 )]
+[node name="SummonCustomerTimers" parent="." instance=ExtResource( 14 )]
 
 [node name="SwoopTween" type="Tween" parent="."]
 
@@ -326,6 +326,6 @@ autoplay = "default"
 anims/default = SubResource( 7 )
 
 [connection signal="timeout" from="HelloTimer" to="HelloTimer" method="_on_timeout"]
-[connection signal="current_creature_index_changed" from="RestaurantViewport/Scene" to="." method="_on_RestaurantPuzzleScene_current_creature_index_changed"]
-[connection signal="current_creature_index_changed" from="RestaurantViewport/Scene" to="Customer/CameraMover" method="_on_RestaurantPuzzleScene_current_creature_index_changed"]
+[connection signal="current_customer_index_changed" from="RestaurantViewport/Scene" to="." method="_on_RestaurantPuzzleScene_current_customer_index_changed"]
+[connection signal="current_customer_index_changed" from="RestaurantViewport/Scene" to="Customer/CameraMover" method="_on_RestaurantPuzzleScene_current_customer_index_changed"]
 [connection signal="food_eaten" from="RestaurantViewport/Scene" to="Customer/CameraMover" method="_on_RestaurantPuzzleScene_food_eaten"]

project/src/main/world/environment/restaurant/customer-camera-mover.gd
@@ -7,7 +7,7 @@ extends AnimationPlayer
 export (NodePath) var restaurant_scene_path: NodePath
 export (NodePath) var customer_camera_path: NodePath
 
-## The amount of empty space over the creature's head.
+## The amount of empty space over the customer's head.
 export (float, 0, 1) var headroom := 1.0 setget set_headroom
 
 ## the position that the restaurant scene's camera lerps to
@@ -59,7 +59,7 @@ func set_headroom(new_headroom: float) -> void:
 ##
 ## These properties are updated by advancing this AnimationPlayer.
 func _refresh_zoom_and_headroom() -> void:
-	var customer := _restaurant_scene.get_customer(_restaurant_scene.current_creature_index)
+	var customer := _restaurant_scene.get_customer(_restaurant_scene.current_customer_index)
 	play("fat-se")
 	advance(customer.get_visual_fatness())
 	stop()
@@ -68,7 +68,7 @@ func _refresh_zoom_and_headroom() -> void:
 
 ## Updates the target camera position based on the current customer's position and the 'headroom' property.
 func _refresh_target_camera_position() -> void:
-	var customer := _restaurant_scene.get_customer(_restaurant_scene.current_creature_index)
+	var customer := _restaurant_scene.get_customer(_restaurant_scene.current_customer_index)
 	# calculate the position within the restaurant scene
 	var target_pos: Vector2 = customer.body_pos_from_head_pos(lerp(Vector2(0, 40), Vector2(0, -65), headroom))
 	_target_camera_position = target_pos
@@ -76,12 +76,12 @@ func _refresh_target_camera_position() -> void:
 
 
 func _on_Creature_visual_fatness_changed(index: int) -> void:
-	if index == _restaurant_scene.current_creature_index:
-		# only adjust the camera if the current creature changes
+	if index == _restaurant_scene.current_customer_index:
+		# only adjust the camera if the current customer changes
 		_refresh_zoom_and_headroom()
 
 
-func _on_RestaurantPuzzleScene_current_creature_index_changed(_value: int) -> void:
+func _on_RestaurantPuzzleScene_current_customer_index_changed(_value: int) -> void:
 	_refresh_zoom_and_headroom()
 
 

commit 97b3454dd5bf4cd857c45333483245336210e7b8

Fixed incorrect 'customer_def' field ref
This was broken in d6cdb627fb41e34805edf73ea1cc3d34a90f0f4b

project/src/main/puzzle/restaurant-view.gd
@@ -150,7 +150,7 @@ func find_customer_index_with_id(customer_creature_id: String) -> int:
 	var customers := get_customers()
 	
 	for i in range(customers.size()):
-		if customers[i].customer_def.creature_id == customer_creature_id:
+		if customers[i].creature_def.creature_id == customer_creature_id:
 			customer_index = i
 			break
 	