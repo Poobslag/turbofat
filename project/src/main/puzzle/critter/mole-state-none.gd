261def504d4fde0fc3b92afdabf2f218299702bf
Poobslag <aaron.pieper@gmail.com>
2023-06-14 10:46:57 -0400

    CHanged onion reset to 'discrete'
    Changed Onion AnimationPlayer's blend modes to 'discrete' to avoid a
    warning, 'Value track: Onion:frame with different update modes are
    blended. Blending prioritizes Discrete mode, so other update mode tracks
    will not be blended. (User)'

--- project/src/main/puzzle/critter/mole-state-none.gd
+++ project/src/main/puzzle/critter/mole-state-none.gd
@@ -9,4 +9,4 @@ func enter(mole: Mole, prev_state_name: String) -> void:
 	if not prev_state_name in ["", "None", "Waiting"]:
 		mole.poof.play_poof_animation()
 		mole.sfx.play_poof_sound()
-	mole.animation_player.stop()
+	mole.animation_player.stop(true)
