Poobslag/turbofat

commit b1853eeba77e41a76e99b179244d2e29fdf3c8f8

Removed spaces from node names.
Added delint.sh rule to report node names with spaces.

delint.sh
@@ -191,6 +191,15 @@ then
   echo "$RESULT"
 fi
 
+# node names with spaces
+RESULT=$(grep -R -n "node name=\"[^\"]* [^\"]*\"" --include="*.tscn" project/src)
+if [ -n "$RESULT" ]
+then
+  echo ""
+  echo "Node names with spaces:"
+  echo "$RESULT"
+fi
+
 # sort signal connections. Workaround for Godot #35084
 # https://github.com/godotengine/godot/issues/35084
 if [ "$CLEAN" ]

project/src/main/editor/creature/CreatureEditor.tscn
@@ -1406,7 +1406,7 @@ allele = "all_rgb"
 text = "All Colors"
 creature_editor_path = NodePath("../../../../../../..")
 
-[node name="Body Color" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
+[node name="BodyColor" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
 margin_top = 64.0
 margin_right = 378.0
 margin_bottom = 90.0
@@ -1414,7 +1414,7 @@ allele = "body_rgb"
 text = "Main Color"
 creature_editor_path = NodePath("../../../../../../..")
 
-[node name="Line Color" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
+[node name="LineColor" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
 margin_top = 94.0
 margin_right = 378.0
 margin_bottom = 120.0
@@ -1446,7 +1446,7 @@ allele = "eye"
 text = "Eye"
 creature_editor_path = NodePath("../../../../../../..")
 
-[node name="Eye Color" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 24 )]
+[node name="EyeColor" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 24 )]
 margin_top = 214.0
 margin_right = 378.0
 margin_bottom = 240.0
@@ -1484,7 +1484,7 @@ allele = "ear"
 text = "Ear"
 creature_editor_path = NodePath("../../../../../../..")
 
-[node name="Horn Color" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
+[node name="HornColor" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
 margin_top = 364.0
 margin_right = 378.0
 margin_bottom = 390.0
@@ -1508,7 +1508,7 @@ allele = "hair"
 text = "Hair"
 creature_editor_path = NodePath("../../../../../../..")
 
-[node name="Hair Color" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
+[node name="HairColor" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
 margin_top = 454.0
 margin_right = 378.0
 margin_bottom = 480.0
@@ -1532,7 +1532,7 @@ allele = "belly"
 text = "Belly"
 creature_editor_path = NodePath("../../../../../../..")
 
-[node name="Belly Color" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
+[node name="BellyColor" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
 margin_top = 544.0
 margin_right = 378.0
 margin_bottom = 570.0
@@ -1570,7 +1570,7 @@ allele = "collar"
 text = "Collar"
 creature_editor_path = NodePath("../../../../../../..")
 
-[node name="Cloth Color" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
+[node name="ClothColor" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
 margin_top = 694.0
 margin_right = 378.0
 margin_bottom = 720.0
@@ -1578,7 +1578,7 @@ allele = "cloth_rgb"
 text = "Cloth Color"
 creature_editor_path = NodePath("../../../../../../..")
 
-[node name="Glass Color" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
+[node name="GlassColor" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
 margin_top = 724.0
 margin_right = 378.0
 margin_bottom = 750.0
@@ -1586,7 +1586,7 @@ allele = "glass_rgb"
 text = "Glass Color"
 creature_editor_path = NodePath("../../../../../../..")
 
-[node name="Plastic Color2" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
+[node name="PlasticColor" parent="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer" instance=ExtResource( 20 )]
 margin_top = 754.0
 margin_right = 378.0
 margin_bottom = 780.0
@@ -1967,9 +1967,6 @@ window_title = "Open a File"
 mode = 0
 access = 2
 filters = PoolStringArray( "*.json" )
-current_dir = "/"
-current_file = "509e7c82-9399-425a-9f15-9370c2b3de8b"
-current_path = "/509e7c82-9399-425a-9f15-9370c2b3de8b"
 
 [node name="Export" type="FileDialog" parent="Ui/Dialogs"]
 anchor_left = 0.5
@@ -1983,9 +1980,6 @@ margin_bottom = 480.0
 popup_exclusive = true
 window_title = "Export Creature"
 access = 2
-current_dir = "/"
-current_file = "509e7c82-9399-425a-9f15-9370c2b3de8b"
-current_path = "/509e7c82-9399-425a-9f15-9370c2b3de8b"
 
 [node name="Error" type="AcceptDialog" parent="Ui/Dialogs"]
 margin_right = 250.0
@@ -1998,28 +1992,28 @@ dialog_autowrap = true
 
 [node name="SceneTransitionCover" parent="." instance=ExtResource( 29 )]
 
-[connection signal="quit_pressed" from="SettingsMenu" to="." method="_on_Quit_pressed"]
 [connection signal="creature_clicked" from="Ui/CreatureSelector" to="." method="_on_CreatureSelector_creature_clicked"]
 [connection signal="hovered_creature_changed" from="Ui/CreatureSelector" to="Ui/Nametags" method="_on_CreatureSelector_hovered_creature_changed"]
 [connection signal="mouse_exited" from="Ui/CreatureSelector" to="Ui/CreatureSelector" method="_on_mouse_exited"]
-[connection signal="about_to_show" from="Ui/Dialogs/Error" to="Ui/Dialogs/Backdrop" method="_on_Dialog_about_to_show"]
-[connection signal="popup_hide" from="Ui/Dialogs/Error" to="Ui/Dialogs/Backdrop" method="_on_Dialog_popup_hide"]
-[connection signal="about_to_show" from="Ui/Dialogs/Export" to="Ui/Dialogs/Backdrop" method="_on_Dialog_about_to_show"]
-[connection signal="file_selected" from="Ui/Dialogs/Export" to="Ui/Dialogs" method="_on_ExportDialog_file_selected"]
-[connection signal="popup_hide" from="Ui/Dialogs/Export" to="Ui/Dialogs/Backdrop" method="_on_Dialog_popup_hide"]
-[connection signal="about_to_show" from="Ui/Dialogs/Import" to="Ui/Dialogs/Backdrop" method="_on_Dialog_about_to_show"]
-[connection signal="file_selected" from="Ui/Dialogs/Import" to="Ui/Dialogs" method="_on_ImportDialog_file_selected"]
-[connection signal="popup_hide" from="Ui/Dialogs/Import" to="Ui/Dialogs/Backdrop" method="_on_Dialog_popup_hide"]
-[connection signal="about_to_show" from="Ui/Dialogs/SaveConfirmation" to="Ui/Dialogs/Backdrop" method="_on_Dialog_about_to_show"]
-[connection signal="confirmed" from="Ui/Dialogs/SaveConfirmation" to="Ui/Dialogs" method="_on_SaveConfirmation_confirmed"]
-[connection signal="popup_hide" from="Ui/Dialogs/SaveConfirmation" to="Ui/Dialogs/Backdrop" method="_on_Dialog_popup_hide"]
+[connection signal="pressed" from="Ui/Settings" to="SettingsMenu" method="_on_Settings_pressed"]
 [connection signal="pressed" from="Ui/Quit" to="." method="_on_Quit_pressed"]
 [connection signal="pressed" from="Ui/Reroll/Button" to="." method="_on_Reroll_pressed"]
 [connection signal="value_changed" from="Ui/Reroll/HSlider" to="Ui/Reroll" method="_on_HSlider_value_changed"]
-[connection signal="pressed" from="Ui/Settings" to="SettingsMenu" method="_on_Settings_pressed"]
-[connection signal="pressed" from="Ui/TabContainer/Mutate/ScrollContainer/MarginContainer/VBoxContainer/ImportContainer/Export" to="Ui/Dialogs" method="_on_ExportButton_pressed"]
-[connection signal="pressed" from="Ui/TabContainer/Mutate/ScrollContainer/MarginContainer/VBoxContainer/ImportContainer/Import" to="Ui/Dialogs" method="_on_ImportButton_pressed"]
-[connection signal="pressed" from="Ui/TabContainer/Mutate/ScrollContainer/MarginContainer/VBoxContainer/Save" to="Ui/Dialogs" method="_on_SaveButton_pressed"]
-[connection signal="pressed" from="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer/ImportContainer/Export" to="Ui/Dialogs" method="_on_ExportButton_pressed"]
 [connection signal="pressed" from="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer/ImportContainer/Import" to="Ui/Dialogs" method="_on_ImportButton_pressed"]
+[connection signal="pressed" from="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer/ImportContainer/Export" to="Ui/Dialogs" method="_on_ExportButton_pressed"]
 [connection signal="pressed" from="Ui/TabContainer/Tweak/ScrollContainer/MarginContainer/VBoxContainer/Save" to="Ui/Dialogs" method="_on_SaveButton_pressed"]
+[connection signal="pressed" from="Ui/TabContainer/Mutate/ScrollContainer/MarginContainer/VBoxContainer/ImportContainer/Import" to="Ui/Dialogs" method="_on_ImportButton_pressed"]
+[connection signal="pressed" from="Ui/TabContainer/Mutate/ScrollContainer/MarginContainer/VBoxContainer/ImportContainer/Export" to="Ui/Dialogs" method="_on_ExportButton_pressed"]
+[connection signal="pressed" from="Ui/TabContainer/Mutate/ScrollContainer/MarginContainer/VBoxContainer/Save" to="Ui/Dialogs" method="_on_SaveButton_pressed"]
+[connection signal="about_to_show" from="Ui/Dialogs/SaveConfirmation" to="Ui/Dialogs/Backdrop" method="_on_Dialog_about_to_show"]
+[connection signal="confirmed" from="Ui/Dialogs/SaveConfirmation" to="Ui/Dialogs" method="_on_SaveConfirmation_confirmed"]
+[connection signal="popup_hide" from="Ui/Dialogs/SaveConfirmation" to="Ui/Dialogs/Backdrop" method="_on_Dialog_popup_hide"]
+[connection signal="about_to_show" from="Ui/Dialogs/Import" to="Ui/Dialogs/Backdrop" method="_on_Dialog_about_to_show"]
+[connection signal="file_selected" from="Ui/Dialogs/Import" to="Ui/Dialogs" method="_on_ImportDialog_file_selected"]
+[connection signal="popup_hide" from="Ui/Dialogs/Import" to="Ui/Dialogs/Backdrop" method="_on_Dialog_popup_hide"]
+[connection signal="about_to_show" from="Ui/Dialogs/Export" to="Ui/Dialogs/Backdrop" method="_on_Dialog_about_to_show"]
+[connection signal="file_selected" from="Ui/Dialogs/Export" to="Ui/Dialogs" method="_on_ExportDialog_file_selected"]
+[connection signal="popup_hide" from="Ui/Dialogs/Export" to="Ui/Dialogs/Backdrop" method="_on_Dialog_popup_hide"]
+[connection signal="about_to_show" from="Ui/Dialogs/Error" to="Ui/Dialogs/Backdrop" method="_on_Dialog_about_to_show"]
+[connection signal="popup_hide" from="Ui/Dialogs/Error" to="Ui/Dialogs/Backdrop" method="_on_Dialog_popup_hide"]
+[connection signal="quit_pressed" from="SettingsMenu" to="." method="_on_Quit_pressed"]

project/src/main/editor/creature/TweakEyeColorRow.tscn
@@ -5,15 +5,12 @@
 [ext_resource path="res://assets/main/editor/creature/edit.png" type="Texture" id=3]
 [ext_resource path="res://src/main/editor/creature/tweak-eye-row.gd" type="Script" id=4]
 
-[node name="Eye Color" type="HBoxContainer"]
+[node name="EyeColor" type="HBoxContainer"]
 margin_top = 124.0
 margin_right = 306.0
 margin_bottom = 150.0
 custom_constants/separation = 10
 script = ExtResource( 4 )
-__meta__ = {
-"_edit_use_anchors_": false
-}
 creature_editor_path = NodePath("../../../../..")
 
 [node name="Label" type="Label" parent="."]
@@ -76,12 +73,12 @@ __meta__ = {
 "_edit_use_anchors_": false
 }
 
-[connection signal="button_down" from="DnaMargin/Dna" to="DnaMargin/Dna" method="_on_button_down"]
-[connection signal="button_up" from="DnaMargin/Dna" to="DnaMargin/Dna" method="_on_button_up"]
-[connection signal="pressed" from="DnaMargin/Dna" to="." method="_on_Dna_pressed"]
 [connection signal="color_changed" from="Edit1" to="." method="_on_Edit_color_changed"]
 [connection signal="popup_closed" from="Edit1" to="." method="_on_Edit_popup_closed"]
 [connection signal="pressed" from="Edit1" to="." method="_on_Edit_pressed"]
 [connection signal="color_changed" from="Edit2" to="." method="_on_Edit_color_changed"]
 [connection signal="popup_closed" from="Edit2" to="." method="_on_Edit_popup_closed"]
 [connection signal="pressed" from="Edit2" to="." method="_on_Edit_pressed"]
+[connection signal="button_down" from="DnaMargin/Dna" to="DnaMargin/Dna" method="_on_button_down"]
+[connection signal="button_up" from="DnaMargin/Dna" to="DnaMargin/Dna" method="_on_button_up"]
+[connection signal="pressed" from="DnaMargin/Dna" to="." method="_on_Dna_pressed"]

project/src/main/editor/creature/TweakEyeRow.tscn
@@ -6,15 +6,12 @@
 [ext_resource path="res://src/main/editor/creature/tweak-eye-row.gd" type="Script" id=4]
 [ext_resource path="res://assets/main/editor/creature/edit-rainbow.png" type="Texture" id=5]
 
-[node name="Eye Color" type="HBoxContainer"]
+[node name="EyeColor" type="HBoxContainer"]
 margin_top = 124.0
 margin_right = 306.0
 margin_bottom = 150.0
 custom_constants/separation = 10
 script = ExtResource( 4 )
-__meta__ = {
-"_edit_use_anchors_": false
-}
 creature_editor_path = NodePath("../../../../..")
 
 [node name="Label" type="Label" parent="."]
@@ -151,12 +148,12 @@ __meta__ = {
 [connection signal="color_changed" from="Edit2" to="." method="_on_Edit_color_changed"]
 [connection signal="popup_closed" from="Edit2" to="." method="_on_Edit_popup_closed"]
 [connection signal="pressed" from="Edit2" to="." method="_on_Edit_pressed"]
-[connection signal="button_down" from="GrayDna/Button" to="GrayDna/Button" method="_on_button_down"]
-[connection signal="button_up" from="GrayDna/Button" to="GrayDna/Button" method="_on_button_up"]
-[connection signal="pressed" from="GrayDna/Button" to="." method="_on_GrayDna_pressed"]
-[connection signal="button_down" from="PurpleDna/Button" to="PurpleDna/Button" method="_on_button_down"]
-[connection signal="button_up" from="PurpleDna/Button" to="PurpleDna/Button" method="_on_button_up"]
-[connection signal="pressed" from="PurpleDna/Button" to="." method="_on_PurpleDna_pressed"]
 [connection signal="button_down" from="RainbowDna/Button" to="RainbowDna/Button" method="_on_button_down"]
 [connection signal="button_up" from="RainbowDna/Button" to="RainbowDna/Button" method="_on_button_up"]
 [connection signal="pressed" from="RainbowDna/Button" to="." method="_on_RainbowDna_pressed"]
+[connection signal="button_down" from="PurpleDna/Button" to="PurpleDna/Button" method="_on_button_down"]
+[connection signal="button_up" from="PurpleDna/Button" to="PurpleDna/Button" method="_on_button_up"]
+[connection signal="pressed" from="PurpleDna/Button" to="." method="_on_PurpleDna_pressed"]
+[connection signal="button_down" from="GrayDna/Button" to="GrayDna/Button" method="_on_button_down"]
+[connection signal="button_up" from="GrayDna/Button" to="GrayDna/Button" method="_on_button_up"]
+[connection signal="pressed" from="GrayDna/Button" to="." method="_on_GrayDna_pressed"]

project/src/main/puzzle/Puzzle.tscn
@@ -240,7 +240,7 @@ script = ExtResource( 21 )
 anchor_right = 1.0
 anchor_bottom = 1.0
 script = ExtResource( 74 )
-shadow_paths = [ NodePath("../Fg/Chalkboard/Chalkboard Shadow"), NodePath("../Fg/PlayfieldShadow/NormalShadow") ]
+shadow_paths = [ NodePath("../Fg/Chalkboard/ChalkboardShadow"), NodePath("../Fg/PlayfieldShadow/NormalShadow") ]
 
 [node name="Bg" parent="BgLoader" instance=ExtResource( 40 )]
 
@@ -264,7 +264,7 @@ texture = ExtResource( 42 )
 expand = true
 script = ExtResource( 66 )
 
-[node name="Chalkboard Shadow" type="Polygon2D" parent="Fg/Chalkboard"]
+[node name="ChalkboardShadow" type="Polygon2D" parent="Fg/Chalkboard"]
 show_behind_parent = true
 position = Vector2( 57.4473, -143.371 )
 scale = Vector2( 0.444724, 0.444458 )
@@ -829,43 +829,43 @@ script = ExtResource( 19 )
 
 [node name="SceneTransitionCover" parent="." instance=ExtResource( 50 )]
 
-[connection signal="cheat_detected" from="CheatCodeDetector" to="." method="_on_CheatCodeDetector_cheat_detected"]
-[connection signal="cheat_detected" from="CheatCodeDetector" to="Hud/PuzzleTrace" method="_on_CheatCodeDetector_cheat_detected"]
-[connection signal="cheat_detected" from="CheatCodeDetector" to="Hud/VBoxContainer/FpsLabel" method="_on_CheatCodeDetector_cheat_detected"]
-[connection signal="cheat_detected" from="CheatCodeDetector" to="Hud/VBoxContainer/FrameDropsLabel" method="_on_CheatCodeDetector_cheat_detected"]
-[connection signal="timeout" from="Fg/FoodItems/FoodFlightTimer" to="Fg/FoodItems" method="_on_FoodFlightTimer_timeout"]
+[connection signal="after_lines_deleted" from="Fg/Playfield" to="Fg/PieceManager" method="_on_Playfield_after_lines_deleted"]
+[connection signal="after_lines_filled" from="Fg/Playfield" to="Fg/PieceManager" method="_on_Playfield_after_lines_filled"]
+[connection signal="all_lines_cleared" from="Fg/Playfield" to="Fg/RestaurantView" method="_on_Playfield_all_lines_cleared"]
+[connection signal="all_lines_cleared" from="Fg/Playfield" to="Hud/Center/PuzzleMessages" method="_on_Playfield_all_lines_cleared"]
+[connection signal="before_line_cleared" from="Fg/Playfield" to="Fg/StarSeeds" method="_on_Playfield_before_line_cleared"]
+[connection signal="before_line_cleared" from="Fg/Playfield" to="Fg/GoopGlobs" method="_on_Playfield_before_line_cleared"]
+[connection signal="before_line_cleared" from="Fg/Playfield" to="Fg/Bursts" method="_on_Playfield_before_line_cleared"]
+[connection signal="blocks_prepared" from="Fg/Playfield" to="Fg/StarSeeds" method="_on_Playfield_blocks_prepared"]
+[connection signal="box_built" from="Fg/Playfield" to="Fg/StarSeeds" method="_on_Playfield_box_built"]
+[connection signal="box_built" from="Fg/Playfield" to="Fg/GoopGlobs" method="_on_Playfield_box_built"]
+[connection signal="food_spawned" from="Fg/Playfield" to="Fg/FoodItems" method="_on_Playfield_food_spawned"]
+[connection signal="line_cleared" from="Fg/Playfield" to="." method="_on_Playfield_line_cleared"]
+[connection signal="line_deleted" from="Fg/Playfield" to="Fg/StarSeeds" method="_on_Playfield_line_deleted"]
+[connection signal="line_erased" from="Fg/Playfield" to="Fg/StarSeeds" method="_on_Playfield_line_erased"]
+[connection signal="line_erased" from="Fg/Playfield" to="Fg/PieceManager" method="_on_Playfield_line_erased"]
+[connection signal="line_filled" from="Fg/Playfield" to="Fg/StarSeeds" method="_on_Playfield_line_filled"]
+[connection signal="line_inserted" from="Fg/Playfield" to="Fg/StarSeeds" method="_on_Playfield_line_inserted"]
+[connection signal="line_inserted" from="Fg/Playfield" to="Fg/PieceManager" method="_on_Playfield_line_inserted"]
+[connection signal="food_spawned" from="Fg/StarSeeds" to="Fg/FoodItems" method="_on_StarSeeds_food_spawned"]
+[connection signal="finished_spin_move" from="Fg/PieceManager" to="Fg/Bursts" method="_on_PieceManager_finished_spin_move"]
+[connection signal="finished_squish_move" from="Fg/PieceManager" to="Fg/Bursts" method="_on_PieceManager_finished_squish_move"]
+[connection signal="squish_moved" from="Fg/PieceManager" to="Fg/GoopGlobs" method="_on_PieceManager_squish_moved"]
+[connection signal="timeout" from="Fg/StepMeter/RecalculateTimer" to="Fg/StepMeter" method="_on_RecalculateTimer_timeout"]
+[connection signal="customer_changed" from="Fg/RestaurantView" to="Fg/FoodItems" method="_on_RestaurantView_customer_changed"]
 [connection signal="hit_next_pieces" from="Fg/GoopGlobs" to="GoopSfx" method="_on_GoopGlobs_hit_next_pieces"]
 [connection signal="hit_playfield" from="Fg/GoopGlobs" to="Fg/Playfield" method="_on_GoopGlobs_hit_playfield"]
 [connection signal="hit_playfield" from="Fg/GoopGlobs" to="GoopSfx" method="_on_GoopGlobs_hit_playfield"]
 [connection signal="hit_wall" from="Fg/GoopGlobs" to="Fg/WallGlobViewports" method="_on_GoopGlobs_hit_wall"]
 [connection signal="hit_wall" from="Fg/GoopGlobs" to="GoopSfx" method="_on_GoopGlobs_hit_wall"]
-[connection signal="finished_spin_move" from="Fg/PieceManager" to="Fg/Bursts" method="_on_PieceManager_finished_spin_move"]
-[connection signal="finished_squish_move" from="Fg/PieceManager" to="Fg/Bursts" method="_on_PieceManager_finished_squish_move"]
-[connection signal="squish_moved" from="Fg/PieceManager" to="Fg/GoopGlobs" method="_on_PieceManager_squish_moved"]
-[connection signal="after_lines_deleted" from="Fg/Playfield" to="Fg/PieceManager" method="_on_Playfield_after_lines_deleted"]
-[connection signal="after_lines_filled" from="Fg/Playfield" to="Fg/PieceManager" method="_on_Playfield_after_lines_filled"]
-[connection signal="all_lines_cleared" from="Fg/Playfield" to="Fg/RestaurantView" method="_on_Playfield_all_lines_cleared"]
-[connection signal="all_lines_cleared" from="Fg/Playfield" to="Hud/Center/PuzzleMessages" method="_on_Playfield_all_lines_cleared"]
-[connection signal="before_line_cleared" from="Fg/Playfield" to="Fg/Bursts" method="_on_Playfield_before_line_cleared"]
-[connection signal="before_line_cleared" from="Fg/Playfield" to="Fg/GoopGlobs" method="_on_Playfield_before_line_cleared"]
-[connection signal="before_line_cleared" from="Fg/Playfield" to="Fg/StarSeeds" method="_on_Playfield_before_line_cleared"]
-[connection signal="blocks_prepared" from="Fg/Playfield" to="Fg/StarSeeds" method="_on_Playfield_blocks_prepared"]
-[connection signal="box_built" from="Fg/Playfield" to="Fg/GoopGlobs" method="_on_Playfield_box_built"]
-[connection signal="box_built" from="Fg/Playfield" to="Fg/StarSeeds" method="_on_Playfield_box_built"]
-[connection signal="food_spawned" from="Fg/Playfield" to="Fg/FoodItems" method="_on_Playfield_food_spawned"]
-[connection signal="line_cleared" from="Fg/Playfield" to="." method="_on_Playfield_line_cleared"]
-[connection signal="line_deleted" from="Fg/Playfield" to="Fg/StarSeeds" method="_on_Playfield_line_deleted"]
-[connection signal="line_erased" from="Fg/Playfield" to="Fg/PieceManager" method="_on_Playfield_line_erased"]
-[connection signal="line_erased" from="Fg/Playfield" to="Fg/StarSeeds" method="_on_Playfield_line_erased"]
-[connection signal="line_filled" from="Fg/Playfield" to="Fg/StarSeeds" method="_on_Playfield_line_filled"]
-[connection signal="line_inserted" from="Fg/Playfield" to="Fg/PieceManager" method="_on_Playfield_line_inserted"]
-[connection signal="line_inserted" from="Fg/Playfield" to="Fg/StarSeeds" method="_on_Playfield_line_inserted"]
-[connection signal="customer_changed" from="Fg/RestaurantView" to="Fg/FoodItems" method="_on_RestaurantView_customer_changed"]
-[connection signal="food_spawned" from="Fg/StarSeeds" to="Fg/FoodItems" method="_on_StarSeeds_food_spawned"]
-[connection signal="timeout" from="Fg/StepMeter/RecalculateTimer" to="Fg/StepMeter" method="_on_RecalculateTimer_timeout"]
+[connection signal="timeout" from="Fg/FoodItems/FoodFlightTimer" to="Fg/FoodItems" method="_on_FoodFlightTimer_timeout"]
 [connection signal="back_button_pressed" from="Hud/Center/PuzzleMessages" to="." method="_on_Hud_back_button_pressed"]
 [connection signal="settings_button_pressed" from="Hud/Center/PuzzleMessages" to="." method="_on_Hud_settings_button_pressed"]
 [connection signal="start_button_pressed" from="Hud/Center/PuzzleMessages" to="." method="_on_Hud_start_button_pressed"]
 [connection signal="hide" from="SettingsMenu" to="Hud/TouchButtons" method="_on_Menu_hide"]
 [connection signal="quit_pressed" from="SettingsMenu" to="." method="_on_SettingsMenu_quit_pressed"]
 [connection signal="show" from="SettingsMenu" to="Hud/TouchButtons" method="_on_Menu_show"]
+[connection signal="cheat_detected" from="CheatCodeDetector" to="." method="_on_CheatCodeDetector_cheat_detected"]
+[connection signal="cheat_detected" from="CheatCodeDetector" to="Hud/PuzzleTrace" method="_on_CheatCodeDetector_cheat_detected"]
+[connection signal="cheat_detected" from="CheatCodeDetector" to="Hud/VBoxContainer/FrameDropsLabel" method="_on_CheatCodeDetector_cheat_detected"]
+[connection signal="cheat_detected" from="CheatCodeDetector" to="Hud/VBoxContainer/FpsLabel" method="_on_CheatCodeDetector_cheat_detected"]

commit 447f820bed7621fe9ed94c3d8f4d404a9fd119cf

Fixed 'print statement' delint script
This was broken in b1853eeb when we added the check for 'print_debug'.
The results were not concatenated, and the 'print' results were erased.

delint.sh
@@ -192,7 +192,12 @@ fi
 
 # print statements that got left in by mistake
 RESULT=$(git diff main | grep print\()
-RESULT=$(git diff main | grep print_debug\()
+RESULT=${RESULT}"Ê"$(git diff main | grep print_debug\()
+RESULT=$(echo "${RESULT}" |
+  sed 's/ÊÊÊ*/Ê/g' | # remove consecutive newline placeholders
+  sed 's/^Ê\(.*\)$/\1/g' | # remove trailing newline placeholders
+  sed 's/^\(.*\)Ê$/\1/g' | # remove following newline placeholders
+  sed 's/Ê/\n/g') # convert newline placeholders to newlines
 if [ -n "$RESULT" ]
 then
   echo ""