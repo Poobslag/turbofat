Poobslag/turbofat

commit b21415d927e407bfa6f444d6c073ce63115f700d

Hardcore levels.
The player is offered a hardcore level at least twice each career
session. Hardcore levels are regular levels, but the player only gets
one life. If they fail, they have to start the career session over. If
they succeed, they get an extra life.

The hardcore level select buttons have a very unique appearance.
Unfortunately Godot does not have an easy way to layer Controls with sprites,
because Controls lack a Z-index property. To work around this limitation I made
a lot of really ugly changes, including changing LevelSelectButton from a
Button to a Control, and adding a few sprites to the scene tree for the sole
purpose of giving Controls a necessary z coordinate. I also changed the HookableLevelGradeLabel scene's z coordinate so it appears over top of the skull effect.

project/src/main/career/ui/career-map-level-select.gd
@@ -5,6 +5,7 @@ extends CanvasLayer
 signal level_button_focused(button_index)
 
 export (PackedScene) var LevelSelectButtonScene: PackedScene
+export (PackedScene) var HardcoreLevelSelectButtonScene: PackedScene
 
 var _duration_calculator := DurationCalculator.new()
 var _prev_focused_level_button_index := -1
@@ -42,7 +43,11 @@ func clear_level_select_buttons() -> void:
 ## Parameters:
 ## 	'settings': The level settings which control the button's appearance.
 func add_level_select_button(settings: LevelSettings) -> LevelSelectButton:
-	var button: LevelSelectButton = LevelSelectButtonScene.instance()
+	var button: LevelSelectButton
+	if settings.lose_condition.top_out == 1:
+		button = HardcoreLevelSelectButtonScene.instance()
+	else:
+		button = LevelSelectButtonScene.instance()
 	button.rect_min_size = Vector2(200, 120)
 	button.size_flags_horizontal = 6
 	button.size_flags_vertical = 4

project/src/main/ui/level-select/level-select-button.gd
@@ -1,5 +1,5 @@
 class_name LevelSelectButton
-extends Button
+extends Control
 ## Button on the level select screen which launches a level.
 ##
 ## The button adjusts its rect_min_size based on level duration.
@@ -66,10 +66,13 @@ var _focus_just_entered := false
 ## 'true' if the 'level started' signal should be emitted in response to a button click.
 var _emit_level_chosen := false
 
-onready var _label := $Label
+onready var _button_control := $ButtonControlHolder/ButtonControl
+onready var _label := $ButtonControlHolder/ButtonControl/Label
 
 func _ready() -> void:
-	text = ""
+	_button_control.text = ""
+	_button_control.rect_size = rect_size
+	_refresh_size()
 	_refresh_appearance()
 
 
@@ -79,7 +82,7 @@ func _process(_delta: float) -> void:
 
 func set_bg_color(new_bg_color: Color) -> void:
 	bg_color = new_bg_color
-	_set_style_color(bg_color)
+	refresh_style_color(bg_color)
 
 
 func set_lock_status(new_lock_status: int) -> void:
@@ -102,9 +105,10 @@ func set_level_duration(new_level_duration: int) -> void:
 	_refresh_appearance()
 
 
-func _set_style_color(color: Color) -> void:
-	get("custom_styles/normal").bg_color = color
-	get("custom_styles/hover").bg_color = color
+## Updates the button's style colors. Can be overridden by child buttons who use different styles.
+func refresh_style_color(color: Color) -> void:
+	_button_control.get("custom_styles/normal").bg_color = color
+	_button_control.get("custom_styles/hover").bg_color = color
 
 
 ## Updates the button's text, colors, size and icon based on the level and its status.
@@ -130,10 +134,21 @@ func _refresh_appearance() -> void:
 			3: new_style_color = BUTTON_COLOR_GREEN
 			4: new_style_color = BUTTON_COLOR_BLUE
 			5: new_style_color = BUTTON_COLOR_PURPLE
-	_set_style_color(new_style_color)
+	refresh_style_color(new_style_color)
 
 
-func _on_pressed() -> void:
+func _refresh_size() -> void:
+	if not _button_control:
+		return
+	
+	_button_control.rect_size = rect_size
+
+
+func _on_resized() -> void:
+	_refresh_size()
+
+
+func _on_ButtonControl_pressed() -> void:
 	if lock_status == STATUS_LOCKED:
 		# level is locked, don't launch the level
 		return
@@ -143,18 +158,18 @@ func _on_pressed() -> void:
 		emit_signal("level_chosen")
 
 
-func _on_focus_entered() -> void:
+func _on_ButtonControl_focus_entered() -> void:
 	_focus_just_entered = true
 	var font: DynamicFont = _label.get("custom_fonts/font")
 	font.outline_color = Color("007a99")
 
 
-func _on_focus_exited() -> void:
+func _on_ButtonControl_focus_exited() -> void:
 	var font: DynamicFont = _label.get("custom_fonts/font")
 	font.outline_color = Color("6c4331")
 
 
-func _on_button_down() -> void:
+func _on_ButtonControl_button_down() -> void:
 	if _focus_just_entered:
 		pass
 	else:

project/assets/main/ui/level-select/hardcore-bg-skull-sheet.png.import
@@ -1,0 +1,35 @@
+[remap]
+
+importer="texture"
+type="StreamTexture"
+path="res://.import/hardcore-bg-skull-sheet.png-1aab292f0b37c61d4ecd29265d812850.stex"
+metadata={
+"vram_texture": false
+}
+
+[deps]
+
+source_file="res://assets/main/ui/level-select/hardcore-bg-skull-sheet.png"
+dest_files=[ "res://.import/hardcore-bg-skull-sheet.png-1aab292f0b37c61d4ecd29265d812850.stex" ]
+
+[params]
+
+compress/mode=0
+compress/lossy_quality=0.7
+compress/hdr_mode=0
+compress/bptc_ldr=0
+compress/normal_map=0
+flags/repeat=0
+flags/filter=true
+flags/mipmaps=true
+flags/anisotropic=true
+flags/srgb=2
+process/fix_alpha_border=true
+process/premult_alpha=false
+process/HDR_as_SRGB=false
+process/invert_color=false
+process/normal_map_invert_y=false
+stream=false
+size_limit=0
+detect_3d=true
+svg/scale=1.0

project/assets/main/ui/level-select/hardcore-fg-skull-sheet.png.import
@@ -1,0 +1,35 @@
+[remap]
+
+importer="texture"
+type="StreamTexture"
+path="res://.import/hardcore-fg-skull-sheet.png-bc2885b146f1160a3241c5c2916ca941.stex"
+metadata={
+"vram_texture": false
+}
+
+[deps]
+
+source_file="res://assets/main/ui/level-select/hardcore-fg-skull-sheet.png"
+dest_files=[ "res://.import/hardcore-fg-skull-sheet.png-bc2885b146f1160a3241c5c2916ca941.stex" ]
+
+[params]
+
+compress/mode=0
+compress/lossy_quality=0.7
+compress/hdr_mode=0
+compress/bptc_ldr=0
+compress/normal_map=0
+flags/repeat=0
+flags/filter=true
+flags/mipmaps=true
+flags/anisotropic=true
+flags/srgb=2
+process/fix_alpha_border=true
+process/premult_alpha=false
+process/HDR_as_SRGB=false
+process/invert_color=false
+process/normal_map_invert_y=false
+stream=false
+size_limit=0
+detect_3d=true
+svg/scale=1.0

project/project.godot
@@ -514,7 +514,7 @@ _global_script_classes=[ {
 "language": "GDScript",
 "path": "res://src/main/world/level-posse.gd"
 }, {
-"base": "Button",
+"base": "Control",
 "class": "LevelSelectButton",
 "language": "GDScript",
 "path": "res://src/main/ui/level-select/level-select-button.gd"

project/src/demo/ui/level-select/LevelSelectButtonDemo.tscn
@@ -1,13 +1,27 @@
-[gd_scene load_steps=5 format=2]
+[gd_scene load_steps=7 format=2]
 
 [ext_resource path="res://src/demo/ui/level-select/level-select-button-demo.gd" type="Script" id=1]
 [ext_resource path="res://src/main/ui/level-select/LevelSelectButton.tscn" type="PackedScene" id=2]
 [ext_resource path="res://src/main/ui/level-select/HookableLevelGradeLabel.tscn" type="PackedScene" id=3]
 [ext_resource path="res://src/main/ui/level-select/level-grade-labels.gd" type="Script" id=4]
+[ext_resource path="res://src/main/ui/level-select/HardcoreLevelSelectButton.tscn" type="PackedScene" id=5]
+
+[sub_resource type="StyleBoxFlat" id=1]
+bg_color = Color( 0.807843, 0.407843, 0.85098, 1 )
+corner_radius_top_left = 50
+corner_radius_top_right = 50
+corner_radius_bottom_right = 50
+corner_radius_bottom_left = 50
 
 [node name="Demo" type="Node"]
 script = ExtResource( 1 )
 LevelButtonScene = ExtResource( 2 )
+HardcoreLevelButtonScene = ExtResource( 5 )
+
+[node name="Panel" type="Panel" parent="."]
+anchor_right = 1.0
+anchor_bottom = 1.0
+custom_styles/panel = SubResource( 1 )
 
 [node name="Control" type="Control" parent="."]
 anchor_right = 1.0

commit 44e5bb5f9a625f7df01ef515987695b7649d166e

Fixed career map level select button focus_entered bug
Converting the LevelSelectButton from a Button to Control to add the
nice graphics in b21415d9 broke 'focus_entered' events from being
propagated correctly. This meant the distance indicators in Career mode
were broken.

Changed 'get_children().size()' calls to 'get_child_count()'

project/src/main/career/ui/career-map-level-select.gd
@@ -25,7 +25,12 @@ func _ready() -> void:
 ##
 ## For a boss level where only one level is available, this will return '0' if the level button is selected.
 func focused_level_button_index() -> int:
-	_prev_focused_level_button_index = _level_buttons_container.get_children().find(_control.get_focus_owner())
+	var focused_child_index := -1
+	for child_index in range(_level_buttons_container.get_child_count()):
+		if _level_buttons_container.get_child(child_index).has_focus():
+			focused_child_index = child_index
+			break
+	_prev_focused_level_button_index = focused_child_index
 	return _prev_focused_level_button_index
 
 

project/src/main/career/ui/progress-board-spots.gd
@@ -91,12 +91,12 @@ func _refresh_spots() -> void:
 	
 	# calculate minimum distance between two adjacent spots
 	var min_dist := 999999.0
-	for child_index in range(1, get_children().size()):
+	for child_index in range(1, get_child_count()):
 		var dist: float = get_child(child_index - 1).rect_position.distance_to(get_child(child_index).rect_position)
 		min_dist = min(dist, min_dist)
 	
 	# assign frames; make each spot a circle, dot, or star
-	for child_index in range(get_children().size()):
+	for child_index in range(get_child_count()):
 		var child: ProgressBoardSpot = get_child(child_index)
 		var possible_frames := []
 		if child_index == get_child_count() - 1:

project/src/main/ui/level-select/level-select-button.gd
@@ -176,6 +176,10 @@ func _on_ButtonControl_focus_entered() -> void:
 	_focus_just_entered = true
 	var font: DynamicFont = _label.get("custom_fonts/font")
 	font.outline_color = Color("007a99")
+	
+	# Propagate the focus_entered signal. This control cannot be focused, but other scenes such as the career map and
+	# level select screen need to react to our focus events
+	emit_signal("focus_entered")
 
 
 func _on_ButtonControl_focus_exited() -> void:

project/src/main/ui/menu/paged-level-buttons.gd
@@ -237,7 +237,7 @@ func _on_CheatCodeDetector_cheat_detected(cheat: String, detector: CheatCodeDete
 		_unlock_cheat_enabled = !_unlock_cheat_enabled
 		detector.play_cheat_sound(_unlock_cheat_enabled)
 		var button_index_to_focus := -1
-		for child_index in range(_grid_container.get_children().size()):
+		for child_index in range(_grid_container.get_child_count()):
 			if _grid_container.get_child(child_index).has_focus():
 				button_index_to_focus = child_index
 				break