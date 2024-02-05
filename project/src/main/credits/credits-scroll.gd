0b1b907b9c956ccfbd271a83ae5cf5e30dea5378
Poobslag <aaron.pieper@gmail.com>
2024-02-05 12:31:48 -0500

    Added 'credits movie' viewport logic.
    Added logic for a movie to play alongside the credits. We'd like some
    cinematic scenes showing creatures running around, being hoisted in the
    air, and fun things like that. This viewport can now fade in and out or
    play on the left or right side of the credits.

--- project/src/main/credits/credits-scroll.gd
+++ project/src/main/credits/credits-scroll.gd
@@ -9,6 +9,12 @@ extends Control
 const FADE_OUT_POINT_POSITION_TOP := Vector2.ZERO
 const FADE_OUT_POINT_POSITION_BOTTOM := Vector2(0, 110)
 
+## The horizontal positions for the movie which plays alongside the credits.
+const MOVIE_POSITION_OFFSCREEN_LEFT := Vector2(-512, 0)
+const MOVIE_POSITION_LEFT := Vector2(0, 0)
+const MOVIE_POSITION_RIGHT := Vector2(512, 0)
+const MOVIE_POSITION_OFFSCREEN_RIGHT := Vector2(1024, 0)
+
 ## The horizontal positions for the credits in LEFT, CENTER or RIGHT positions.
 const LINES_POSITION_LEFT := Vector2.ZERO
 const LINES_POSITION_CENTER := Vector2(256, 0)
@@ -25,6 +31,12 @@ export (Credits.CreditsPosition) var credits_position := Credits.CreditsPosition
 		setget set_credits_position
 
 var velocity: Vector2 = Vector2(0, -50)
+
+## 'true' if the credits movie should be playing onscreen. The credits movie will still not be visible if the credits
+## position is CENTER_BOTTOM or CENTER_TOP, since the lines will be in the way.
+var movie_visible: bool = false setget set_movie_visible
+
+var _movie_tween_x: SceneTreeTween
 
 var _lines_tween_x: SceneTreeTween
 var _lines_tween_y: SceneTreeTween
@@ -49,8 +61,15 @@ onready var _lines := $FixedContainer/ScrollingContainer/Lines
 ## Puzzle pieces which fly out of the floating orb.
 onready var _credits_pieces: CreditsPieces = $FixedContainer/OrbHolder/Pieces
 
+## Movie which plays alongside the credits lines.
+onready var _movie: Node2D = $Movie
+
 func _ready() -> void:
 	MusicPlayer.play_credits_bgm()
+	
+	# initialize the movie to visible but transparent; this way the tweens don't have to toggle the 'visible' property
+	_movie.visible = true
+	_movie.modulate = Color.transparent
 
 
 ## Adds a text line like 'Directed by:'
@@ -94,6 +113,18 @@ func set_credits_position(new_credits_position: int) -> void:
 	credits_position = new_credits_position
 	
 	_shift_credits(old_credits_position, new_credits_position)
+	_shift_movie(movie_visible, movie_visible, old_credits_position, new_credits_position)
+
+
+func set_movie_visible(new_movie_visible: bool) -> void:
+	var old_movie_visible := movie_visible
+	movie_visible = new_movie_visible
+	
+	_shift_movie(old_movie_visible, new_movie_visible, credits_position, credits_position)
+
+
+func set_target_header_letter_for_piece(piece_index: int, header_letter_index: int) -> void:
+	_credits_pieces.set_target_header_letter_for_piece(piece_index, header_letter_index)
 
 
 ## Shifts the title credits lines based on the credits position.
@@ -117,6 +148,52 @@ func _shift_credits(old_credits_position: int, new_credits_position: int) -> voi
 	if Credits.is_position_right(credits_position) and not Credits.is_position_right(old_credits_position):
 		# move credits to a right position; shift everything horizontally
 		_shift_lines_horizontally(LINES_POSITION_RIGHT)
+
+
+## Shifts the credits movie based on the credits position.
+func _shift_movie(old_movie_visible: bool, new_movie_visible: bool, \
+		old_credits_position: int, new_credits_position: int) -> void:
+	
+	# The movie swoops offscreen if it's set invisible or blocked by the credit scroll.
+	var tweening_out := old_movie_visible and not Credits.is_position_center(old_credits_position) \
+			and (new_credits_position != old_credits_position or new_movie_visible == false)
+	
+	# The movie swoops onscreen if it's set visible and not blocked by the credits scroll.
+	var tweening_in := new_movie_visible and not Credits.is_position_center(new_credits_position) \
+			and (new_credits_position != old_credits_position or old_movie_visible == false)
+	
+	if tweening_out:
+		_movie_tween_x = Utils.recreate_tween(self, _movie_tween_x)
+		_movie_tween_x.tween_property(_movie, "modulate", Color.transparent, 0.1).set_delay(0.2)
+		var new_movie_position: Vector2
+		if Credits.is_position_left(old_credits_position):
+			new_movie_position = MOVIE_POSITION_OFFSCREEN_RIGHT
+		else:
+			new_movie_position = MOVIE_POSITION_OFFSCREEN_LEFT
+		_movie_tween_x.parallel().tween_property(_movie, "position", new_movie_position, 0.5) \
+				.set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN_OUT)
+	
+	if tweening_in:
+		if not tweening_out:
+			_movie_tween_x = Utils.recreate_tween(self, _movie_tween_x)
+		
+		# Set the movie position so that it swoops in from the correct direction offscreen
+		if tweening_out or _movie.position in [MOVIE_POSITION_OFFSCREEN_LEFT, MOVIE_POSITION_OFFSCREEN_RIGHT]:
+			var new_source_movie_position: Vector2
+			if Credits.is_position_right(new_credits_position):
+				new_source_movie_position = MOVIE_POSITION_OFFSCREEN_LEFT
+			else:
+				new_source_movie_position = MOVIE_POSITION_OFFSCREEN_RIGHT
+			_movie_tween_x.tween_callback(_movie, "set", ["position", new_source_movie_position])
+		
+		_movie_tween_x.tween_property(_movie, "modulate", Color.white, 0.1).set_delay(0.2)
+		var new_movie_position: Vector2
+		if Credits.is_position_left(new_credits_position):
+			new_movie_position = MOVIE_POSITION_RIGHT
+		else:
+			new_movie_position = MOVIE_POSITION_LEFT
+		_movie_tween_x.parallel().tween_property(_movie, "position", new_movie_position, 0.5) \
+				.set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN_OUT)
 
 
 ## Adjusts the fade_out_point and shows or hides the header.
