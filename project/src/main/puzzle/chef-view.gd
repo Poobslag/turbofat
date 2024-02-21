9fc74273cb4e1f27964ab0af403d3c896f8693d9
Poobslag <aaron.pieper@gmail.com>
2024-02-21 15:21:18 -0500

    chef-view -> dynamic-viewport-container
    This component is very generic, and I plan to reuse it in the credits.

--- project/src/main/puzzle/chef-view.gd
+++
@@ -1,8 +0,0 @@
-extends ViewportContainer
-## Shows the player's chef character in the restaurant scene.
-
-export (NodePath) var restaurant_viewport_path: NodePath
-
-func _ready() -> void:
-	$Viewport.world_2d = get_node(restaurant_viewport_path).world_2d if restaurant_viewport_path else null
-	$Viewport.size = rect_size * 4
---
+++ project/src/main/utils/dynamic-viewport-container.gd
@@ -0,0 +1,22 @@
+extends ViewportContainer
+## Container which resizes its child viewport based on the container size.
+
+export (NodePath) var restaurant_viewport_path: NodePath
+
+onready var _viewport := $Viewport
+
+func _ready() -> void:
+	connect("item_rect_changed", self, "_on_item_rect_changed")
+	
+	_viewport.world_2d = get_node(restaurant_viewport_path).world_2d if restaurant_viewport_path else null
+	_refresh_viewport_size()
+
+
+func _refresh_viewport_size() -> void:
+	_viewport.size = rect_size * stretch_shrink
+
+
+## When the viewport's dimensions change, we refresh the viewport's size. Otherwise, Godot automatically changes the
+## viewport's size to something nonsensical.
+func _on_item_rect_changed() -> void:
+	_refresh_viewport_size()
