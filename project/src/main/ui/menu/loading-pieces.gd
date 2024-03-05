746cc85b40214a34d1a9111e18eafa0b11967f6e
Poobslag <aaron.pieper@gmail.com>
2024-03-05 09:02:37 -0500

    piece_scene -> PieceScene

--- project/src/main/ui/menu/loading-pieces.gd
+++ project/src/main/ui/menu/loading-pieces.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 export (NodePath) var orb_path: NodePath
 export (NodePath) var progress_bar_path: NodePath
-export (PackedScene) var piece_scene: PackedScene
+export (PackedScene) var PieceScene: PackedScene
 
 onready var _orb: LoadingOrb = get_node(orb_path)
 onready var _progress_bar: LoadingProgressBar = get_node(progress_bar_path)
@@ -14,6 +14,6 @@ func _ready() -> void:
 
 ## When the orb advances to the next frame, we launch a puzzle piece.
 func _on_Orb_frame_changed() -> void:
-	var piece := piece_scene.instance()
+	var piece := PieceScene.instance()
 	piece.initialize(_orb, _progress_bar)
 	add_child(piece)
