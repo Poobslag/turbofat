a6d10cece9eaaeba6a85cb4fa0b5bea972298ef8
Poobslag <aaron.pieper@gmail.com>
2024-04-03 15:48:59 -0400

    Chocolava/credits applause SFX are continuous
    Replaced 6-second cheer sound effect with a continuous sound effect.
    
    The cutscene now specifies when it wants the sound effect to stop with a
    'stop_sfx' call. I also removed the second 'lava_cheer_2' sound effect.
    This was a slightly different sound effect with more lead-in applause,
    but it felt unnecessary.
    
    The credits now play applause continuously, and the volume decreases as
    the crowdies stop cheering. There is a second separate applause sound
    effect for the lead-in applause when tossing the characters in the air.

--- project/src/main/world/environment/lava/lava-sfx.gd
+++ project/src/main/world/environment/lava/lava-sfx.gd
@@ -1,18 +1,35 @@
 extends Node
 ## Plays sound effects for Chocolava Canyon's cutscenes.
 
-onready var _cheer_1 := $Cheer1
-onready var _cheer_2 := $Cheer2
+onready var _cheer := $Cheer
+
+var _tween: SceneTreeTween
 
 func _ready() -> void:
+	SceneTransition.connect("fade_out_started", self, "_on_SceneTransition_fade_out_started")
+	
 	if Global.get_overworld_ui():
 		Global.get_overworld_ui().connect("chat_event_meta_played", self, "_on_OverworldUi_chat_event_meta_played")
+
+
+## Fade out and stop the sound effect
+func _stop_sfx(duration: float) -> void:
+	_tween = Utils.recreate_tween(self, _tween)
+	_tween.set_parallel(true)
+	_tween.tween_property(_cheer, "volume_db", -23.0, duration)
+	_tween.tween_callback(_cheer, "stop").set_delay(duration)
 
 
 ## Listen for 'play_sfx' chat events and play sound effects
 func _on_OverworldUi_chat_event_meta_played(meta_item: String) -> void:
 	match meta_item:
-		"play_sfx lava_cheer_1":
-			_cheer_1.play()
-		"play_sfx lava_cheer_2":
-			_cheer_2.play()
+		"play_sfx lava_cheer":
+			_tween = Utils.kill_tween(_tween)
+			_cheer.volume_db = -8.0
+			_cheer.play()
+		"stop_sfx lava_cheer":
+			_stop_sfx(1.0)
+
+
+func _on_SceneTransition_fade_out_started(duration: float) -> void:
+	_stop_sfx(duration * 0.5)
