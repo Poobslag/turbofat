6d3877508550e7b8cacca75e65cd8bb131f947b9
Poobslag <aaron.pieper@gmail.com>
2024-07-19 11:57:11 -0400

    Bug: Gut tests were deleting user data.
    Gut tests had logic to override the player's data_filename, but this was
    then being reverted when the SystemSave reacted to the 'save_slot_changed'
    signal, setting the filename back to the user's save slots. The result
    was that some tests would delete the user's data instead of deleting the
    test's data.
    
    I've added two protections for the Gut tests:
    1. SystemSave no longer assigns the filenames during tests.
    2. Gut tests override the filenames by default in a pre-run hook.

--- project/src/test/pre-run-hook.gd
+++ project/src/test/pre-run-hook.gd
@@ -1,6 +1,15 @@
 extends GutHookScript
 ## Performs initialization steps for Gut tests.
 
+const TEMP_FILENAME := "test-harbor-unobtainable.json"
+const TEMP_LEGACY_FILENAME := "test-moaning-corn.json"
+
 func run() -> void:
 	# Prevent the player's settings from triggering fullscreen mode or vsync.
 	SystemData.disallow_graphics_customization()
+	
+	# Prevent the tests from deleting the user's save data.
+	SystemSave.data_filename = "user://%s" % TEMP_FILENAME
+	SystemSave.legacy_filename = "user://%s" % TEMP_LEGACY_FILENAME
+	PlayerSave.data_filename = "user://%s" % TEMP_FILENAME
+	SystemSave.ignore_save_slot_filename = true
