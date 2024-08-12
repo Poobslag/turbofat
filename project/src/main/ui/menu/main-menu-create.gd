357241dfc3d2ef9cb0e8dd64342bb785fbe76023
Poobslag <aaron.pieper@gmail.com>
2024-08-12 09:40:37 -0400

    Reorganized main menu: Adventure, Practice, Avatar
    Reorganized main menu from 'Play: Career, Practice, Tutorials' 'Create:
    Creatures, Levels' to 'Adventure: Play, Avatar' 'Practice: Training,
    Tutorials'.
    
    I didn't like the 'Create' menu only having one item. I also didn't
    think a typical player would connect 'Create -> Creatures' to
    customizing their own avatar.
    
    I replaced the DNA icon for creating creatures with a face icon used in
    the Creature editor.

--- project/src/main/ui/menu/main-menu-create.gd
+++
@@ -1,7 +0,0 @@
-extends VBoxContainer
-## Main menu panel with various editors.
-
-
-func _on_Creatures_pressed() -> void:
-	MusicPlayer.stop()
-	SceneTransition.push_trail("res://src/main/editor/creature/CreatureEditor.tscn")
