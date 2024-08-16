1080de9a0cf231784d59e626c291a9b83610590f
Poobslag <aaron.pieper@gmail.com>
2024-08-16 10:41:41 -0400

    Refactor: Extract 'LevelSelectButton.decorate_for_level'
    Assignment of the level name, id, duration and color was behavior
    partially shared between PagedLevelButtons and CareerMapLevelSelect.
    With the upcoming TrainingMenu improvements, it's convenient to have
    this code in one place.

--- project/src/main/ui/menu/paged-level-buttons.gd
+++ project/src/main/ui/menu/paged-level-buttons.gd
@@ -171,38 +171,9 @@ func _max_selectable_page() -> int:
 ## 	A new orphaned LevelSelectButton instance for the specified level
 func _level_select_button(level_id: String, level_count: int) -> Node:
 	var level_settings: LevelSettings = _level_settings_by_id[level_id]
-	
 	var level_button: LevelSelectButton = LevelButtonScene.instance()
-	level_button.level_id = level_id
+	level_button.decorate_for_level(region, level_settings, _unlock_cheat_enabled)
 	level_button.level_duration = LevelSelectButton.MEDIUM if level_count >= 10 else LevelSelectButton.LONG
-	level_button.level_name = level_settings.name
-	
-	# calculate the lock status
-	level_button.lock_status = LevelSelectButton.STATUS_NONE
-	if region is CareerRegion and not PlayerData.level_history.has_result(level_id) and not _unlock_cheat_enabled:
-		# career levels are locked if the player hasn't played them
-		level_button.lock_status = LevelSelectButton.STATUS_LOCKED
-	elif region.id == OtherRegion.ID_TUTORIAL:
-		# tutorial levels show a checkmark if completed
-		if PlayerData.level_history.is_level_finished(level_id):
-			level_button.lock_status = LevelSelectButton.STATUS_CLEARED
-	elif region is OtherRegion and region.id in [OtherRegion.ID_RANK, OtherRegion.ID_MARATHON]:
-		# rank/marathon levels show a crown if completed
-		if PlayerData.level_history.is_level_success(level_id):
-			level_button.lock_status = LevelSelectButton.STATUS_CROWN
-	
-	# calculate the background color. this is usually random, but for rank mode we use specific colors
-	if level_settings.color_string:
-		match level_settings.color_string:
-			"red": level_button.set_bg_color(LevelSelectButton.BUTTON_COLOR_RED)
-			"orange": level_button.set_bg_color(LevelSelectButton.BUTTON_COLOR_ORANGE)
-			"yellow": level_button.set_bg_color(LevelSelectButton.BUTTON_COLOR_YELLOW)
-			"green": level_button.set_bg_color(LevelSelectButton.BUTTON_COLOR_GREEN)
-			"blue": level_button.set_bg_color(LevelSelectButton.BUTTON_COLOR_BLUE)
-			"purple": level_button.set_bg_color(LevelSelectButton.BUTTON_COLOR_PURPLE)
-			_:
-				push_warning("Unrecognized color string '%s'" % [level_settings.color_string])
-				pass
 	
 	level_button.connect("focus_entered", self, "_on_LevelButton_focus_entered", [level_button, level_id])
 	level_button.connect("level_chosen", self, "_on_LevelButton_level_chosen", [level_settings])
