92507a673cdd652315e2f4bd50a37d66f3c385d4
Poobslag <aaron.pieper@gmail.com>
2024-09-27 11:43:49 -0400

    Removed legacy rank/duration calculator, show_x_rank fields
    These mechanisms are preserved on the 'legacy rank calculator' branch if
    for some reason we want to resurrect them.

--- project/src/main/puzzle/level/rank-rules.gd
+++ project/src/main/puzzle/level/rank-rules.gd
@@ -1,33 +1,6 @@
 class_name RankRules
 ## Tweaks to rank calculation.
 
-## Parses legacy rank criteria such as 'legacy box_factor' into the 'legacy_rules' field.
-class LegacyCriteriaParser extends RuleParser.PropertyParser:
-	func _init(init_target: Object, init_name: String).(init_target, init_name) -> void:
-		pass
-	
-	
-	# don't write legacy properties, we don't want to preserve them in future versions of the game
-	func to_json_strings() -> Array:
-		return []
-	
-	
-	func from_json_string(json: String) -> void:
-		var json_strings := json.split(" ")
-		
-		match json_strings[1]:
-			"box_factor", \
-			"combo_factor", \
-			"extra_seconds_per_piece", \
-			"master_pickup_score", \
-			"master_pickup_score_per_line":
-				target().legacy_rules[json_strings[1]] = float(json_strings[2])
-			"customer_combo", \
-			"leftover_lines", \
-			"preplaced_pieces":
-				target().legacy_rules[json_strings[1]] = int(json_strings[2])
-
-
 ## Parses a json string like 'rank_criteria m=1000 s=800' into a RankCriteria instance.
 class RankCriteriaParser extends RuleParser.PropertyParser:
 	func _init(init_target: Object, init_name: String).(init_target, init_name) -> void:
@@ -53,52 +26,10 @@ class RankCriteriaParser extends RuleParser.PropertyParser:
 		return not target().rank_criteria.thresholds_by_grade
 
 
-## Parses a json string like 'hide_combos_rank' into an enum like 'ShowRank.HIDE'
-class ShowRankPropertyParser extends RuleParser.PropertyParser:
-	var _hide_string: String
-	
-	func _init(init_target: Object, init_name: String).(init_target, init_name) -> void:
-		default = []
-		_hide_string = "hide%s" % [init_name.trim_prefix("show")]
-		keys.append(_hide_string)
-	
-	
-	func to_json_strings() -> Array:
-		var result := []
-		match target().get(name):
-			ShowRank.SHOW: result.append(name)
-			ShowRank.HIDE: result.append(_hide_string)
-		return result
-	
-	
-	func from_json_string(json: String) -> void:
-		match json:
-			name: target().set(name, ShowRank.SHOW)
-			_hide_string: target().set(name, ShowRank.HIDE)
-			_: push_warning("Unrecognized: %s" % [json])
-	
-	
-	func is_default() -> bool:
-		return target().get(name) == ShowRank.DEFAULT
-
-
-enum ShowRank {
-	DEFAULT, # rank should be automatically shown/hidden based on the finish condition
-	SHOW, # rank should be shown
-	HIDE, # rank should be hidden
-}
-
 var rank_criteria: RankCriteria = RankCriteria.new()
 
 ## the level's estimated duration
 var duration: int
-
-var show_boxes_rank: int = ShowRank.DEFAULT
-var show_combos_rank: int = ShowRank.DEFAULT
-var show_lines_rank: int = ShowRank.DEFAULT
-var show_pickups_rank: int = ShowRank.DEFAULT
-var show_pieces_rank: int = ShowRank.DEFAULT
-var show_speed_rank: int = ShowRank.DEFAULT
 
 ## 'true' if the results screen should be skipped. Used for tutorials.
 var skip_results := false
@@ -110,21 +41,13 @@ var success_bonus := 0.0
 
 ## If 'true' the player is not given a rank for this level.
 var unranked := false
-var legacy_rules := {}
 
 var _rule_parser: RuleParser
 
 func _init() -> void:
 	_rule_parser = RuleParser.new(self)
 	_rule_parser.add_int("duration")
-	_rule_parser.add(LegacyCriteriaParser.new(self, "legacy"))
 	_rule_parser.add(RankCriteriaParser.new(self, "rank_criteria"))
-	_rule_parser.add(ShowRankPropertyParser.new(self, "show_boxes_rank"))
-	_rule_parser.add(ShowRankPropertyParser.new(self, "show_combos_rank"))
-	_rule_parser.add(ShowRankPropertyParser.new(self, "show_lines_rank"))
-	_rule_parser.add(ShowRankPropertyParser.new(self, "show_pickups_rank"))
-	_rule_parser.add(ShowRankPropertyParser.new(self, "show_pieces_rank"))
-	_rule_parser.add(ShowRankPropertyParser.new(self, "show_speed_rank"))
 	_rule_parser.add_bool("skip_results")
 	_rule_parser.add_float("success_bonus")
 	_rule_parser.add_bool("unranked")
--- project/src/test/puzzle/level/test-rank-rules.gd
+++ project/src/test/puzzle/level/test-rank-rules.gd
@@ -19,12 +19,6 @@ func test_to_json_empty() -> void:
 func test_convert_to_json_and_back() -> void:
 	rules.rank_criteria.add_threshold("M", 1000)
 	rules.rank_criteria.add_threshold("S-", 500)
-	rules.show_boxes_rank = RankRules.ShowRank.SHOW
-	rules.show_combos_rank = RankRules.ShowRank.HIDE
-	rules.show_lines_rank = RankRules.ShowRank.SHOW
-	rules.show_pickups_rank = RankRules.ShowRank.HIDE
-	rules.show_pieces_rank = RankRules.ShowRank.SHOW
-	rules.show_speed_rank = RankRules.ShowRank.HIDE
 	rules.skip_results = true
 	rules.success_bonus = 9.0
 	rules.unranked = true
@@ -33,12 +27,6 @@ func test_convert_to_json_and_back() -> void:
 	assert_eq(2, rules.rank_criteria.thresholds_by_grade.size())
 	assert_eq(1000, rules.rank_criteria.thresholds_by_grade.get("M"))
 	assert_eq(500, rules.rank_criteria.thresholds_by_grade.get("S-"))
-	assert_eq(rules.show_boxes_rank, RankRules.ShowRank.SHOW)
-	assert_eq(rules.show_combos_rank, RankRules.ShowRank.HIDE)
-	assert_eq(rules.show_lines_rank, RankRules.ShowRank.SHOW)
-	assert_eq(rules.show_pickups_rank, RankRules.ShowRank.HIDE)
-	assert_eq(rules.show_pieces_rank, RankRules.ShowRank.SHOW)
-	assert_eq(rules.show_speed_rank, RankRules.ShowRank.HIDE)
 	assert_eq(rules.skip_results, true)
 	assert_eq(rules.success_bonus, 9.0)
 	assert_eq(rules.unranked, true)
