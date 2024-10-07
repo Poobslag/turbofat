38c987a82a7db351ffc9533c25195f6f84961b5d
Poobslag <aaron.pieper@gmail.com>
2024-10-07 13:04:22 -0400

    Combined steam achievement tests to avoid gut warning
    Gut was sending a [did not assert] warning because it's strange to have
    a test whose entire purpose is to not crash. I've combined these two
    tests to avoid the warning.

--- project/src/test/steam/test-steam-achievements.gd
+++ project/src/test/steam/test-steam-achievements.gd
@@ -1,10 +1,8 @@
 extends GutTest
 
-func test_achievements_exist() -> void:
+func test_achievements_dont_crash() -> void:
 	# There should be a bunch of achievements. If there's fewer than five, something is wrong
 	assert_true(SteamAchievements.get_achievements().size() >= 5)
-
-
-func test_achievements_dont_crash() -> void:
+	
 	for achievement in SteamAchievements.get_achievements():
 		achievement.refresh_achievement()
