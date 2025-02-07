c568977c5d232f2cc7e5d549a55ec6f6ff0a181c
Poobslag <aaron.pieper@gmail.com>
2025-02-07 09:01:43 -0500

    Touch size settings: tick count, limits
    Decreased the maximum touch size from 1.69 to 1.40. In an upcoming
    change which introduces the hold piece, a scale of 1.69 is too large
    and causes the buttons to overlap.
    
    Settings menu no longer hard-codes the max value and tick count, these
    values are derived from the SettingsTouchSize.VALUES constant.

--- project/src/main/ui/settings/settings-touch-size.gd
+++ project/src/main/ui/settings/settings-touch-size.gd
@@ -3,11 +3,13 @@ extends Control
 
 ## step values of 1.19
 const VALUES := [
-	0.25, 0.30, 0.35, 0.42, 0.50, 0.59, 0.71, 0.84, 1.00, 1.19, 1.42, 1.69
+	0.25, 0.30, 0.35, 0.42, 0.50, 0.59, 0.71, 0.84, 1.00, 1.18, 1.40
 ]
 
 func _ready() -> void:
 	$Control/HSlider.value = Utils.find_closest(VALUES, SystemData.touch_settings.size)
+	$Control/HSlider.max_value = VALUES.size() - 1
+	$Control/HSlider.tick_count = VALUES.size()
 
 
 func _on_HSlider_value_changed(index: float) -> void:
